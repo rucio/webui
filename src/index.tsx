1a1a01cc7e6563f2ee69443f6f9e314c46e42173
maany <imptodefeat@gmail.com>
2022-03-09 12:42:30 +0000

    add eslint

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+console.log("yo")
 ReactDOM.render(
   <React.StrictMode>
     <App />
