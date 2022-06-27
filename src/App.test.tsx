9bf80e28c8549d00b96b23b00dbef3cb18e201f7
Nimish Bongale <nimishnb98@gmail.com>
2022-06-27 22:07:37 +0530

    Feature 10 login page (#35)
    * Reconfigure autoests to run on all branches except gh-pages
    
    * Reconfigure build-deploy-storybook to run on master with custom bash script
    
    * Add build-storybook cicd for PRs
    
    * Add custom bash script to deploy to gh pages
    
    * Remove "push" & "pull_request" duplication
    
    * Rectify git stash
    
    * Add git config to identify user
    
    * Add fetch-depth 0 to checkout all branches
    
    * Add git config as required by GitHub actions
    
    * Remove additional folders, rename rucio to rucio-bot
    
    * CI CD completed!
    
    * Update GitHub config, force remove folders starting with a "."
    
    * Add .deepsource.toml
    
    * Update Button component with latest styles
    
    * Update & move header component into folder
    
    * Update & move Page component into folder
    
    * Create image component
    
    * Create Card component
    
    * Create Form component
    
    * Create TextInput Component
    
    * Delete deepsource.toml
    
    * Remove preexisting template CSS
    
    * Add Login Page
    
    * Create main.css with global styles
    
    * Add utlities CSS
    
    * Update test to make CI/CD pass for now
    
    Co-authored-by: DeepSource Bot <bot@deepsource.io>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders react app', () => {
     render(<App />)
-    const linkElement = screen.getByText(/learn react/i)
+    const linkElement = screen.getByText(/Rucio Login/i)
     expect(linkElement).toBeInTheDocument()
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Button } from './stories/Button/Button'
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
+            <div className="limiter">
+                <div
+                    className="container-login100"
+                    style={{ backgroundColor: 'black' }}
                 >
-                    Learn React
-                </a>
-            </header>
+                    <div className="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
+                        <form className="login100-form validate-form flex-sb flex-w">
+                            <img src="https://rucio-ui.cern.ch/media/favicon.ico" />
+                            <span className="login100-form-title p-b-27">
+                                Rucio Login
+                                <h4>Welcome to Rucio!</h4>
+                            </span>
+
+                            <a href="#" className="btn-face m-b-20">
+                                <i className="fa fa-certificate"></i>
+                                X509 Certificate
+                            </a>
+
+                            <a href="#" className="btn-face m-b-20">
+                                <i className="fa fa-certificate"></i>
+                                OIDC OAuth
+                            </a>
+
+                            <div className="p-t-31 p-b-9">
+                                <span className="txt1">Username</span>
+                            </div>
+                            <div
+                                className="wrap-input100 validate-input"
+                                data-validate="Username is required"
+                            >
+                                <input
+                                    className="input100"
+                                    type="text"
+                                    name="username"
+                                />
+                                <span className="focus-input100"></span>
+                            </div>
+
+                            <div className="p-t-13 p-b-9">
+                                <span className="txt1">Password</span>
+
+                                <a href="#" className="txt2 bo1 m-l-5">
+                                    Forgot?
+                                </a>
+                            </div>
+                            <div
+                                className="wrap-input100 validate-input"
+                                data-validate="Password is required"
+                            >
+                                <input
+                                    className="input100"
+                                    type="password"
+                                    name="pass"
+                                />
+                                <span className="focus-input100"></span>
+                            </div>
+
+                            <div className="container-login100-form-btn m-t-17">
+                                <button className="login100-form-btn">
+                                    Sign In
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
