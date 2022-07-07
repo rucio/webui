f65e8161552447cfbe262f71d242c0cef34b9a08
nimishbongale <nimishnb98@gmail.com>
2022-07-07 10:22:03 +0530

    Moved utils to tools, created .env.template, updated README.md

--- src/util.ts
+++
@@ -1,8 +0,0 @@
-// access any environment variable with env('KEY')
-export const env = (value: string): string | undefined => {
-    if (process.env['REACT_APP_local'] === 'true') {
-        return process.env['REACT_APP_' + value]
-    } else {
-        return process.env[value]
-    }
-}
