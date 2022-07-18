e8c0d754559d153e0a8d1c3d8ec79d97d312a3da
nimishbongale <nimishnb98@gmail.com>
2022-07-18 12:31:51 +0530

    Update resApiWrapper functions according to PR comments

--- src/utils/restApiWrapper.ts
+++ src/utils/restApiWrapper.ts
@@ -1,58 +1,89 @@
+import { env } from '../util'
+
+const defaultHostEndpoint: string | undefined = env('RUCIO_HOST')
+
 export async function postData(
-    url = '',
+    endpoint = '/',
+    queryParams = {},
     headers = {},
     data = {},
+    host = defaultHostEndpoint,
 ): Promise<any> {
-    const response = await fetch(url, {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {
-            ...headers,
-            'Content-Type': 'application/json',
+    const response = await fetch(
+        host + endpoint + '?' + new URLSearchParams(queryParams),
+        {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
         },
-        body: JSON.stringify(data),
-    })
+    )
     return response
 }
 
-export async function getData(url = '', headers = {}): Promise<any> {
-    const response = await fetch(url, {
-        method: 'GET',
-        mode: 'no-cors',
-        headers: {
-            ...headers,
-            'Content-Type': 'application/json',
+export async function getData(
+    endpoint = '/',
+    queryParams = {},
+    headers = {},
+    host = defaultHostEndpoint,
+): Promise<any> {
+    const response = await fetch(
+        host + endpoint + '?' + new URLSearchParams(queryParams),
+        {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json',
+            },
         },
-    })
+    )
     return response
 }
 
-export async function putData(url = '', headers = {}, data = {}): Promise<any> {
-    const response = await fetch(url, {
-        method: 'PUT',
-        mode: 'no-cors',
-        headers: {
-            ...headers,
-            'Content-Type': 'application/json',
+export async function putData(
+    endpoint = '/',
+    queryParams = {},
+    headers = {},
+    data = {},
+    host = defaultHostEndpoint,
+): Promise<any> {
+    const response = await fetch(
+        host + endpoint + '?' + new URLSearchParams(queryParams),
+        {
+            method: 'PUT',
+            mode: 'no-cors',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
         },
-        body: JSON.stringify(data),
-    })
+    )
     return response
 }
 
 export async function deleteData(
-    url = '',
+    endpoint = '/',
+    queryParams = {},
     headers = {},
     data = {},
+    host = defaultHostEndpoint,
 ): Promise<any> {
-    const response = await fetch(url, {
-        method: 'DELETE',
-        mode: 'no-cors',
-        headers: {
-            ...headers,
-            'Content-Type': 'application/json',
+    const response = await fetch(
+        host + endpoint + '?' + new URLSearchParams(queryParams),
+        {
+            method: 'DELETE',
+            mode: 'no-cors',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
         },
-        body: JSON.stringify(data),
-    })
+    )
     return response
 }
