7925d4b38cd1a1282bd80aaf83066abe5ede280e
nimishbongale <nimishnb98@gmail.com>
2022-07-20 10:17:19 +0530

    Changes to restApiWrapper functions to account for PR comments

--- src/utils/restApiWrapper.ts
+++ src/utils/restApiWrapper.ts
@@ -4,28 +4,18 @@ const defaultHostEndpoint: string | undefined = env('RUCIO_HOST')
 
 export async function postData(
     endpoint: string | undefined = '/',
-    queryParams:
-        | string
-        | string[][]
-        | Record<string, string>
-        | URLSearchParams
-        | undefined = {},
     headers: HeadersInit | undefined = {},
     data: unknown = {},
     host: string | undefined = defaultHostEndpoint,
 ): Promise<any> {
-    const response = await fetch(
-        host + endpoint + '?' + new URLSearchParams(queryParams),
-        {
-            method: 'POST',
-            mode: 'no-cors',
-            headers: {
-                ...headers,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
+    const response = await fetch(host + endpoint, {
+        method: 'POST',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json',
         },
-    )
+        body: JSON.stringify(data),
+    })
     return response
 }
 
@@ -44,7 +34,6 @@ export async function getData(
         host + endpoint + '?' + new URLSearchParams(queryParams),
         {
             method: 'GET',
-            mode: 'no-cors',
             headers: {
                 ...headers,
                 'Content-Type': 'application/json',
@@ -56,54 +45,34 @@ export async function getData(
 
 export async function putData(
     endpoint: string | undefined = '/',
-    queryParams:
-        | string
-        | string[][]
-        | Record<string, string>
-        | URLSearchParams
-        | undefined = {},
     headers: HeadersInit | undefined = {},
     data: unknown = {},
     host: string | undefined = defaultHostEndpoint,
 ): Promise<any> {
-    const response = await fetch(
-        host + endpoint + '?' + new URLSearchParams(queryParams),
-        {
-            method: 'PUT',
-            mode: 'no-cors',
-            headers: {
-                ...headers,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
+    const response = await fetch(host + endpoint, {
+        method: 'PUT',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json',
         },
-    )
+        body: JSON.stringify(data),
+    })
     return response
 }
 
 export async function deleteData(
     endpoint: string | undefined = '/',
-    queryParams:
-        | string
-        | string[][]
-        | Record<string, string>
-        | URLSearchParams
-        | undefined = {},
     headers: HeadersInit | undefined = {},
     data: unknown = {},
     host: string | undefined = defaultHostEndpoint,
 ): Promise<any> {
-    const response = await fetch(
-        host + endpoint + '?' + new URLSearchParams(queryParams),
-        {
-            method: 'DELETE',
-            mode: 'no-cors',
-            headers: {
-                ...headers,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
+    const response = await fetch(host + endpoint, {
+        method: 'DELETE',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json',
         },
-    )
+        body: JSON.stringify(data),
+    })
     return response
 }
