0a395b8358fb2b2a30de1858e22d33315a1a6c0a
nimishbongale <nimishnb98@gmail.com>
2022-07-22 02:58:49 +0530

    Unit tests for login page

--- src/tests/Login.test.tsx
+++ src/tests/Login.test.tsx
@@ -1,35 +1,91 @@
-import { render, screen } from '@testing-library/react'
+import { act, fireEvent, render, screen } from '@testing-library/react'
 import Login from '../components/Login'
 import { BrowserRouter } from 'react-router-dom'
 
-test('Check textual content, no user action', () => {
+test('Login page render', () => {
     render(
         <BrowserRouter>
             <Login />
         </BrowserRouter>,
     )
-    const loginPrimaryText = screen.getByText(/Rucio Login/i)
+    const loginPrimaryText = screen.getByText('Rucio Login')
     expect(loginPrimaryText).toBeInTheDocument()
 
-    const loginSecondaryText = screen.getByText(/Welcome to Rucio!/i)
+    const loginSecondaryText = screen.getByText('Welcome to Rucio!')
     expect(loginSecondaryText).toBeInTheDocument()
 
-    const x509PassAuthText = screen.getByText(/x509 Certificate/i)
+    const x509PassAuthText = screen.getByText('x509 Certificate')
     expect(x509PassAuthText).toBeInTheDocument()
 
-    const OAuthText = screen.getByText(/OIDC OAuth/i)
+    const OAuthText = screen.getByText('OIDC OAuth')
     expect(OAuthText).toBeInTheDocument()
 
-    const userPassAuthText = screen.getByText(/Username\/Password/i)
+    const userPassAuthText = screen.getByText('Username / Password')
     expect(userPassAuthText).toBeInTheDocument()
 })
 
-test('Check textual content, with user action', () => {
+test('x509 auth flow', () => {
     render(
         <BrowserRouter>
             <Login />
         </BrowserRouter>,
     )
-    const userPassButton = screen.findAllByLabelText('Username/Password')
-    expect(userPassButton).toBeInTheDocument()
+    const x509Button = screen.getByText('x509 Certificate')
+    expect(x509Button.getAttribute('type')).toEqual('submit')
+
+    act(() => {
+        x509Button.click()
+    })
+
+    expect(() => act(() => x509Button.click())).not.toThrow()
+})
+
+test('OIDC auth flow', () => {
+    render(
+        <BrowserRouter>
+            <Login />
+        </BrowserRouter>,
+    )
+    const OIDCButton = screen.getByText('OIDC OAuth')
+    expect(OIDCButton.getAttribute('type')).toEqual('submit')
+
+    act(() => {
+        OIDCButton.click()
+    })
+
+    expect(() => act(() => OIDCButton.click())).not.toThrow()
+})
+
+test('Userpass auth flow', () => {
+    render(
+        <BrowserRouter>
+            <Login />
+        </BrowserRouter>,
+    )
+    const userPassButton = screen.getByText('Username / Password')
+
+    act(() => {
+        userPassButton.click()
+    })
+
+    const userPassUsernameInput = screen.getByPlaceholderText('Enter Username')
+    expect(userPassUsernameInput).toBeInTheDocument()
+    expect(userPassUsernameInput.innerHTML.length).toEqual(0)
+
+    const userPassPasswordInput = screen.getByPlaceholderText('Enter Password')
+    expect(userPassPasswordInput).toBeInTheDocument()
+    expect(userPassPasswordInput.innerHTML.length).toEqual(0)
+
+    const userPassSignInButton = screen.getByText('Sign In')
+    expect(userPassSignInButton).toBeInTheDocument()
+    expect(userPassSignInButton.getAttribute('type')).toEqual('button')
+    expect(userPassSignInButton.getAttribute('aria-disabled')).toEqual('true')
+
+    fireEvent.change(userPassUsernameInput, { target: { value: 'ddmlab' } })
+    fireEvent.change(userPassPasswordInput, { target: { value: 'secret' } })
+
+    expect(userPassSignInButton.getAttribute('type')).toEqual('submit')
+    expect(userPassSignInButton.getAttribute('aria-disabled')).toEqual('false')
+
+    expect(() => act(() => userPassSignInButton.click())).not.toThrow()
 })
