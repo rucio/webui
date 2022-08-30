a0815ae6402d8695db387fb1a01bfcb1a0955ece
maany <imptodefeat@gmail.com>
2022-08-30 13:33:58 +0200

    breadcrums for handling readable stream

--- src/utils/restApiWrapper.ts
+++ src/utils/restApiWrapper.ts
@@ -76,3 +76,103 @@ export async function deleteData(
     })
     return response
 }
+
+export async function streamData(
+    endpoint: string | undefined = '/',
+    headers: HeadersInit | undefined = {},
+    data: unknown = {},
+    host: string | undefined = defaultHostEndpoint,
+): Promise<any> {
+    const response = await fetch(host + endpoint, {
+        method: 'GET',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/x-json-stream'
+        },
+    })
+    const reader = response.body?.getReader()
+    // reader?.read().then(function processResult(result): Promise<ReadableStream> | undefined {
+    //     if (result.done) {
+    //         console.log('Stream complete')
+    //     } else {
+    //         console.log(result.value)
+    //         return reader?.read().then(processResult)
+    //     }
+    // })
+    function processStreamResponse(result:any, ret_data: any[]): any[] | undefined {
+        if (result.done) {
+            console.log('Stream complete')
+            return undefined
+        } else {
+            if (result.value !== undefined){
+                const data = String.fromCharCode.apply(null, Array.from(result.value))
+                const split_data = data.split('\n');
+                
+                for (let i = 0; i < split_data.length; i++) {
+                    if (split_data[i].length <= 0) {
+                        break;
+                    }
+                    ret_data.push(JSON.parse(split_data[i].replace(/Infinity/g, "-1")));
+                }
+                console.log(ret_data);
+            }
+            return ret_data
+        }
+    }
+    const streamReader = reader?.read()
+    streamReader
+    // function processData(stream:any): Promise<ReadableStream> | undefined {
+    //     const ret_data: any[] = [];
+    //     const data = stream.read()
+    //     data.then(function processResult(result:any): Promise<ReadableStream> | undefined {
+    //         if (result.done ) {
+    //             console.log('Stream complete')
+    //         } else {
+    //             if (result.value !== undefined){
+    //                 const data = String.fromCharCode.apply(null, Array.from(result.value))
+    //                 const split_data = data.split('\n');
+                    
+    //                 for (let i = 0; i < split_data.length; i++) {
+    //                     if (split_data[i].length <= 0) {
+    //                         break;
+    //                     }
+    //                     ret_data.push(JSON.parse(split_data[i].replace(/Infinity/g, "-1")));
+    //                 }
+    //                 console.log(ret_data);
+    //             }
+    //             return processData(stream)
+            
+    //             //     const arr = JSON.stringify(result.value, (k, v: any) => {
+    //             //         if (v !== null && v !== undefined && v instanceof Uint8Array) {
+    //             //             console.log(v)
+    //             //             return String.fromCharCode.apply(null, Array.from(result.value))
+    //             //         }
+    //             //         return v
+    //             //     })
+    //             //     result = String.fromCharCode.apply(null, Array.from(result.value))
+    //             //     for (data of result){
+    //             //         console.log(data)
+    //             //     }
+    //             // }
+    //             return processResult(stream)
+    //         }
+    //     })
+    // }
+    // processData(reader)
+
+    // reader?.read().then(({ value, done }) => {
+    //     if (done) {
+    //         console.log('Stream complete')
+    //     } else {
+    //         console.log(String.fromCharCode.apply(null, Array.from(value)))
+    //         // return reader?.read().then(processResult)
+    //     }
+    // })
+    // console.log('Recieved all data')
+    // .then(response => response.body?.getReader())
+    // .then(reader => ({
+    //     const {value, done} = await reader.read()
+    // })
+
+    return response
+}
