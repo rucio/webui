2697928335b706aed9fc3546b09751d718c60076
elizabethh777 <vedzyu@gmail.com>
2022-09-02 12:09:31 +0200

    New Card element from bulma

--- src/stories/Card/Card.tsx
+++ src/stories/Card/Card.tsx
@@ -1,28 +1,50 @@
 import './card.scss'
+import {Image} from "../Image/Image";
+import {ReactElement} from "react";
 
 interface CardProps {
-    title?: string
-    type?: 'condensed' | 'spacious' | 'blue' | 'danger'
-    background?: string
-    children?: any
+    img?:boolean
+    img_src?:any
+    header?:string
+    content?:ReactElement
+    footer?:Array<any>
 }
 
 export const Card = ({
-    title = 'Sample Card',
-    background,
-    type = 'spacious',
-    children,
+    img=false,
+    img_src,
+    header='',
+    content,
+    footer=[],
     ...props
 }: CardProps) => {
     return (
-        <div
-            className={`rucio-card ${type}`}
-            style={{ backgroundColor: background }}
-        >
-            <div className="header">
-                <h3 className="title">{title}</h3>
+        <div className="rucio-card">
+            {img ? (
+                <div className={"image"}>
+                    <figure className="image is-4by3">
+                        <Image src={img_src} height={960} width={1280}/>
+                    </figure>
+                </div>
+            ):null}
+            { header.length > 0 ?(
+                <header className={"header"}>
+                    <p className={"header-title"}>{header}</p>
+                </header>
+            ):null}
+            <div className="card-content">
+                <div className="content">
+                    {content}
+                </div>
             </div>
-            {children}
+            {footer.length > 0 ?(
+                <footer className={"card-footer"}>
+                    {footer.map(element=> {
+                        return (
+                            <a href={element[0]} className="card-footer-item">{element[1]}</a>
+                        )})}
+                </footer>
+            ):null}
         </div>
     )
 }
