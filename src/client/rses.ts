c3b0bd7ec533e53e59151baac86ebb7b9b2adf2f
elizabethh777 <vedzyu@gmail.com>
2022-09-23 14:37:34 +0200

    ran prettier

--- src/client/rses.ts
+++ src/client/rses.ts
@@ -1,8 +1,9 @@
-import { streamData } from "../utils/restApiWrapper"
+import { streamData } from '../utils/restApiWrapper'
 
-export function list_rses(expression:boolean){
-    let url='/rses/'
-    if (expression===true) 
-        {url+='?expression=' + encodeURIComponent(expression)}
+export function list_rses(expression: boolean) {
+    let url = '/rses/'
+    if (expression === true) {
+        url += '?expression=' + encodeURIComponent(expression)
+    }
     return streamData(url)
 }
