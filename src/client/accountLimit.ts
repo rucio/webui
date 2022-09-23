0a3a67bd9ad8b7fdb7aa660eee882e6027ea40be
elizabethh777 <vedzyu@gmail.com>
2022-09-23 14:19:10 +0200

    added 2 modes in the models.ts (DIDModel & RSEModel), whihc are used in rule deifnition page. Additionaly added client functionaity for accounts, accountlimits, dids and rses

--- src/client/accountLimit.ts
+++ src/client/accountLimit.ts
@@ -1,1 +1,6 @@
-export {}
+import { streamData } from "../utils/restApiWrapper"
+
+export function get_account_limits(account:string){
+    const url='/accounts/' + account + '/limits'
+    return streamData(url,)
+}
--- src/client/dids.ts
+++ src/client/dids.ts
@@ -1,1 +1,16 @@
-export {}
+import { streamData } from "../utils/restApiWrapper"
+
+export function search(scope: string, name: string, type: string) {
+    const url = "/dids/" + scope + "/dids/search"
+    return streamData(url, {
+            "name": name,
+            "type": type
+        },
+    )
+}
+
+export function get_did(scope:string, name:string, dynamic:boolean){
+    let url = "/dids/" + scope + '/' + name
+    if (dynamic===true) {url+='?dynamic=' + dynamic}
+    return streamData(url)
+}
