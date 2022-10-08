a81e19fb86b873af0d8c1647c549b4fb73600ca9
nimishbongale <nimishnb98@gmail.com>
2022-10-08 17:21:31 +0530

    UI refactor for RuleDef Page

--- src/stories/ToggleSwitch/ToggleSwitch.tsx
+++ src/stories/ToggleSwitch/ToggleSwitch.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './toggleswitch.scss'
 
 interface ToggleSwitchProps {
@@ -5,26 +6,33 @@ interface ToggleSwitchProps {
     kind?: 'danger' | 'warning' | 'success' | 'info'
     size?: 'small' | 'normal' | 'medium' | 'large'
     style?: 'rounded' | 'outlined'
-    checked: boolean
-    handleChange: (event: any) => void
+    checked?: boolean
+    handleChange?: (event: any) => void
 }
 
 export const ToggleSwitch = ({
     label = '',
-    kind,
+    kind = 'success',
     size = 'normal',
-    style,
+    style = 'rounded',
+    checked = false,
+    handleChange = args => args,
     ...props
 }: ToggleSwitchProps) => {
+    const [checkedState, setCheckedState] = useState(checked)
     return (
-        <>
+        <div
+            onClick={(event: any) => {
+                setCheckedState(!checkedState)
+                handleChange(event)
+            }}
+        >
             <input
                 className={`rucio-switch ${kind} ${size} ${style}`}
                 type="checkbox"
-                checked={props.checked}
-                onChange={props.handleChange}
+                checked={checkedState}
             />
             <label>{label}</label>
-        </>
+        </div>
     )
 }
