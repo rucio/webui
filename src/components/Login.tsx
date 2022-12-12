52979aaf0e1b8f5523740e190f1a72a8726ff4be
George Matthews <george.matthews@stfc.ac.uk>
2022-12-12 16:55:16 +0000

    Update OIDC login
    Add ability for more that 3 oidc providers on login
    Add environment variables for oidc providers

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -68,6 +68,38 @@ export const Login = ({ onLoginSuccess }: any) => {
         </div>
     )
 
+    const oidcButtons = (provider: string, vo = '') => (
+        <div key={provider} className="rucio-flex-item">
+            <Button
+                icon={
+                    <svg
+                        className="octicon"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        width="16"
+                        height="16"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z"
+                        ></path>
+                    </svg>
+                }
+                type="submit"
+                kind="outline"
+                label={provider}
+                show="invisible"
+                size="large"
+                onClick={() => {
+                    authType.current = 'OAuth'
+                    oidcProvider.current = vo
+                        ? `oidc_provider_${provider}_${vo}`
+                        : `oidc_provider_${provider}`
+                }}
+            />
+        </div>
+    )
+
     const loginNavigateHome = (account: string) => {
         showAlert({
             message: 'Login successful!',
@@ -290,153 +322,22 @@ export const Login = ({ onLoginSuccess }: any) => {
                             )}
 
                             <Form title="" subtitle="" onSubmit={handleSubmit}>
-                                <div className="rucio-flex m-b-20">
-                                    {(
+                                <div className="rucio-flex m-b-20 flex-w">
+                                    {env(
                                         multi_vo
-                                            ? env(
-                                                  'oidc_provider_1_' +
-                                                      selectedVO,
-                                              ) ?? ''
-                                            : env('oidc_provider_1') ?? ''
-                                    ) ? (
-                                        <div className="rucio-flex-item">
-                                            <Button
-                                                icon={
-                                                    <svg
-                                                        className="octicon"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 16 16"
-                                                        width="16"
-                                                        height="16"
-                                                    >
-                                                        <path
-                                                            fillRule="evenodd"
-                                                            d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z"
-                                                        ></path>
-                                                    </svg>
-                                                }
-                                                type="submit"
-                                                kind="outline"
-                                                label={
-                                                    multi_vo
-                                                        ? env(
-                                                              'oidc_provider_1_' +
-                                                                  selectedVO,
-                                                          ) ?? ''
-                                                        : env(
-                                                              'oidc_provider_1',
-                                                          ) ?? ''
-                                                }
-                                                show="invisible"
-                                                size="large"
-                                                onClick={() => {
-                                                    authType.current = 'OAuth'
-                                                    oidcProvider.current =
-                                                        multi_vo
-                                                            ? '1_' + selectedVO
-                                                            : '1'
-                                                }}
-                                            />
-                                        </div>
-                                    ) : null}
-                                    {(
-                                        multi_vo
-                                            ? env(
-                                                  'oidc_provider_2_' +
-                                                      selectedVO,
-                                              ) ?? ''
-                                            : env('oidc_provider_2') ?? ''
-                                    ) ? (
-                                        <div className="rucio-flex-item">
-                                            <Button
-                                                icon={
-                                                    <svg
-                                                        className="octicon"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 16 16"
-                                                        width="16"
-                                                        height="16"
-                                                    >
-                                                        <path
-                                                            fillRule="evenodd"
-                                                            d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z"
-                                                        ></path>
-                                                    </svg>
-                                                }
-                                                type="submit"
-                                                kind="outline"
-                                                label={
-                                                    multi_vo
-                                                        ? env(
-                                                              'oidc_provider_2_' +
-                                                                  selectedVO,
-                                                          ) ?? ''
-                                                        : env(
-                                                              'oidc_provider_2',
-                                                          ) ?? ''
-                                                }
-                                                show="invisible"
-                                                size="large"
-                                                onClick={() => {
-                                                    authType.current = 'OAuth'
-                                                    oidcProvider.current =
-                                                        multi_vo
-                                                            ? '2_' + selectedVO
-                                                            : '2'
-                                                }}
-                                            />
-                                        </div>
-                                    ) : null}
-                                    {(
-                                        multi_vo
-                                            ? env(
-                                                  'oidc_provider_3_' +
-                                                      selectedVO,
-                                              ) ?? ''
-                                            : env('oidc_provider_3') ?? ''
-                                    ) ? (
-                                        <div className="rucio-flex-item">
-                                            <Button
-                                                icon={
-                                                    <svg
-                                                        className="octicon"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 16 16"
-                                                        width="16"
-                                                        height="16"
-                                                    >
-                                                        <path
-                                                            fillRule="evenodd"
-                                                            d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z"
-                                                        ></path>
-                                                    </svg>
-                                                }
-                                                type="submit"
-                                                kind="outline"
-                                                label={
-                                                    env(
-                                                        multi_vo
-                                                            ? env(
-                                                                  'oidc_provider_3_' +
-                                                                      selectedVO,
-                                                              ) ?? ''
-                                                            : env(
-                                                                  'oidc_provider_3',
-                                                              ) ?? '',
-                                                    ) as string
-                                                }
-                                                show="invisible"
-                                                size="large"
-                                                onClick={() => {
-                                                    authType.current = 'OAuth'
-                                                    oidcProvider.current =
-                                                        multi_vo
-                                                            ? '3_' + selectedVO
-                                                            : '3'
-                                                }}
-                                            />
-                                        </div>
-                                    ) : null}
+                                            ? `oidc_providers_${selectedVO}`
+                                            : `oidc_providers`,
+                                    )
+                                        ?.replace(/\s+/g, '')
+                                        .split(',')
+                                        .map(provider =>
+                                            oidcButtons(
+                                                provider,
+                                                multi_vo
+                                                    ? selectedVO
+                                                    : undefined,
+                                            ),
+                                        )}
                                 </div>
 
                                 <Button
