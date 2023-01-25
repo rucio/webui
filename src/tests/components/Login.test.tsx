6528a144ff74c72d912968da910e5b3566e7627a
George Matthews <112955466+gmatthews20@users.noreply.github.com>
2023-01-25 11:24:58 +0000

    Multi vo login tests (#123)
    * Add VO tab tests
    
    Add VO tab tests to Login page test
    Add .env.test
    Update test script to use .env.test
    
    * Update autotests
    
    Include test for multi-vo functionality
    
    * Remove NODE_ENV
    
    Setting NODE_ENV to test is not required
    
    * Remove redundant tests
    
    remove toBeInTheDocument tests since getByText would error if not in document
    
    * Remove testId
    
    Change get by from testid to parentelement
    
    * Remove SignIn button test
    
    * Run prettier
    
    * Revert to testid
    
    Revert to testid because of error

--- src/tests/components/Login.test.tsx
+++ src/tests/components/Login.test.tsx
@@ -3,6 +3,7 @@ import Login from '../../components/Login'
 import { BrowserRouter } from 'react-router-dom'
 import { ServiceProvider } from '../../components/GlobalHooks'
 import { ReactElement } from 'react'
+import { env } from '../../util'
 
 window.scrollTo = jest.fn()
 
@@ -14,51 +15,88 @@ const renderComponent: ReactElement = (
     </ServiceProvider>
 )
 
-test('Login page render', () => {
-    render(renderComponent)
-    const loginPrimaryText = screen.getByText('Rucio Login')
-    expect(loginPrimaryText).toBeInTheDocument()
+const multi_vo = env('multi_vo_enabled')?.toLowerCase() === 'true'
 
-    const loginSecondaryText = screen.getByText('Welcome to Rucio!')
-    expect(loginSecondaryText).toBeInTheDocument()
+const itif = (condition: boolean) => (condition ? it : it.skip)
 
-    const x509PassAuthText = screen.getByText('x509 Certificate')
-    expect(x509PassAuthText).toBeInTheDocument()
-
-    const userPassAuthText = screen.getByText('Username / Password')
-    expect(userPassAuthText).toBeInTheDocument()
-})
-
-test('Userpass auth flow', () => {
-    render(renderComponent)
-    const userPassButton = screen.getByText('Username / Password')
-
-    act(() => {
-        userPassButton.click()
+describe('Login page render', () => {
+    test('Titles', () => {
+        render(renderComponent)
+        const loginPrimaryText = screen.getByText('Rucio Login')
+        const loginSecondaryText = screen.getByText('Welcome to Rucio!')
     })
 
-    const userPassUsernameInput = screen.getByPlaceholderText('Enter Username')
-    expect(userPassUsernameInput).toBeInTheDocument()
-    expect(userPassUsernameInput.innerHTML.length).toEqual(0)
+    itif(multi_vo)('Multi-vo Tabs Enabled', () => {
+        render(renderComponent)
 
-    const userPassPasswordInput = screen.getByPlaceholderText('Enter Password')
-    expect(userPassPasswordInput).toBeInTheDocument()
-    expect(userPassPasswordInput.innerHTML.length).toEqual(0)
+        const voTab1 = screen.getByTestId('Tab0')
+        const voTab2 = screen.getByTestId('Tab1')
+        const voTab3 = screen.getByTestId('Tab2')
 
-    const userPassSignInButton = screen.getByText('Sign In')
-    expect(userPassSignInButton).toBeInTheDocument()
-    expect(userPassSignInButton.getAttribute('type')).toEqual('button')
-    expect(userPassSignInButton.getAttribute('aria-disabled')).toEqual('true')
+        expect(voTab1.getAttribute('class')).toEqual('is-active')
+        expect(voTab2.getAttribute('class')).toEqual(null)
+        expect(voTab3.getAttribute('class')).toEqual(null)
 
-    fireEvent.change(userPassUsernameInput, { target: { value: 'ddmlab' } })
-    fireEvent.change(userPassPasswordInput, { target: { value: 'secret' } })
+        expect(() =>
+            act(() => screen.getByText('Operations').click()),
+        ).not.toThrow()
 
-    const userPassSignInButtonEnabled = screen.getByText('Sign In')
-    expect(userPassSignInButtonEnabled).toBeInTheDocument()
-    expect(userPassSignInButtonEnabled.getAttribute('type')).toEqual('submit')
-    expect(userPassSignInButtonEnabled.getAttribute('aria-disabled')).toEqual(
-        'false',
-    )
+        expect(voTab1.getAttribute('class')).toEqual(null)
+        expect(voTab2.getAttribute('class')).toEqual('is-active')
+        expect(voTab3.getAttribute('class')).toEqual(null)
 
-    expect(() => act(() => userPassSignInButtonEnabled.click())).not.toThrow()
+        expect(() => act(() => screen.getByText('Dteam').click())).not.toThrow()
+
+        expect(voTab1.getAttribute('class')).toEqual(null)
+        expect(voTab2.getAttribute('class')).toEqual(null)
+        expect(voTab3.getAttribute('class')).toEqual('is-active')
+
+        expect(() => act(() => screen.getByText('ATLAS').click())).not.toThrow()
+
+        expect(voTab1.getAttribute('class')).toEqual('is-active')
+        expect(voTab2.getAttribute('class')).toEqual(null)
+        expect(voTab3.getAttribute('class')).toEqual(null)
+    })
+
+    test('Login Options', () => {
+        render(renderComponent)
+        const x509PassAuthText = screen.getByText('x509 Certificate')
+        const userPassAuthText = screen.getByText('Username / Password')
+    })
+})
+
+describe('Auth workflow', () => {
+    test('Userpass', () => {
+        render(renderComponent)
+        const userPassButton = screen.getByText('Username / Password')
+
+        act(() => {
+            userPassButton.click()
+        })
+
+        const userPassUsernameInput =
+            screen.getByPlaceholderText('Enter Username')
+        expect(userPassUsernameInput.innerHTML.length).toEqual(0)
+
+        const userPassPasswordInput =
+            screen.getByPlaceholderText('Enter Password')
+        expect(userPassPasswordInput.innerHTML.length).toEqual(0)
+
+        const userPassSignInButton = screen.getByText('Sign In')
+        expect(userPassSignInButton.getAttribute('type')).toEqual('button')
+        expect(userPassSignInButton.getAttribute('aria-disabled')).toEqual(
+            'true',
+        )
+
+        fireEvent.change(userPassUsernameInput, { target: { value: 'ddmlab' } })
+        fireEvent.change(userPassPasswordInput, { target: { value: 'secret' } })
+
+        const userPassSignInButtonEnabled = screen.getByText('Sign In')
+        expect(userPassSignInButtonEnabled.getAttribute('type')).toEqual(
+            'submit',
+        )
+        expect(
+            userPassSignInButtonEnabled.getAttribute('aria-disabled'),
+        ).toEqual('false')
+    })
 })
