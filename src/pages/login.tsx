506a7bf8ac0626fdd4982c18b63b453d0818a4a4
maany <imptodefeat@gmail.com>
2023-02-02 10:34:23 +0100

    add login page and dashboard

---
+++ src/pages/login.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { NextPage } from 'next';
+
+const Login: NextPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Add your authentication logic here
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
