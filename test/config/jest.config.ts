aabf71cb1605cd97243c5176a490c3234f0f1486
maany <imptodefeat@gmail.com>
2023-02-17 10:53:35 +0100

    move jest config to /tests/config

--- test/config/jest.config.ts
+++ test/config/jest.config.ts
@@ -9,6 +9,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
+  rootDir: '../../',
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/test/config/jest.setup.ts'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
