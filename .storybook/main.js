8cc270ede509889baee138197639b294a3cb9773
Anton Schwarz <anton.schwarz@cern.ch>
2023-02-17 15:09:10 +0100

    Upgrade to Storybook v7 (beta)
    Moved to Storybook `7.0.0-beta.49`. Had to add `util` to `resolve.fallback`, installed `util`.
    
    In upgrading, accepted all suggested migrations except for `autodocs:
    true`. These are:
    * `storybook-binary`
    * `sb-scripts`
    * `newFrameworks` (which removes webpack5)
    * `nextjsFramework` (which replaces `newFrameworks` with one specific to
      `nextjs`
    * `bare-mdx-stories-glob`

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,20 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
+  stories: [
+    "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-scss"
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: {
+    name: "@storybook/nextjs",
+    options: {}
+  },
+  resolve: {
+    fallback: {
+      util: require.resolve("util")
+    }
+  },
+};
