1a06f28f51e7917dd565bbb93fee5771c66c72d4
maany <imptodefeat@gmail.com>
2023-03-02 19:43:02 +0100

    add environment variable specific functions for env gateway #137

--- src/lib/core/port/secondary/env-config-gateway-output-port.ts
+++ src/lib/core/port/secondary/env-config-gateway-output-port.ts
@@ -1,7 +1,15 @@
+import { OIDCProvider, VO } from "../../entity/auth-models"
+
 export default interface EnvConfigGatewayOutputPort {
     /**
      * Returns the value of the environment variable with the given key if it exists. Returns undefined otherwise.
+     * p.s. There is no special Data Transfer Object (DTO) for this use case.
      * @param key The key of the environment variable
      */
     get(key: string): Promise<string | undefined>
+    oidcEnabled(): Promise<boolean>
+    oidcProviders(): Promise<OIDCProvider[]>
+    multiVOEnabled(): Promise<boolean>
+    voList(): Promise<VO[]>
+    x509Enabled(): Promise<boolean>
 }
--- test/gateway/env-config-gateway.test.ts
+++ test/gateway/env-config-gateway.test.ts
@@ -1,3 +1,4 @@
+import { InvalidConfig } from "@/lib/core/data/env-config-exceptions"
 import EnvConfigGatewayOutputPort from "@/lib/core/port/secondary/env-config-gateway-output-port"
 import appContainer from "@/lib/infrastructure/config/ioc/container-config"
 import GATEWAYS from "@/lib/infrastructure/config/ioc/ioc-symbols-gateway"
@@ -18,5 +19,164 @@ describe('env-config-gateway', () => {
         const gateway = appContainer.get<EnvConfigGatewayOutputPort>(GATEWAYS.ENV_CONFIG)
         const result = await gateway.get(key)
         expect(result).toBeUndefined()
+    })
+    it('should return x509 enabled by default', async () => {
+        const gateway = appContainer.get<EnvConfigGatewayOutputPort>(GATEWAYS.ENV_CONFIG)
+        const result = await gateway.x509Enabled()
+        expect(result).toBe(true)
+    })
+    it('should return x509 enabled if the environment variable is true', async () => {
+        const key = 'X509_ENABLED'
+        const value = 'true'
+        process.env[key] = value
+        const gateway = appContainer.get<EnvConfigGatewayOutputPort>(GATEWAYS.ENV_CONFIG)
+        const result = await gateway.x509Enabled()
+        expect(result).toBe(true)
+    })
+    it('should return x509 disabled if the environment variable is false', async () => {
+        const key = 'X509_ENABLED'
+        const value = 'false'
+        process.env[key] = value
+        const gateway = appContainer.get<EnvConfigGatewayOutputPort>(GATEWAYS.ENV_CONFIG)
+        const result = await gateway.x509Enabled()
+        expect(result).toBe(false)
+    })        
+})
+
+
+describe('env-config-gateway valid oidc config test', () => {
+    beforeEach(() => {
+        process.env['OIDC_ENABLED'] = 'true'
+        process.env['OIDC_PROVIDERS'] = 'cern, test-provider'
+        process.env['OIDC_PROVIDER_CERN_URL'] = 'https://cern.ch'
+        process.env['OIDC_PROVIDER_CERN_ICON_URL'] = 'https://cern.ch/icon.png'
+        process.env['OIDC_PROVIDER_CERN_CLIENT_ID'] = 'client-id'
+        process.env['OIDC_PROVIDER_CERN_CLIENT_SECRET'] = 'client-secret'
+        process.env['OIDC_PROVIDER_CERN_SCOPES'] = 'scope1,scope2'
+        process.env['OIDC_PROVIDER_CERN_AUTHORIZATION_URL'] = 'https://cern.ch/auth'
+        process.env['OIDC_PROVIDER_CERN_TOKEN_URL'] = 'https://cern.ch/token'
+        process.env['OIDC_PROVIDER_CERN_USERINFO_URL'] = 'https://cern.ch/userinfo'
+        process.env['OIDC_PROVIDER_CERN_REDIRECT_URL'] = 'https://cern.ch/redirect'
+        process.env['OIDC_PROVIDER_CERN_REFRESH_TOKEN_URL'] = 'https://cern.ch/userinfo'
+        process.env['OIDC_PROVIDER_CERN_LOGOUT_URL'] = 'https://cern.ch/logout'
+
+        process.env['OIDC_PROVIDER_TEST-PROVIDER_URL'] = 'https://test-provider.ch'
+        process.env['OIDC_PROVIDER_TEST-PROVIDER_ICON_URL'] = 'https://test-provider.ch/icon.png'
+        process.env['OIDC_PROVIDER_TEST-PROVIDER_CLIENT_ID'] = 'client-id'
+        process.env['OIDC_PROVIDER_TEST-PROVIDER_CLIENT_SECRET'] = 'client-secret'
+        process.env['OIDC_PROVIDER_TEST-PROVIDER_SCOPES'] = 'scope1,scope2'
+        process.env['OIDC_PROVIDER_TEST-PROVIDER_AUTHORIZATION_URL'] = 'https://test-provider.ch/auth'
+        process.env['OIDC_PROVIDER_TEST-PROVIDER_TOKEN_URL'] = 'https://test-provider.ch/token'
+        process.env['OIDC_PROVIDER_TEST-PROVIDER_USERINFO_URL'] = 'https://test-provider.ch/userinfo'
+        process.env['OIDC_PROVIDER_TEST-PROVIDER_REDIRECT_URL'] = 'https://test-provider.ch/redirect'
+        process.env['OIDC_PROVIDER_TEST-PROVIDER_REFRESH_TOKEN_URL'] = 'https://test-provider.ch/userinfo'
+        process.env['OIDC_PROVIDER_TEST-PROVIDER_LOGOUT_URL'] = 'https://test-provider.ch/logout'
+
+    })
+    afterEach(() => {
+        delete process.env['OIDC_ENABLED']
+        delete process.env['OIDC_PROVIDERS']
+        delete process.env['OIDC_PROVIDER_CERN_URL']
+        delete process.env['OIDC_PROVIDER_CERN_ICON_URL']
+        delete process.env['OIDC_PROVIDER_CERN_CLIENT_ID']
+        delete process.env['OIDC_PROVIDER_CERN_CLIENT_SECRET']
+        delete process.env['OIDC_PROVIDER_CERN_SCOPES']
+        delete process.env['OIDC_PROVIDER_CERN_AUTHORIZATION_URL']
+        delete process.env['OIDC_PROVIDER_CERN_TOKEN_URL']
+        delete process.env['OIDC_PROVIDER_CERN_USERINFO_URL']
+        delete process.env['OIDC_PROVIDER_CERN_REDIRECT_URL']
+        delete process.env['OIDC_PROVIDER_CERN_REFRESH_TOKEN_URL']
+        delete process.env['OIDC_PROVIDER_CERN_LOGOUT_URL']
+
+        delete process.env['OIDC_PROVIDER_TEST-PROVIDER_URL']
+        delete process.env['OIDC_PROVIDER_TEST-PROVIDER_ICON_URL']
+        delete process.env['OIDC_PROVIDER_TEST-PROVIDER_CLIENT_ID']
+        delete process.env['OIDC_PROVIDER_TEST-PROVIDER_CLIENT_SECRET']
+        delete process.env['OIDC_PROVIDER_TEST-PROVIDER_SCOPES']
+        delete process.env['OIDC_PROVIDER_TEST-PROVIDER_AUTHORIZATION_URL']
+        delete process.env['OIDC_PROVIDER_TEST-PROVIDER_TOKEN_URL']
+        delete process.env['OIDC_PROVIDER_TEST-PROVIDER_USERINFO_URL']
+        delete process.env['OIDC_PROVIDER_TEST-PROVIDER_REDIRECT_URL']
+        delete process.env['OIDC_PROVIDER_TEST-PROVIDER_REFRESH_TOKEN_URL']
+        delete process.env['OIDC_PROVIDER_TEST-PROVIDER_LOGOUT_URL']
+    })
+    it('should return oidc enabled if the environment variable is true', async () => {
+        const gateway = appContainer.get<EnvConfigGatewayOutputPort>(GATEWAYS.ENV_CONFIG)
+        const result = await gateway.oidcEnabled()
+        expect(result).toBe(true)
+    })
+    it('should return oidc disabled if the environment variable is false', async () => {
+        process.env['OIDC_ENABLED'] = 'false'
+        const gateway = appContainer.get<EnvConfigGatewayOutputPort>(GATEWAYS.ENV_CONFIG)
+        const result = await gateway.oidcEnabled()
+        expect(result).toBe(false)
+    })
+    it('should return oidc providers', async () => {
+        const gateway = appContainer.get<EnvConfigGatewayOutputPort>(GATEWAYS.ENV_CONFIG)
+        const result = await gateway.oidcProviders()
+        expect(result.length).toEqual(2)
+        const cern = result[0]
+
+        expect(cern.name).toEqual('cern')
+        expect(cern.url).toEqual('https://cern.ch')
+        expect(cern.iconUrl).toEqual('https://cern.ch/icon.png')
+        expect(cern.clientId).toEqual('client-id')
+        expect(cern.clientSecret).toEqual('client-secret')
+        expect(cern.scopes).toEqual(['scope1', 'scope2'])
+        expect(cern.authorizationUrl).toEqual('https://cern.ch/auth')
+        expect(cern.tokenUrl).toEqual('https://cern.ch/token')
+        expect(cern.userInfoUrl).toEqual('https://cern.ch/userinfo')
+        expect(cern.redirectUrl).toEqual('https://cern.ch/redirect')
+        expect(cern.refreshTokenUrl).toEqual('https://cern.ch/userinfo')
+        
+    })
+
+})
+
+
+describe('env-config-gateway invalid oidc config test', () => {
+
+    it('should throw errors if invalid oidc config is provided', async () => {
+        const gateway = appContainer.get<EnvConfigGatewayOutputPort>(GATEWAYS.ENV_CONFIG)
+        const oidcEnabled = await gateway.oidcEnabled()
+        expect(oidcEnabled).toBe(false)
+        
+        process.env['OIDC_ENABLED'] = 'true'
+        process.env['OIDC_PROVIDERS'] = 'cern'
+
+        expect(await gateway.oidcEnabled()).toBe(true)
+
+        // expect(gateway.oidcProviders()).rejects.toThrow(InvalidConfig)
+        
+        process.env['OIDC_PROVIDER_CERN_URL'] = 'https://cern.ch'
+        process.env['OIDC_PROVIDER_CERN_ICON_URL'] = 'https://cern.ch/icon.png'
+        process.env['OIDC_PROVIDER_CERN_CLIENT_ID'] = 'client-id'
+        process.env['OIDC_PROVIDER_CERN_CLIENT_SECRET'] = 'client-secret'
+        process.env['OIDC_PROVIDER_CERN_SCOPES'] = 'scope1,scope2'
+        process.env['OIDC_PROVIDER_CERN_AUTHORIZATION_URL'] = 'https://cern.ch/auth'
+        process.env['OIDC_PROVIDER_CERN_TOKEN_URL'] = 'https://cern.ch/token'
+        process.env['OIDC_PROVIDER_CERN_USERINFO_URL'] = 'https://cern.ch/userinfo'
+        process.env['OIDC_PROVIDER_CERN_REDIRECT_URL'] = 'https://cern.ch/redirect'
+        process.env['OIDC_PROVIDER_CERN_REFRESH_TOKEN_URL'] = 'https://cern.ch/userinfo'
+        process.env['OIDC_PROVIDER_CERN_LOGOUT_URL'] = 'https://cern.ch/logout'
+
+        expect(await gateway.oidcProviders()).toHaveLength(1)
+
+        delete process.env['OIDC_ENABLED']
+        delete process.env['OIDC_PROVIDERS']
+        delete process.env['OIDC_PROVIDER_CERN_URL']
+        delete process.env['OIDC_PROVIDER_CERN_ICON_URL']
+        delete process.env['OIDC_PROVIDER_CERN_CLIENT_ID']
+        delete process.env['OIDC_PROVIDER_CERN_CLIENT_SECRET']
+        delete process.env['OIDC_PROVIDER_CERN_SCOPES']
+        delete process.env['OIDC_PROVIDER_CERN_AUTHORIZATION_URL']
+        delete process.env['OIDC_PROVIDER_CERN_TOKEN_URL']
+        delete process.env['OIDC_PROVIDER_CERN_USERINFO_URL']
+        delete process.env['OIDC_PROVIDER_CERN_REDIRECT_URL']
+        delete process.env['OIDC_PROVIDER_CERN_REFRESH_TOKEN_URL']
+        delete process.env['OIDC_PROVIDER_CERN_LOGOUT_URL']
+        
+
+        
     })
 })
