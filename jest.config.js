f86ea9b7a1d03f3c28d0f6aa214b9ebb89d9f3b7
maany <imptodefeat@gmail.com>
2023-03-02 14:40:49 +0100

    add jest and vscode config for gateway tests #137

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
     projects: [ 
       '<rootDir>/test/api/jest.api.config.js',
       '<rootDir>/test/component/jest.component.config.js',
+      '<rootDir>/test/gateway/jest.gateway.config.js',
     ],
   }
---
+++ test/gateway/jest.gateway.setup.ts
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom/extend-expect'
+import "reflect-metadata"
+import fetchMock from 'jest-fetch-mock'
+import "@inrupt/jest-jsdom-polyfills"
+fetchMock.enableMocks()
+
