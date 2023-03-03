6fd8f35239afe7a5e70dd23aa86e88ab2a1ae378
maany <imptodefeat@gmail.com>
2023-03-03 10:54:20 +0100

    add login-config-usecase #137

--- src/lib/core/use-case/login-config-usecase.ts
+++ src/lib/core/use-case/login-config-usecase.ts
@@ -1,4 +1,6 @@
 import { injectable } from "inversify";
+import { ConfigNotFound, InvalidConfig } from "../data/env-config-exceptions";
+import { LoginConfigResponse } from "../data/login-config";
 import type LoginConfigInputPort from "../port/primary/login-config-input-port";
 import type LoginConfigOutputPort from "../port/primary/login-config-output-port";
 import type EnvConfigGatewayOutputPort from "../port/secondary/env-config-gateway-output-port";
@@ -6,19 +8,53 @@ import type EnvConfigGatewayOutputPort from "../port/secondary/env-config-gatewa
 
 @injectable()
 class LoginConfigUseCase implements LoginConfigInputPort {
-    
+
     constructor(
         private presenter: LoginConfigOutputPort<any>,
         private envConfigGateway: EnvConfigGatewayOutputPort,
-    ){
+    ) {
         this.presenter = presenter;
         this.envConfigGateway = envConfigGateway;
     }
-    execute(): void {
-        const config = await Promise.all([
-            this.envConfigGateway.
-        ])
-        
+    async execute(): Promise<void> {
+        try {
+            const config = await Promise.all([
+                this.envConfigGateway.x509Enabled(),
+                this.envConfigGateway.oidcEnabled(),
+                this.envConfigGateway.multiVOEnabled(),
+                this.envConfigGateway.voList(),
+                this.envConfigGateway.oidcProviders(),
+            ])
+            const responseModel: LoginConfigResponse = {
+                x509Enabled: config[0],
+                oidcEnabled: config[1],
+                multiVOEnabled: config[2],
+                voList: config[3],
+                oidcProviders: config[4],
+            }
+            await this.presenter.presentSuccess(responseModel)
+            return
+        } catch (error) {
+            if (error instanceof ConfigNotFound) {
+                await this.presenter.presentError({
+                    type: 'ConfigNotFound',
+                    message: error.message,
+                })
+            }
+            else if (error instanceof InvalidConfig) {
+                await this.presenter.presentError({
+                    type: 'InvalidConfig',
+                    message: error.message,
+                })
+            }
+            else {
+                await this.presenter.presentError({
+                    type: 'UnknownError',
+                    message: 'An unknown error occurred while fetching the configuration of login page from the server.',
+                })
+            }
+            return
+        }
     }
 
 }
