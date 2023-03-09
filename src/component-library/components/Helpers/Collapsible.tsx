277f7c3fb9c825b4ea1dc8a63279004ab0e5dc10
maany <imptodefeat@gmail.com>
2023-03-09 19:49:32 +0100

    Alert component state management #151

--- src/component-library/components/Helpers/Collapsible.tsx
+++ src/component-library/components/Helpers/Collapsible.tsx
@@ -1,6 +1,6 @@
 export const Collapsible = (props: {showIf: boolean, id?: string, children: any,}) => {
     return (
-        <div className={props.showIf ? "" : "collapse"} data-testid={props.id? props.id: "" }>
+        <div className={props.showIf ? "" : "collapse"} data-testid={props.id? props.id: undefined }>
             {props.children}
         </div>
     )
