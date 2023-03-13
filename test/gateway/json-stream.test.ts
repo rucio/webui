a20408fa5fafe11a2b4d64ee9db080c847195a07
maany <imptodefeat@gmail.com>
2023-03-13 11:00:21 +0100

    stream list of objects #142

--- test/gateway/json-stream.test.ts
+++ test/gateway/json-stream.test.ts
@@ -28,8 +28,49 @@ describe('Streaming tests for JSON encoded text payloads', () => {
             }
             if (req.url.endsWith('nestedjsonstream') && req.method === 'GET') {
                 const stream = new PassThrough()
-                stream.write('{"RSE":')
-                stream.write('"World"}')
+                const nestedObject = {
+                    parent: {
+                        child: {
+                            grandchild: {
+                                key1: 'Value1',
+                                key2: 'Value2',
+                            },
+                        },
+                    },
+                }
+                const json = JSON.stringify(nestedObject)
+                const chunks = json.match(/.{1,10}/g) || []
+                for (const chunk of chunks) {
+                    stream.write(chunk)
+                }
+                stream.end()
+                return Promise.resolve({
+                    status: 200,
+                    body: stream,
+                })
+            }
+            if(req.url.endsWith('listofobjects')) {
+                const stream = new PassThrough()
+                const baseObject = {
+                    RSE: {
+                        name: 'RSE',
+                        info: {
+                            size: 100,
+                        }
+                    }                            
+                }
+                const listOfObjects = []
+                for(let i = 0; i < 10; i++) {
+                    listOfObjects.push(baseObject)
+                    baseObject.RSE.name = `RSE${i}`
+                    baseObject.RSE.info.size = i * 100
+                }
+                const json = JSON.stringify(listOfObjects)
+                // split the json into chunks of 10 characters
+                const chunks = json.match(/.{1,20}/g) || []
+                for (const chunk of chunks) {
+                    stream.write(chunk)
+                }
                 stream.end()
                 return Promise.resolve({
                     status: 200,
@@ -64,6 +105,7 @@ describe('Streaming tests for JSON encoded text payloads', () => {
             await new Promise(resolve => setTimeout(resolve, 1000))
         }
     })
+
     it('should return the mocked chunks as JSON', async () => {
         const url = 'http://localhost:8080/goodjsonstream'
         const streamingGateway = appContainer.get<StreamGatewayOutputPort>(GATEWAYS.STREAM)
@@ -87,5 +129,75 @@ describe('Streaming tests for JSON encoded text payloads', () => {
         while(outputStream.readableLength > 0) {
             await new Promise(resolve => setTimeout(resolve, 1000))
         }
+    })
+    it('should return nested json responses as JSON', async () => {
+        const url = 'http://localhost:8080/nestedjsonstream'
+        const streamingGateway = appContainer.get<StreamGatewayOutputPort>(GATEWAYS.STREAM)
+        const jsonStream: PassThrough | null = await streamingGateway.getJSONChunks(url)
+        expect(jsonStream).not.toBeNull()
+        // if response is null fail the test
+        if(jsonStream === null) {
+            fail('response is null')
+        }
+        let chunks:string[] = []
+        const outputStream = new PassThrough()
+        jsonStream.pipe(outputStream)
+
+        outputStream.on('data', (chunk) => {
+            chunks.push(chunk.toString())
+        })
+        outputStream.on('end', () => {
+            const json = JSON.parse(chunks.join(''))
+            expect(json).toEqual({
+                parent: {
+                    child: {
+                        grandchild: {
+                            key1: 'Value1',
+                            key2: 'Value2',
+                        },
+                    },
+                },
+            })
+        })
+        while(outputStream.readableLength > 0) {
+            await new Promise(resolve => setTimeout(resolve, 1000))
+        }
+    })
+    it('should return list of objects as JSON', async () => {
+        const url = 'http://localhost:8080/listofobjects'
+        const streamingGateway = appContainer.get<StreamGatewayOutputPort>(GATEWAYS.STREAM)
+        const jsonStream: PassThrough | null = await streamingGateway.getJSONChunks(url)
+        expect(jsonStream).not.toBeNull()
+        // if response is null fail the test
+        if(jsonStream === null) {
+            fail('response is null')
+        }
+        let chunks:string[] = []
+        const outputStream = new PassThrough()
+        jsonStream.pipe(outputStream)
+
+        outputStream.on('data', (chunk: string) => {
+            chunks.push(chunk.toString())
+        })
+        outputStream.on('end', () => {
+            const json = JSON.parse(chunks.join(''))
+            expect(json.length).toEqual(10)
+            expect(json[0]).toEqual({
+                RSE: {
+                    name: 'RSE0',
+                    info: {
+                        size: 0,
+                    },
+                }
+            })
+            expect(json[9]).toEqual({
+                RSE: {
+                    name: 'RSE9',
+                    info: {
+                        size: 900,
+                    },
+                }
+            })
+        })
     })
 })
