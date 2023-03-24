e096b572a40227f855eac0f119d3c79f45d35ff9
maany <imptodefeat@gmail.com>
2023-03-24 17:47:46 +0100

    client side rendering by manual component injection and hydration

--- src/app/stream/page.tsx
+++ src/app/stream/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 import React, { useState, useRef, useEffect, useMemo } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot, Root } from 'react-dom/client';
 import Head from 'next/head';
 import { Partytown } from '@builder.io/partytown/react';
+import ReactDOM from 'react-dom';
+import { debug } from 'util';
 
 export type Props = {
     columns: string[],
@@ -25,17 +27,60 @@ const Row = ({ data }: RowProps) => {
     )
 }
 const StreamingTable = () => {
-    const workerRef = useRef<Worker>();
     const tableBodyRef = useRef<HTMLTableSectionElement | null>(null);
+    const [tableBodyRootElement, setTableBodyRootElement] = useState<Root | null>(null);
+    
+    const dataQueue = useMemo(() => {
+        return new Array<RowData>();
+    }, []);
+    
     const [rows, setRows] = useState<RowData[]>([]);
     const [columns, setColumns] = useState<string[]>([]);
-    const [rootElement, setRootElement] = useState<HTMLElement | null>(null);
-    const dataRoot = useMemo(() => {
-        return rootElement ? createRoot(rootElement) : null;
-    }, [rootElement]);
 
     useEffect(() => {
-        const tableBody = document.getElementById('table-body');
+        if (tableBodyRootElement === null) {
+            console.log('tableBodyRootElement is null')
+            return;
+        }
+        console.log('tableBodyRootElement in other useEffect', tableBodyRootElement)
+        const rowElements = rows.map((row: RowData) => {
+            // eslint-disable-next-line react/jsx-key
+            return <Row data={row} />
+        })
+        tableBodyRootElement.render(rowElements);
+    }, [tableBodyRootElement, rows]);
+
+
+    useEffect(() => {
+        if(tableBodyRef === null) {
+            console.log('tableBodyRef is null')
+            return;
+        }
+        if (tableBodyRef.current === null) {
+            console.log('tableBodyRef.current is null')
+            return;
+        }
+        if  (tableBodyRef.current.id === '' || tableBodyRef.current.id === null) {
+            console.log('tableBodyRef.current.id is empty')
+            return;
+        }
+        const tableBody = document.getElementById(tableBodyRef.current.id);
+        if(tableBody === null) {
+            console.log('tableBody is null')
+            return;
+        }
+        const root = createRoot(tableBody);
+        if ( root === null) {
+            console.log('root is null')
+            return;
+        }
+        console.log('root', root)
+        console.log('Setting tableBodyRootElement')
+        setTableBodyRootElement(root);
+    }, []);
+
+
+    useEffect(() => {
         const initialColumns = ['id', 'name', 'email']
         const initialRows = [
             {
@@ -51,42 +96,27 @@ const StreamingTable = () => {
         ];
         setRows(initialRows);
         setColumns(initialColumns);
-        if(rootElement === null) {
-            setRootElement(tableBody);
-        }
-        console.log('Setting initial rows and root element')
-    }, [rootElement]);
+    }, []);
 
-    useEffect(() => {
-        // const tableBody = document.getElementById('table-body');
-        const tableBody = tableBodyRef.current;
-        if (tableBody === null) {
-            console.log('tableBody is null')
-            return;
-        }
-        if (dataRoot === null) {
-                console.log('dataRoot is null')
-                return;
-        }
-        // const dataRoot = createRoot(tableBody);
-        console.log('dataRoot', dataRoot)
-        console.log('rows', rows)
-        if (tableBody) {
-            tableBody.innerHTML = '';
-            rows.forEach((row: RowData) => {
-                const rowReactElement = <Row data={row} />
-                console.log('rowData', row)
-                dataRoot.render(rowReactElement);
-                // const rowElement = document.createElement('tr');
-                // Object.keys(row).forEach((key: string) => {
-                // const cell = document.createElement('td');
-                // cell.innerText = row[key];
-                // rowElement.appendChild(cell);
-                // });
-                // tableBody.appendChild(rowElement);
-            })
-        }
-    }, [rows, dataRoot]);
+    // useEffect(() => {
+    //     // const tableBody = document.getElementById('table-body');
+    //     const tableBody = tableBodyRef.current;
+    //     if (tableBody === null) {
+    //         console.log('tableBody is null')
+    //         return;
+    //     }
+
+    //     console.log('Available rows', rows)
+    
+    //     tableBody.innerHTML = '';
+    //     rows.forEach((row: RowData) => {
+    //         const rowReactElement = <Row data={row} />
+    //         console.log('Inserting rowData', row)
+    //         // add row to table body
+    //         tableBody.appendChild(ReactDOM.render(rowReactElement));
+    //     })
+        
+    // }, [rows, tableBodyRef]);
 
     return (
         <>
