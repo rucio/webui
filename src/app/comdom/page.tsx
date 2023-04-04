89e1f1407c76c4424a7f7d03840b0844b749b9bd
maany <imptodefeat@gmail.com>
2023-04-04 16:22:18 +0200

    disable lifecycle viewer #142

--- src/app/comdom/page.tsx
+++ src/app/comdom/page.tsx
@@ -94,7 +94,7 @@ export default function RSETable() {
                 <RSEQueryStatus status={query.fetchStatus}/>
                 <UseComDOMStatusCard status={status}/>
             </div>
-            <ComDOMLifeCycle/>
+            {/* <ComDOMLifeCycle workerStatus={comDOMStatus} queryStatus={query.fetchStatus}/> */}
 
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
