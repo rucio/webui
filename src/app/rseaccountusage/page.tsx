70ae7798d7acc5835ca87a3274fc9923e3a3528c
Anton Schwarz <anton.schwarz@cern.ch>
2023-04-06 18:41:36 +0200

    Select Row Works
    Selecting a row works in the new RSEQuotaTable (streamed). Selection
    works exclusively via then checkbox to the right, but I want to add
    tapping the row as well.
    
    Tanstack seems to be working against me. I hoped to use the tanstack API
    to save the selected spaces and then use `useEffect` linked to the
    tanstack table (or, even better, the selected rows within the table),
    but that just would not work. Hence, pressing the checkbox will update
    the react state and then update the tanstack state.
    
    I hope to find a better solution soon.

--- src/app/rseaccountusage/page.tsx
+++ src/app/rseaccountusage/page.tsx
@@ -2,52 +2,80 @@
 import { H3 } from "@/component-library/components/Text/Headings/H3"
 import { P } from "@/component-library/components/Text/Content/P"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import { RSEAccountUsageLimitDTO } from "@/lib/core/data/rucio-dto"
 import useComDOM from "@/lib/infrastructure/hooks/useComDOM"
 import { createColumnHelper, flexRender, getCoreRowModel, TableOptions, useReactTable } from "@tanstack/react-table"
+import { tab } from "@testing-library/user-event/dist/types/convenience"
 
 const columnHelper = createColumnHelper<RSEAccountUsageLimitDTO>()
 
-const columns: any[] = [
-    columnHelper.accessor('rse_id', {
-        id: 'rse_id',
-        header: 'ID',
-        cell: (info) => info.getValue(),
-    }),
-    columnHelper.accessor('rse', {
-        id: 'rse',
-        header: () => <H3>RSE Name</H3>,
-        cell: (info) => info.getValue(),
-    }),
-    columnHelper.accessor('used_bytes', {
-        id: 'used_bytes',
-        header: () => <H3>Used</H3>,
-        cell: (props) => {
-            // if value is greater than quota bytes, print in red
-            if (props.row.original.quota_bytes && props.row.original.quota_bytes < props.row.original.used_bytes) {
-                return <P className="text-red-500">{props.row.original.used_bytes}</P>
-            }
-            return <P>{props.row.original.used_bytes}</P>
-        },
-    }),
-    columnHelper.accessor('quota_bytes', {
-        id: 'quota_bytes',
-        header: () => <H3>Quota</H3>,
-    }),
-    columnHelper.accessor('used_files', {
-        id: 'used_files',
-        header: 'Files',
-    }),
-    columnHelper.accessor('account', {
-        id: 'account',
-        header: 'Account',
-    }),
-]
 
 
 export default function RSEAccountUsage() {
+    
+    const [selectedDIDs, setSelectedDIDs] = useState<string[]>([])
+    
+    const columns: any[] = [
+        {
+            id: 'selection',
+            header: "Select",
+            cell: (props) => {
+                return <input
+                    type="checkbox"
+                    disabled={
+                        // if quota_bytes is less than used_bytes, disable checkbox
+                        // TODO handle "askpermission" case
+                        (props.row.original.quota_bytes && props.row.original.quota_bytes < props.row.original.used_bytes) ? true : false
+                    }
+                    checked={props.row.getIsSelected()}
+                    onChange={(event: any) => {
+                        if (event.target.checked) {
+                            setSelectedDIDs([...selectedDIDs, props.row.original.rse_id])
+                        }
+                        else {
+                            setSelectedDIDs(selectedDIDs.filter((did) => did !== props.row.original.rse_id))
+                        }
+                        props.row.getToggleSelectedHandler()(event)
+                    }}
+                />
+            },
+        },
+        columnHelper.accessor('rse_id', {
+            id: 'rse_id',
+            header: 'ID',
+            cell: (info) => info.getValue(),
+        }),
+        columnHelper.accessor('rse', {
+            id: 'rse',
+            header: () => <H3>RSE Name</H3>,
+            cell: (info) => info.getValue(),
+        }),
+        columnHelper.accessor('used_bytes', {
+            id: 'used_bytes',
+            header: () => <H3>Used</H3>,
+            cell: (props) => {
+                // if value is greater than quota bytes, print in red
+                if (props.row.original.quota_bytes && props.row.original.quota_bytes < props.row.original.used_bytes) {
+                    return <P mono className="text-red-500 dark:text-red-500 font-bold">{props.row.original.used_bytes}</P>
+                }
+                return <P mono>{props.row.original.used_bytes}</P>
+            },
+        }),
+        columnHelper.accessor('quota_bytes', {
+            id: 'quota_bytes',
+            header: () => <H3>Quota</H3>,
+        }),
+        columnHelper.accessor('used_files', {
+            id: 'used_files',
+            header: 'Files',
+        }),
+        columnHelper.accessor('account', {
+            id: 'account',
+            header: 'Account',
+        }),
+    ]
     const {
         query,
         dataSink,
@@ -71,7 +99,7 @@ export default function RSEAccountUsage() {
         true
     )
     const [columnVisibility, setColumnVisibility] = useState(
-        { 
+        {
             account: false,
             used_files: false,
             rse_id: false,
@@ -86,6 +114,7 @@ export default function RSEAccountUsage() {
         columns: columns,
         getCoreRowModel: getCoreRowModel(),
         debugTable: true,
+        enableRowSelection: true,
         state: {
             columnVisibility: columnVisibility,
         }
@@ -105,6 +134,11 @@ export default function RSEAccountUsage() {
                 }}>Pause</button>
                 <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={resume}>Resume</button>
                 <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" onClick={clean}>Clean</button>
+                <button className="focus:outline-none text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-900" onClick={
+                    () => {
+                        console.info(table.getSelectedRowModel().flatRows.map(row => row.original.rse_id));
+                    }
+                }>Row Model</button>
                 <div className="flex flex-row">Poll Interval: {pollInterval}</div>
             </div>
             <table className="w-full text-left">
@@ -126,7 +160,7 @@ export default function RSEAccountUsage() {
                 </thead>
                 <tbody>
                     {table.getRowModel().rows.map(row => (
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={row.id}>
+                        <tr className="hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-900 w-full border-b hover:cursor-pointer" key={row.id}>
                             {row.getVisibleCells().map(cell => (
                                 <td key={cell.id}>
                                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
@@ -136,6 +170,7 @@ export default function RSEAccountUsage() {
                     ))}
                 </tbody>
             </table>
+            {selectedDIDs.map((did, index) => <P key={index}>{did}</P>)}
         </div>
     )
 }
