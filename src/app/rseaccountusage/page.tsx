effadfb5ddc3aab5d82f2f3a4d62df6b3caa7639
Anton Schwarz <anton.schwarz@cern.ch>
2023-04-11 14:07:46 +0200

    Responsive Table Design
    Can collapse rightmost table column
    
    Also fixed the column widths for tanstack

--- src/app/rseaccountusage/page.tsx
+++ src/app/rseaccountusage/page.tsx
@@ -7,50 +7,55 @@ import { useEffect, useState } from "react"
 import { RSEAccountUsageLimitDTO } from "@/lib/core/data/rucio-dto"
 import useComDOM from "@/lib/infrastructure/hooks/useComDOM"
 import { createColumnHelper, flexRender, getCoreRowModel, TableOptions, useReactTable } from "@tanstack/react-table"
-import { tab } from "@testing-library/user-event/dist/types/convenience"
 
 const columnHelper = createColumnHelper<RSEAccountUsageLimitDTO>()
 
 
 
 export default function RSEAccountUsage() {
-    
-    const [selectedDIDs, setSelectedDIDs] = useState<string[]>([])
-    
+
+    const [selectedRSEIDs, setSelectedRSEIDs] = useState<string[]>([])
+
     const columns: any[] = [
         {
             id: 'selection',
-            header: "Select",
+            header: () => <span className="w-8"/>,
             cell: (props) => {
-                return <input
-                    type="checkbox"
-                    disabled={
-                        // if quota_bytes is less than used_bytes, disable checkbox
-                        // TODO handle "askpermission" case
-                        (props.row.original.quota_bytes && props.row.original.quota_bytes < props.row.original.used_bytes) ? true : false
-                    }
-                    checked={props.row.getIsSelected()}
-                    onChange={(event: any) => {
-                        if (event.target.checked) {
-                            setSelectedDIDs([...selectedDIDs, props.row.original.rse_id])
+                return <span className="w-8">
+                    <input
+                        type="checkbox"
+                        disabled={
+                            // if quota_bytes is less than used_bytes, disable checkbox
+                            // TODO handle "askpermission" case
+                            (props.row.original.quota_bytes && props.row.original.quota_bytes < props.row.original.used_bytes) ? true : false
                         }
-                        else {
-                            setSelectedDIDs(selectedDIDs.filter((did) => did !== props.row.original.rse_id))
-                        }
-                        props.row.getToggleSelectedHandler()(event)
-                    }}
-                />
+                        checked={props.row.getIsSelected()}
+                        onChange={(event: any) => {
+                            if (event.target.checked) {
+                                setSelectedRSEIDs([...selectedRSEIDs, props.row.original.rse_id])
+                            }
+                            else {
+                                setSelectedRSEIDs(selectedRSEIDs.filter((did) => did !== props.row.original.rse_id))
+                            }
+                            props.row.getToggleSelectedHandler()(event)
+                        }}
+                    />
+                </span>
             },
         },
         columnHelper.accessor('rse_id', {
             id: 'rse_id',
             header: 'ID',
-            cell: (info) => info.getValue(),
+            cell: (info) => { <P mono>{info.getValue()}</P> },
         }),
         columnHelper.accessor('rse', {
             id: 'rse',
-            header: () => <H3>RSE Name</H3>,
-            cell: (info) => info.getValue(),
+            header: () => (
+                <span className="w-1/2">
+                    <H3>RSE Name</H3>
+                </span>
+            ),
+            cell: (info) => <P mono>{info.getValue()}</P>,
         }),
         columnHelper.accessor('used_bytes', {
             id: 'used_bytes',
@@ -66,6 +71,9 @@ export default function RSEAccountUsage() {
         columnHelper.accessor('quota_bytes', {
             id: 'quota_bytes',
             header: () => <H3>Quota</H3>,
+            cell: (props) => {
+                return <P mono>{props.row.original.quota_bytes}</P>
+            }
         }),
         columnHelper.accessor('used_files', {
             id: 'used_files',
@@ -134,35 +142,45 @@ export default function RSEAccountUsage() {
                 }}>Pause</button>
                 <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={resume}>Resume</button>
                 <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" onClick={clean}>Clean</button>
-                <button className="focus:outline-none text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-900" onClick={
-                    () => {
-                        console.info(table.getSelectedRowModel().flatRows.map(row => row.original.rse_id));
-                    }
-                }>Row Model</button>
                 <div className="flex flex-row">Poll Interval: {pollInterval}</div>
             </div>
-            <table className={`w-full text-left ${query.fetchStatus === "fetching" ? "hover:cursor-wait" : ""}`}>
+            <table className={`table-fixed border w-full text-left ${query.fetchStatus === "fetching" ? "hover:cursor-wait" : ""}`}>
                 <thead className="w-full border-b">
                     {table.getHeaderGroups().map(headerGroup => (
-                        <tr key={headerGroup.id}>
-                            {headerGroup.headers.map(header => (
-                                <th key={header.id}>
-                                    {header.isPlaceholder
-                                        ? null
-                                        : flexRender(
-                                            header.column.columnDef.header,
-                                            header.getContext()
-                                        )}
-                                </th>
-                            ))}
+                        <tr
+                            key={headerGroup.id}
+                            className="w-full flex-row"
+                        >
+                            <th className="w-8 grow-0"></th>
+                            <th className="w-1/2 flex-auto">RSE Name</th>
+                            <th className="flex-initial">Used Quota</th>
+                            <th className="hidden sm:table-cell sm:flex-initial">Total Quota</th>
                         </tr>
                     ))}
                 </thead>
-                <tbody>
+                <tbody
+                    className="w-full"
+                >
                     {table.getRowModel().rows.map(row => (
-                        <tr className="hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-900 w-full border-b hover:cursor-pointer" key={row.id}>
+                        <tr
+                            className="hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-900 w-full border-b hover:cursor-pointer"
+                            key={row.id}
+                            onClick={(event) => {
+                                let rse_id = row.original.rse_id
+                                if (selectedRSEIDs.includes(rse_id)) {
+                                    setSelectedRSEIDs(selectedRSEIDs.filter(id => id !== rse_id))
+                                } else {
+                                    setSelectedRSEIDs([...selectedRSEIDs, rse_id])
+                                }
+                                row.getToggleSelectedHandler()(event)
+                            }}
+                        >
                             {row.getVisibleCells().map(cell => (
-                                <td key={cell.id}>
+                                
+                                <td
+                                    key={cell.id}
+                                    className={cell.column.id === "quota_bytes" ? "hidden sm:table-cell" : ""}
+                                >
                                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                 </td>
                             ))}
@@ -170,7 +188,7 @@ export default function RSEAccountUsage() {
                     ))}
                 </tbody>
             </table>
-            {selectedDIDs.map((did, index) => <P key={index}>{did}</P>)}
+            {selectedRSEIDs.map((did, index) => <P key={index}>{did}</P>)}
             <P>{query.fetchStatus}</P>
         </div>
     )
