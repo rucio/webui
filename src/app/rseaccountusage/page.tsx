37bfd34fe27b5377f2ac864981fb8ece1dbf3335
Anton Schwarz <anton.schwarz@cern.ch>
2023-04-12 13:19:07 +0200

    Streaming Table is now Scrollable
    The best solution to have a dedicated space which is empty and with a
    border that gets filled with table rows (while keeping the table header
    at the top when scrolling) is not the same way as I did it earlier.
    
    Instead of overflowing `tbody`, the entire `table` is placed into an
    overflowing `div`. The sole `tr` of `thead` is `sticky` and `top-0` in
    tailwindcss terms.
    
    The border between head and the `tbody` is provided
    by a shadow (since borders are somehow not visible in the sticky setup).
    The shadow gives a nice effect.

--- src/app/rseaccountusage/page.tsx
+++ src/app/rseaccountusage/page.tsx
@@ -144,50 +144,54 @@ export default function RSEAccountUsage() {
                 <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" onClick={clean}>Clean</button>
                 <div className="flex flex-row">Poll Interval: {pollInterval}</div>
             </div>
-            <table className={`table-fixed border w-full text-left ${query.fetchStatus === "fetching" ? "hover:cursor-wait" : ""}`}>
-                <thead className="w-full border-b">
-                    {table.getHeaderGroups().map(headerGroup => (
-                        <tr
-                            key={headerGroup.id}
-                            className="w-full flex-row"
-                        >
-                            <th className="w-8 grow-0"></th>
-                            <th className="w-1/2 flex-auto">RSE Name</th>
-                            <th className="flex-initial">Used Quota</th>
-                            <th className="hidden sm:table-cell sm:flex-initial">Total Quota</th>
-                        </tr>
-                    ))}
-                </thead>
-                <tbody
-                    className="w-full"
-                >
-                    {table.getRowModel().rows.map(row => (
-                        <tr
-                            className="hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-900 w-full border-b hover:cursor-pointer"
-                            key={row.id}
-                            onClick={(event) => {
-                                let rse_id = row.original.rse_id
-                                if (selectedRSEIDs.includes(rse_id)) {
-                                    setSelectedRSEIDs(selectedRSEIDs.filter(id => id !== rse_id))
-                                } else {
-                                    setSelectedRSEIDs([...selectedRSEIDs, rse_id])
-                                }
-                                row.getToggleSelectedHandler()(event)
-                            }}
-                        >
-                            {row.getVisibleCells().map(cell => (
-                                
-                                <td
-                                    key={cell.id}
-                                    className={cell.column.id === "quota_bytes" ? "hidden sm:table-cell" : ""}
+            <div className="p-4">
+                <div className={`h-72 overflow-y-auto border ${query.fetchStatus === "fetching" ? "hover:cursor-wait" : ""}`}>
+                    <table className="table-fixed w-full text-left">
+                        <thead className="w-full">
+                            {table.getHeaderGroups().map(headerGroup => (
+                                <tr
+                                    key={headerGroup.id}
+                                    className="w-full flex-row sticky top-0 bg-white shadow-md"
                                 >
-                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                </td>
+                                    <th className="w-8 grow-0"></th>
+                                    <th className="w-1/2 flex-auto">RSE Name</th>
+                                    <th className="flex-initial">Used Quota</th>
+                                    <th className="hidden sm:table-cell sm:flex-initial">Total Quota</th>
+                                </tr>
                             ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+                        </thead>
+                        <tbody
+                            className="w-full"
+                        >
+                            {table.getRowModel().rows.map(row => (
+                                <tr
+                                    className="hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-900 w-full border-b hover:cursor-pointer"
+                                    key={row.id}
+                                    onClick={(event) => {
+                                        let rse_id = row.original.rse_id
+                                        if (selectedRSEIDs.includes(rse_id)) {
+                                            setSelectedRSEIDs(selectedRSEIDs.filter(id => id !== rse_id))
+                                        } else {
+                                            setSelectedRSEIDs([...selectedRSEIDs, rse_id])
+                                        }
+                                        row.getToggleSelectedHandler()(event)
+                                    }}
+                                >
+                                    {row.getVisibleCells().map(cell => (
+                    
+                                        <td
+                                            key={cell.id}
+                                            className={cell.column.id === "quota_bytes" ? "hidden sm:table-cell" : ""}
+                                        >
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        </td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
             {selectedRSEIDs.map((did, index) => <P key={index}>{did}</P>)}
             <P>{query.fetchStatus}</P>
         </div>
