fee489394556685a636b664ee3593df0efae4e06
Anton Schwarz <anton.schwarz@cern.ch>
2023-04-14 14:15:00 +0200

    Extended `Button` Class to Handle icon-only buttons
    Icons without labels will now be handled differently so that the icon is
    properly centered.
    
    Note that this is not a proper rework of the button class and that width
    `w-full` is still the default.

--- src/component-library/components/Button/Button.tsx
+++ src/component-library/components/Button/Button.tsx
@@ -1,29 +1,27 @@
 import { Collapsible } from "../Helpers/Collapsible"
 
 interface ButtonProps {
-    label: string
-    icon?: any
-    type?: 'button' | 'submit' | 'reset'
-    disabled?: boolean
-    theme?: 'blue' | 'orange',
-    fullwidth?: boolean
-    onClick?: (args: unknown) => void
-    id?: string
 }
 
-export const Button = ({
-    label,
-    icon = null,
-    type = "button",
-    disabled = false,
-    theme = "blue",
-    fullwidth = true,
-    onClick = undefined,
-    id = undefined,
-}: ButtonProps) => {
-    const handleOnClick = disabled ? undefined : onClick
+export const Button = (
+    props: {
+        label?: string
+        icon?: any
+        type?: 'button' | 'submit' | 'reset'
+        disabled?: boolean
+        theme?: 'blue' | 'orange',
+        fullwidth?: boolean
+        onClick?: (args: unknown) => void
+        id?: string
+    }
+) => {
+    const type = props.type ?? "button"
+    const theme = props.theme ?? "blue"
+    const fullwidth = props.fullwidth ?? true
+
+    const handleOnClick = props.disabled ? undefined : props.onClick
     var classes: string[] = ["font-bold rounded py-1 px-3"]
-    if (!disabled) {
+    if (!props.disabled) {
         switch (type) {
             case "submit":
                 classes.push("bg-green-500", "hover:bg-green-600", "text-white")
@@ -46,20 +44,38 @@ export const Button = ({
     if (fullwidth) {
         classes.push("w-full")
     }
-    return (
-
-        <button
-            type={type}
-            disabled={disabled}
-            className={classes.join(" ")}
-            onClick={handleOnClick}
-            id={id}
-        >
-            <div className="flex justify-center">
-                <label htmlFor={id}>{label}</label>
-                {icon ? "\u00A0" : "" /* nonbreaking space */}
-                <div className="flex items-center">{icon}</div>
-            </div>
-        </button>
-    )
+    if (!!props.label) {
+        return (
+            <button
+                type={type}
+                disabled={props.disabled}
+                className={classes.join(" ")}
+                onClick={handleOnClick}
+                id={props.id}
+            >
+                <div className="flex justify-center">
+                    <label htmlFor={props.id}>
+                        {props.label}
+                    </label>
+                    {props.icon ? "\u00A0" : "" /* nonbreaking space */}
+                    <div className="flex items-center">{props.icon}</div>
+                </div>
+            </button>
+        )
+    }
+    else {
+        return (
+            <button
+                type={type}
+                disabled={props.disabled}
+                className={classes.join(" ")}
+                onClick={handleOnClick}
+                id={props.id}
+            >
+                <label htmlFor={props.id} className="flex justify-center hover:cursor-pointer">
+                    {props.icon}
+                </label>
+            </button>
+        )
+    }
 }
