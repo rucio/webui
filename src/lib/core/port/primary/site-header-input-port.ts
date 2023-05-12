e8c3032e2a2432898211299ef47852d5a633f4bf
maany <imptodefeat@gmail.com>
2023-05-12 15:42:26 +0200

    api: remove hosturl from site header #169

--- src/lib/core/port/primary/site-header-input-port.ts
+++ src/lib/core/port/primary/site-header-input-port.ts
@@ -7,7 +7,6 @@ export default interface SiteHeaderInputPort {
     /**
      * 
      * @param session The unencrypted session object that contains the user information
-     * @param host The hostname of the WebUI that will be used to generate a HomeURL
      */
-    generateSiteHeader(session: IronSession, host: string): Promise<void>;
+    generateSiteHeader(session: IronSession): Promise<void>;
 }
