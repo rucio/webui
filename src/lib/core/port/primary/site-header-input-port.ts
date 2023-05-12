9aec21e88ec8472fb1ab3b6c4b58e488e530d1a8
maany <imptodefeat@gmail.com>
2023-05-12 14:37:28 +0200

    api: Create site header usecase #169

--- src/lib/core/port/primary/site-header-input-port.ts
+++ src/lib/core/port/primary/site-header-input-port.ts
@@ -4,5 +4,10 @@ import { IronSession } from "iron-session";
  * Interface that represents a UseCase that will gather information from avilable gateways to help generate the site header.
  */
 export default interface SiteHeaderInputPort {
-    generateSiteHeader(session: IronSession): Promise<void>;
+    /**
+     * 
+     * @param session The unencrypted session object that contains the user information
+     * @param host The hostname of the WebUI that will be used to generate a HomeURL
+     */
+    generateSiteHeader(session: IronSession, host: string): Promise<void>;
 }
