9e762b81a14314a149552eb1bb351f371766212d
maany <imptodefeat@gmail.com>
2023-05-12 13:56:58 +0200

    api: implement presenter for site header use case #169

--- src/lib/core/port/primary/site-header-output-port.ts
+++ src/lib/core/port/primary/site-header-output-port.ts
@@ -1,4 +1,4 @@
-import { SiteHeaderResponse } from "../../data/site-header-usecase-models";
+import { SiteHeaderError, SiteHeaderResponse } from "../../data/site-header-usecase-models";
 
 /**
  * The interface defines a presenter that will generate a ViewModel used by the Site Header component.
@@ -6,5 +6,5 @@ import { SiteHeaderResponse } from "../../data/site-header-usecase-models";
 export default interface SiteHeaderOutputPort<T> {
     response: T;
     presentSuccess(responseModel: SiteHeaderResponse): Promise<void>;
-    presentError(errorModel: SiteHeaderResponse): Promise<void>;
+    presentError(errorModel: SiteHeaderError): Promise<void>;
 }
---
+++ src/lib/infrastructure/presenter/site-header-presenter.ts
@@ -0,0 +1,36 @@
+import { SiteHeaderError, SiteHeaderResponse } from "@/lib/core/data/site-header-usecase-models";
+import SiteHeaderOutputPort from "@/lib/core/port/primary/site-header-output-port";
+import { NextApiResponse } from "next";
+import { SiteHeaderViewModel } from "../data/view-model/site-header";
+
+/**
+ * Provides an implementation of the {@link SiteHeaderOutputPort} interface.
+ */
+export default class SiteHeaderPresenter implements SiteHeaderOutputPort<NextApiResponse> {
+    response: NextApiResponse<any>;
+    
+    constructor(response: NextApiResponse) {
+        this.response = response;
+    }
+
+    async presentSuccess(responseModel: SiteHeaderResponse): Promise<void> {
+        const viewModel: SiteHeaderViewModel = {
+            activeAccount: responseModel.activeUser || undefined,
+            availableAccounts: responseModel.availableUsers || [],
+            homeUrl: responseModel.homeUrl,
+            projectUrl: responseModel.projectURL
+        }
+        this.response.status(200).json(viewModel);
+    }
+
+    async presentError(errorModel: SiteHeaderError): Promise<void> {
+        if (errorModel.error === 'project-url-not-found') {
+            this.response.status(500).json(errorModel);
+            return;
+        }
+        if (errorModel.error === 'no-active-user') {
+            this.response.status(418).json(errorModel);
+        }
+    }
+    
+}
