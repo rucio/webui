905029909b71d3758e2e2cd9ae95494932e15c00
maany <imptodefeat@gmail.com>
2023-05-12 14:49:05 +0200

    api: site header controller #169

--- src/lib/infrastructure/config/ioc/ioc-symbols-usecase-factory.ts
+++ src/lib/infrastructure/config/ioc/ioc-symbols-usecase-factory.ts
@@ -5,10 +5,12 @@
  * They inject the response object into the presenter used by the use case.
  */
 const USECASE_FACTORY = {
-    USERPASS_LOGIN: Symbol.for("Factory<UserPassLoginUseCase>"),
     LOGIN_CONFIG: Symbol.for("Factory<LoginConfigUseCase>"),
     SET_X509_LOGIN_SESSION: Symbol.for("Factory<SetX509LoginSessionUseCase>"),
+    SITE_HEADER: Symbol.for("Factory<SiteHeaderUseCase>"),
     STREAM: Symbol.for("Factory<StreamUseCase>"),
+    USERPASS_LOGIN: Symbol.for("Factory<UserPassLoginUseCase>"),
+
 }
 
 export default USECASE_FACTORY;
---
+++ src/lib/infrastructure/controller/site-header-controller.ts
@@ -0,0 +1,25 @@
+import SiteHeaderInputPort from "@/lib/core/port/primary/site-header-input-port";
+import { inject, injectable } from "inversify";
+import { IronSession } from "iron-session";
+import { NextApiRequest, NextApiResponse } from "next";
+import USECASE_FACTORY from "../config/ioc/ioc-symbols-usecase-factory";
+
+export interface ISiteHeaderController {
+    handle(session: IronSession, request: NextApiRequest, response: NextApiResponse): void;
+}
+
+@injectable()
+class SiteHeaderController implements ISiteHeaderController {
+    private useCase: SiteHeaderInputPort | null = null
+    private useCaseFactory: (session: IronSession, response: NextApiResponse) => SiteHeaderInputPort
+    constructor(
+        @inject(USECASE_FACTORY.SITE_HEADER) useCaseFactory: (session: IronSession, response: NextApiResponse) => SiteHeaderInputPort,
+    ){
+        this.useCaseFactory = useCaseFactory;
+    }
+    handle(session: IronSession, request: NextApiRequest, response: NextApiResponse) {
+        this.useCase = this.useCaseFactory(session, response);
+        const hostname = request.headers.host
+        this.useCase.generateSiteHeader(session, hostname)
+    }
+}
