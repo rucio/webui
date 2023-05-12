c882439df114041e89666086b07f672ad8911b59
maany <imptodefeat@gmail.com>
2023-05-12 13:56:23 +0200

    api: update view, usecase and auth models for site header #169

--- src/lib/core/data/site-header-usecase-models.ts
+++ src/lib/core/data/site-header-usecase-models.ts
@@ -1,11 +1,17 @@
+import { User } from "../entity/auth-models"
+
 /**
  * Response model for SiteHeaderUseCase {@link SiteHeaderUseCase}
  * @property {string} homeUrl - URL of the home page (dashboard)
- * @property {string} experimentUrl - URL of the experiment page
- * @property {string | null} error - Error message if any
+ * @property {string} projectURL - URL of the experiment page
  */
 export type SiteHeaderResponse = {
     homeUrl: string
-    experimentUrl: string
-    error: string | null
+    projectURL: string
+    activeUser: User | null
+    availableUsers: User[]
+}
+
+export type SiteHeaderError = {
+    error: 'project-url-not-found' | 'no-active-user'
 }
