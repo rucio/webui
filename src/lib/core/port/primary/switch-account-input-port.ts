84d2957d83c1684808ea2a6dbd2f9a8957eb5df4
maany <imptodefeat@gmail.com>
2023-05-15 18:00:01 +0200

    api: add SwitchAccount Usecase #166

--- src/lib/core/port/primary/switch-account-input-port.ts
+++ src/lib/core/port/primary/switch-account-input-port.ts
@@ -1,5 +1,14 @@
+import { IronSession } from "iron-session";
 import { SwitchAccountRequest } from "../../data/switch-account-usecase-models";
 
+/**
+ * Provides an interface for the {@link SwitchAccountUseCase}.
+ */
 export default interface SwitchAccountInputPort {
-    switchAccount(request: SwitchAccountRequest): Promise<void>
+    /**
+     * This function breaks the clean architecture, but it is necessary to do so in this case.
+     * @param request {@link SwitchAccountRequest}
+     * @param session The IronSession object
+     */
+    switchAccount(request: SwitchAccountRequest, session: IronSession): Promise<void>
 }
