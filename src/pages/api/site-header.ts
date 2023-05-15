cf63fe5166cc599e65855eb7063cfaf01ad898ca
maany <imptodefeat@gmail.com>
2023-05-15 15:37:08 +0200

    api: add /api/site-header endpoint #169

---
+++ src/pages/api/site-header.ts
@@ -0,0 +1,12 @@
+import { withSessionRoute } from "@/lib/infrastructure/auth/session-utils";
+import appContainer from "@/lib/infrastructure/config/ioc/container-config";
+import CONTROLLERS from "@/lib/infrastructure/config/ioc/ioc-symbols-controllers";
+import { ISiteHeaderController } from "@/lib/infrastructure/controller/site-header-controller";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function siteHeader(req: NextApiRequest, res: NextApiResponse) {
+    const siteHeaderController: ISiteHeaderController = appContainer.get(CONTROLLERS.SITE_HEADER)
+    await siteHeaderController.handle(req.session, res)
+}
+
+export default withSessionRoute(siteHeader)
