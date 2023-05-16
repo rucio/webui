801a9167bfe3a4c9a10ced775729c0618d377be7
maany <imptodefeat@gmail.com>
2023-05-16 11:21:28 +0200

    api: add test for site header when no user is logged in #169

--- test/api/site-header.test.ts
+++ test/api/site-header.test.ts
@@ -7,6 +7,7 @@ import { SiteHeaderViewModel } from "@/lib/infrastructure/data/view-model/site-h
 import { getIronSession } from "iron-session";
 import { NextApiRequest } from "next";
 import { createMocks } from "node-mocks-http";
+import { createHttpMocks } from "test/fixtures/http-fixtures";
 
 describe('SiteHeader API Test', () => {
     it('should present successful SiteHeaderViewModel', async () => {
@@ -44,6 +45,26 @@ describe('SiteHeader API Test', () => {
         expect(viewModel).toHaveProperty('activeAccount');
         expect(viewModel.activeAccount?.rucioIdentity).toBe('ddmlab');
         expect(viewModel.homeUrl).toBe('/dashboard')
+
+        delete process.env['PROJECT_URL']
+    })
+
+    it('should present SiteHeaderViewModel when Session does not contain any users', async () => {
+        const { req, res, session } = await createHttpMocks() 
+        const siteHeaderController = appContainer.get<ISiteHeaderController>(CONTROLLERS.SITE_HEADER)
+        await siteHeaderController.handle(session, res as undefined as NextApiRequest);
+        
+        expect(res._getStatusCode()).toBe(418);
+        const data = JSON.parse(res._getData());
+        expect(data.error).toBe('no-active-user')
+        expect(data).not.toHaveProperty('projectUrl');
+        expect(data).not.toHaveProperty('activeAccount');
+        expect(data).toHaveProperty('homeUrl');
+        expect(data.homeUrl).toBe('/dashboard')
+
+        expect(session.user).toBeUndefined()
+        expect(session.allUsers).toBeUndefined()
+
     })
 
 })
