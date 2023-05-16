c9a2cdbe3ceb63113d6e31d3e8de9195deb5e0d9
Anton Schwarz <anton.schwarz@cern.ch>
2023-05-16 17:22:24 +0200

    Redesign of `PageDID` and also `DIDMetaView`
    Moved the `DIDMetaView` into a horizontal (grid-based) version and to
    the top of the `PageDID`, meaning that there is more horizontal space
    available for the tables.

--- src/component-library/components/Pages/PageDID/PageDID.tsx
+++ src/component-library/components/Pages/PageDID/PageDID.tsx
@@ -27,25 +27,25 @@ export interface PageDIDPageProps {
     fromDidList?: string; // if coming from DIDList, this will be the DIDList's query
     // Parent DIDs [FILE]
     didParentsSearch: (didSearchQuery: DIDSearchQuery) => void
-    didParentsResponse: { data: any; fetchStatus: FetchStatus}
+    didParentsResponse: { data: any; fetchStatus: FetchStatus }
     // Metadata [BOTH]
     didMetadataSearch: (didSearchQuery: DIDSearchQuery) => void
-    didMetadataResponse: { data: any; fetchStatus: FetchStatus}
+    didMetadataResponse: { data: any; fetchStatus: FetchStatus }
     // File Replica States [FILE]
     didFileReplicasSearch: (didSearchQuery: DIDSearchQuery) => void
-    didFileReplicasResponse: { data: any; fetchStatus: FetchStatus}
+    didFileReplicasResponse: { data: any; fetchStatus: FetchStatus }
     // File Replica States [DATASET]
     didFileReplicasDatasetSearch: (didSearchQuery: DIDSearchQuery) => void
-    didFileReplicasDatasetResponse: { data: any; fetchStatus: FetchStatus}
+    didFileReplicasDatasetResponse: { data: any; fetchStatus: FetchStatus }
     // Rule State [DATASET]
     didRulesSearch: (didSearchQuery: DIDSearchQuery) => void
-    didRulesResponse: { data: any; fetchStatus: FetchStatus}
+    didRulesResponse: { data: any; fetchStatus: FetchStatus }
     // Contents [COLLECTION]
     didContentsSearch: (didSearchQuery: DIDSearchQuery) => void
-    didContentsResponse: { data: DIDContents[]; fetchStatus: FetchStatus}
+    didContentsResponse: { data: DIDContents[]; fetchStatus: FetchStatus }
     // Dataset Replica States [DATASET]
     didDatasetReplicasSearch: (didSearchQuery: DIDSearchQuery) => void
-    didDatasetReplicasResponse: { data: any; fetchStatus: FetchStatus}
+    didDatasetReplicasResponse: { data: any; fetchStatus: FetchStatus }
 }
 
 const SubPage: (
@@ -87,148 +87,149 @@ export const PageDID = (
                 className={twMerge(
                     "rounded-md w-full",
                     "border dark:border-2 dark:border-gray-200 p-2",
-                    "flex flex-col items-start space-y-2 md:flex-row md:justify-between md:items-end md:space-y-0",
+                    "flex flex-col items-start space-y-2",
                     "bg-white dark:bg-gray-800"
-                )}
-            >
-                <span className="flex flex-row space-x-4">
-                    <H3>DID Page for {props.didMeta.scope}:{props.didMeta.name}</H3>
-                    <DIDTypeTag didtype={props.didMeta.did_type} />
-                </span>
-                <a
-                    className={twMerge(
-                        props.fromDidList ? "flex" : "hidden",
-                        "bg-blue-500 hover:bg-blue-600 text-white",
-                        "py-1 px-3 h-8 rounded",
-                        "font-bold",
-                        "cursor-pointer",
-                        "flex-row items-center space-x-2"
-                    )}
-                    href={props.fromDidList ? "/listdids?=" + props.fromDidList : "/"} // TODO connect properly
-                    id="back-to-didlist-button"
-                >
-                    <HiArrowCircleLeft className="text-xl" />
-                    <label className="cursor-pointer" htmlFor="back-to-didlist-button">
-                        Back to DID List
-                    </label>
-                </a>
-            </div>
-            <div
-                className={twMerge(
-                    "flex flex-col lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:gap-y-0 lg:gap-x-2", // maybe use xl or even 2xl
-                    "bg-white dark:bg-gray-800 border dark:border-gray-200 dark:border-2",
-                    "p-2 rounded-lg"
                 )}
             >
                 <div
                     className={twMerge(
-                        "min-w-0",
-                        "lg:col-span-2",
-                        "flex flex-col"
+                        "flex flex-col items-end space-y-2 lg:flex-row lg:justify-between lg:items-baseline lg:space-y-0 w-full",
+                        "bg-white dark:bg-gray-800"
                     )}
                 >
-                    <Tabs
-                        tabs={
-                            didtype === "File" ? ["File Replica States", "Parent DIDs", "Metadata"] :
-                                (didtype === "Dataset" ? ["Rules", "Dataset Replicas", "File Replica States", "Metadata"] :
-                                    ["Contents", "Rules", "File Replica States", "Metadata"]
-                                )
-                        } // remember difference between collections and files
-                        active={0}
-                        handleClick={(event: any) => { console.log(event.target.dataset.id); setSubpageIndex(Number(event.target.dataset.id)) }}
-                    />
-                    <SubPage
-                        show={didtype === "File" ? false : didtype === "Dataset" ? subpageIndex === 0 : subpageIndex === 1}
-                        id="subpage-rules"
+                    <span className="flex flex-row space-x-4">
+                        <H3>DID Page for {props.didMeta.scope}:{props.didMeta.name}</H3>
+                        <DIDTypeTag didtype={props.didMeta.did_type} />
+                    </span>
+                    <a
+                        className={twMerge(
+                            props.fromDidList ? "flex" : "hidden",
+                            "bg-blue-500 hover:bg-blue-600 text-white",
+                            "py-1 px-3 h-8 rounded",
+                            "font-bold",
+                            "cursor-pointer",
+                            "flex-row items-center space-x-2 shrink-0"
+                        )}
+                        href={props.fromDidList ? "/listdids?=" + props.fromDidList : "/"} // TODO connect properly
+                        id="back-to-didlist-button"
                     >
-                        <PageDIDRules tableData={{
-                            data: props.didRulesResponse.data,
-                            fetchStatus: props.didRulesResponse.fetchStatus,
-                            pageSize: 10
-                        }}
-                        />
-                    </SubPage>
-                    <SubPage
-                        show={didtype === "File" ? false : didtype === "Dataset" ? subpageIndex === 1 : false}
-                        id="subpage-dataset-replicas"
-                    >
-                        <PageDIDDatasetReplicas tableData={{
-                            data: props.didDatasetReplicasResponse.data,
-                            fetchStatus: props.didDatasetReplicasResponse.fetchStatus,
-                            pageSize: 10
-                        }}/>
-                    </SubPage>
-                    <SubPage
-                        show={didtype === "File" ? subpageIndex === 0 : didtype === "Dataset" ? subpageIndex === 2 : subpageIndex === 2}
-                        id="subpage-file-replica-states"
-                    >
-                        <div className={twMerge( didtype === "File" ? "block" : "hidden")}>
-                            <PageDIDFilereplicas tableData={{
-                                data: props.didFileReplicasResponse.data,
-                                fetchStatus: props.didFileReplicasResponse.fetchStatus,
-                                pageSize: 10
-                            }}
-                            />
-                        </div>
-                        <div className={twMerge( didtype === "Dataset" ? "block" : "hidden")}>
-                            <PageDIDFilereplicasD
-                                replicaTableData={{
-                                    data: props.didFileReplicasResponse.data,
-                                    fetchStatus: props.didFileReplicasResponse.fetchStatus,
-                                    pageSize: 10
-                                }}
-                                datasetTableData={{
-                                    data: props.didFileReplicasDatasetResponse.data,
-                                    fetchStatus: props.didFileReplicasDatasetResponse.fetchStatus,
-                                    pageSize: 10
-                                }}
-                                onChangeDatasetSelection={(dataset: string) => { console.log("datasetSelection", dataset)}}
-                            />
-
-                        </div>
-                    </SubPage>
-                    <SubPage
-                        show={didtype === "File" ? subpageIndex === 1 : false}
-                        id="subpage-parent-dids"
-                    >
-                        <PageDIDParents tableData={{
-                            data: props.didParentsResponse.data,
-                            fetchStatus: props.didParentsResponse.fetchStatus,
-                            pageSize: 10
-                        }}/>
-                    </SubPage>
-                    <SubPage
-                        show={didtype === "File" ? subpageIndex === 2 : didtype === "Dataset" ? subpageIndex === 3 : subpageIndex === 3}
-                        id="subpage-metadata"
-                    >
-                        <PageDIDMetadata tableData={{
-                            data: props.didMetadataResponse.data,
-                            fetchStatus: props.didMetadataResponse.fetchStatus,
-                            pageSize: 10
-                        }}/>
-                    </SubPage>
-                    <SubPage
-                        show={didtype === "Container" ? subpageIndex === 0 : false}
-                        id="subpage-contents"
-                    >
-                        <PageDIDContents showDIDType tableData={{
-                            data: props.didContentsResponse.data,
-                            fetchStatus: props.didContentsResponse.fetchStatus,
-                            pageSize: 10
-                        }}/>
-                    </SubPage>
-
+                        <HiArrowCircleLeft className="text-xl" />
+                        <label className="cursor-pointer" htmlFor="back-to-didlist-button">
+                            Back to DID List
+                        </label>
+                    </a>
                 </div>
                 <div
                     className={twMerge(
                         "bg-stone-100 dark:bg-gray-900",
                         "rounded-md p-2",
                         "flex flex-col space-y-2",
-                        "min-h-0"
+                        "min-h-0 w-full"
                     )}
                 >
-                    <DIDMetaView data={props.didMeta} show />
+                    <DIDMetaView data={props.didMeta} show horizontal />
                 </div>
+            </div>
+
+            <div
+                className={twMerge(
+                    "min-w-0",
+                    "lg:col-span-2",
+                    "flex flex-col",
+                    "rounded-md p-2 border"
+                )}
+            >
+                <Tabs
+                    tabs={
+                        didtype === "File" ? ["File Replica States", "Parent DIDs", "Metadata"] :
+                            (didtype === "Dataset" ? ["Rules", "Dataset Replicas", "File Replica States", "Metadata"] :
+                                ["Contents", "Rules", "File Replica States", "Metadata"]
+                            )
+                    } // remember difference between collections and files
+                    active={0}
+                    handleClick={(event: any) => { console.log(event.target.dataset.id); setSubpageIndex(Number(event.target.dataset.id)) }}
+                />
+                <SubPage
+                    show={didtype === "File" ? false : didtype === "Dataset" ? subpageIndex === 0 : subpageIndex === 1}
+                    id="subpage-rules"
+                >
+                    <PageDIDRules tableData={{
+                        data: props.didRulesResponse.data,
+                        fetchStatus: props.didRulesResponse.fetchStatus,
+                        pageSize: 10
+                    }}
+                    />
+                </SubPage>
+                <SubPage
+                    show={didtype === "File" ? false : didtype === "Dataset" ? subpageIndex === 1 : false}
+                    id="subpage-dataset-replicas"
+                >
+                    <PageDIDDatasetReplicas tableData={{
+                        data: props.didDatasetReplicasResponse.data,
+                        fetchStatus: props.didDatasetReplicasResponse.fetchStatus,
+                        pageSize: 10
+                    }} />
+                </SubPage>
+                <SubPage
+                    show={didtype === "File" ? subpageIndex === 0 : didtype === "Dataset" ? subpageIndex === 2 : subpageIndex === 2}
+                    id="subpage-file-replica-states"
+                >
+                    <div className={twMerge(didtype === "File" ? "block" : "hidden")}>
+                        <PageDIDFilereplicas tableData={{
+                            data: props.didFileReplicasResponse.data,
+                            fetchStatus: props.didFileReplicasResponse.fetchStatus,
+                            pageSize: 10
+                        }}
+                        />
+                    </div>
+                    <div className={twMerge(didtype === "Dataset" ? "block" : "hidden")}>
+                        <PageDIDFilereplicasD
+                            replicaTableData={{
+                                data: props.didFileReplicasResponse.data,
+                                fetchStatus: props.didFileReplicasResponse.fetchStatus,
+                                pageSize: 10
+                            }}
+                            datasetTableData={{
+                                data: props.didFileReplicasDatasetResponse.data,
+                                fetchStatus: props.didFileReplicasDatasetResponse.fetchStatus,
+                                pageSize: 10
+                            }}
+                            onChangeDatasetSelection={(dataset: string) => { console.log("datasetSelection", dataset) }}
+                        />
+
+                    </div>
+                </SubPage>
+                <SubPage
+                    show={didtype === "File" ? subpageIndex === 1 : false}
+                    id="subpage-parent-dids"
+                >
+                    <PageDIDParents tableData={{
+                        data: props.didParentsResponse.data,
+                        fetchStatus: props.didParentsResponse.fetchStatus,
+                        pageSize: 10
+                    }} />
+                </SubPage>
+                <SubPage
+                    show={didtype === "File" ? subpageIndex === 2 : didtype === "Dataset" ? subpageIndex === 3 : subpageIndex === 3}
+                    id="subpage-metadata"
+                >
+                    <PageDIDMetadata tableData={{
+                        data: props.didMetadataResponse.data,
+                        fetchStatus: props.didMetadataResponse.fetchStatus,
+                        pageSize: 10
+                    }} />
+                </SubPage>
+                <SubPage
+                    show={didtype === "Container" ? subpageIndex === 0 : false}
+                    id="subpage-contents"
+                >
+                    <PageDIDContents showDIDType tableData={{
+                        data: props.didContentsResponse.data,
+                        fetchStatus: props.didContentsResponse.fetchStatus,
+                        pageSize: 10
+                    }} />
+                </SubPage>
+
             </div>
         </div>
     )
