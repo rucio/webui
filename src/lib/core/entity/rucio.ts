355c7816a14cd51640e762555ee61a0d19597c65
maany <imptodefeat@gmail.com>
2023-06-08 16:14:20 +0200

    Merge rucio.ts from Anton's frontend work

--- src/lib/core/entity/rucio.ts
+++ src/lib/core/entity/rucio.ts
@@ -20,6 +20,7 @@ export enum DIDAvailability {
     Lost = "Lost",
 }
 
+
 // copied from deployed rucio UI
 export type DIDMeta = {
     name: string
@@ -43,13 +44,102 @@ export type DIDMeta = {
     filesize: number | null
 }
 
-
 export enum DIDType {
     DATASET = "DATASET",
     CONTAINER = "CONTAINER",
     FILE = "FILE",
     ALL = "ALL",
     UNKNOWN = "UNKNOWN"
+}
+
+// results of web::flaskapi:v1::rses::RSEAccountUsageLimit::get
+export type RSEAccountUsageLimit = {
+    rse_id: string;
+    rse: string;
+    account: string;
+    used_files: number;
+    used_bytes: number;
+    quota_bytes: number;
+}
+
+// copied from deployed rucio UI
+export type RuleMeta = {
+    account: string
+    activity: string // VO-specific enums, just show string here.
+    copies: number
+    created_at: Date
+    did_type: DIDType
+    expires_at: Date
+    grouping: DIDType
+    id: string
+    ignore_account_limit: boolean
+    ignore_availability: boolean
+    locked: boolean
+    locks_ok_cnt: number
+    locks_replicating_cnt: number
+    locks_stuck_cnt: number
+    name: string
+    notification: RuleNotification
+    priority: number
+    purge_replicas: boolean
+    rse_expression: string
+    scope: string
+    split_container: boolean
+    state: RuleState
+    updated_at: Date
+}
+
+export type SubscriptionMeta = {
+    account: string
+    comments: string
+    created_at: Date
+    id: string
+    last_processed: Date
+    lifetime: Date
+    name: string
+    policyid: number
+    retroactive: boolean
+    state: SubscriptionState
+    updated_at: Date
+    // more difficult datatypes, cast as string for now:
+    filter: string
+    replication_rules: string
+}
+
+export type SubscriptionRuleStates = {
+    name: string
+    state_ok: number
+    state_replicating: number
+    state_stuck: number
+    state_suspended: number
+}
+
+/*
+================================================================================
+========================= ENUMS and DISCRETE TYPES =============================
+================================================================================
+*/
+
+// rucio.db.sqla.constants::LockState
+export enum LockState {
+    Replicating = "R",
+    OK = "O",
+    Stuck = "S",
+}
+
+// rucio.db.sqla.constants::RuleNotification
+export enum RuleNotification {
+    Yes = "Y", // notify when the rules state becomes OK
+    No = "N", // no notification
+    Close = "C", // notify when the rules state becomes OK *and* the DID is closed -> least verbose except for "no"
+    Progress = "P", // notify for each chang
+}
+
+export enum DIDType {
+    Dataset = "Dataset",
+    Container = "Container",
+    Collection = "Collection",
+    File = "File",
 }
 
 export type RSE = {
