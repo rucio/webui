478f161d257e77fe9fccdb2c806d2e245eb640c9
maany <imptodefeat@gmail.com>
2023-08-14 13:18:22 +0200

    add middleware to redirect to login #280
    The limitation with the middleware is that only GET requests
    can be properly redirected after a successful login
    Other types of requests cannot be cached in the session as the session
    object in the middleware is not the same as the one made available
    to the rest of the application. This is a bug with IronSession.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,28 +1,19 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { getRucioAuthToken, getSessionUser } from '@/lib/infrastructure/auth/session-utils'
-import { ReadonlyRequestCookies } from 'next/dist/server/app-render';
-import { RequestCookies } from "next/dist/server/web/spec-extension/cookies";
-import { SessionUser } from "@/lib/core/entity/auth-models";
-
-export default async function DashboardLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const all_cookies: RequestCookies | ReadonlyRequestCookies = cookies()
-  const sessionUser: SessionUser | undefined = await getSessionUser(all_cookies as unknown as ReadonlyRequestCookies)
-  const rucioAuthToken = await getRucioAuthToken(all_cookies)
-  if (!rucioAuthToken || rucioAuthToken === '') {
-    const callbackUrl = encodeURIComponent('/dashboard')
-    const loginUrl = `/api/auth/login?callbackUrl=${callbackUrl}`
-    redirect(loginUrl)
-  }
+
   return (
-    <div>
-    <h3>You are logged in as {sessionUser?.rucioIdentity}</h3>
-    <h3>Your rucio auth token is {rucioAuthToken}</h3>
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
         {children}
-    </div>
+      </body>
+    </html>
   )
 }
---
+++ src/middleware.ts
@@ -0,0 +1,63 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { getIronSession } from 'iron-session/edge' 
+import { sessionOptions } from './lib/infrastructure/auth/session'
+import { IronSession } from 'iron-session'
+import { validateRucioToken } from './lib/infrastructure/auth/auth-utils'
+
+
+const getSession = async(
+    req: NextRequest,
+    res: NextResponse,
+): Promise<IronSession> => {
+    const session  = await getIronSession(
+        req, res, sessionOptions
+    )
+    return session
+}
+
+export const config = {
+    matcher: [
+        /*
+        * Match all request paths except for the ones starting with:
+        * - api (API routes)
+        * - _next/static (static files)
+        * - _next/image (image optimization files)
+        * - favicon.ico (favicon file)
+        */
+    '/((?!api/auth|_next/static|_next/image|favicon.ico|auth/.*).*)',
+    ]
+}
+
+async function initiateLogin(request: NextRequest){
+    const loginPage = new URL(`/api/auth/login?callbackUrl=${request.url}`, request.url)
+    return NextResponse.redirect(loginPage)
+}
+
+export async function middleware(request: NextRequest) {
+    const response = NextResponse.next()
+    try {
+        const session = await getSession(request, response)
+        
+        // check session
+        if(!session || !session.user || !session.user.isLoggedIn || !session.user.rucioAuthToken) {
+            return await initiateLogin(request)
+        }
+
+        // check if rucio token is valid
+        try {
+            validateRucioToken(session.user)
+        }
+        catch(error) {
+            return await initiateLogin(request)
+        }
+
+        // All checks have passed, redirect to the original request if it exists
+        return response
+
+    }catch(error) {
+        console.log(error)
+        return new Response('Internal Server Error. Could not authenticate or rediect to login page', { status: 500 })
+    }
+}
+ 
