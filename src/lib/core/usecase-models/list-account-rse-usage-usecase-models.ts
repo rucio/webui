109d5e4c81249936a3ca32ded908ace9e715dc05
maany <imptodefeat@gmail.com>
2023-08-15 17:49:39 +0200

    api: define ports and request/response model for ListAccountRSEUsage #170

--- src/lib/core/usecase-models/list-account-rse-usage-usecase-models.ts
+++ src/lib/core/usecase-models/list-account-rse-usage-usecase-models.ts
@@ -1,4 +1,24 @@
+import { BaseErrorResponseModel, BaseResponseModel } from "@/lib/sdk/usecase-models";
+import { RSEAccountUsageLimit } from "../entity/rucio";
+
+/**
+ * @interface ListAccountRSEUsageRequest represents the ListAccountRSEUsage request model for ListAccountRSEUsage feature.
+ */
 export interface ListAccountRSEUsageRequest {
     account: string;
-    
+}
+
+/**
+ * @interface ListAccountRSEUsageResponse represents the ListAccountRSEUsage response model for a streamed element.
+ */
+export interface ListAccountRSEUsageResponse extends RSEAccountUsageLimit, BaseResponseModel {}
+
+
+/**
+ * @interface ListAccountRSEUsageError represents the ListAccountRSEUsage error model for a streamed element.
+ * @property error - Error Message contains error from gateway unless the following type of error occurs:
+ * INVALID_ACCOUNT: The account specified in the request is not same as the account present in the session
+ */
+export interface ListAccountRSEUsageError extends BaseErrorResponseModel {
+    error: 'INVALID_ACCOUNT' | string
 }
