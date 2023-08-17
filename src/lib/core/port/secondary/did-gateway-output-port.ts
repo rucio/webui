38400c8a3f679108590a2fc60f152024a414efe7
Anton Schwarz <anton.schwarz@cern.ch>
2023-08-17 10:55:31 +0200

    Gateway: registered get-did-keyvaluepairs to did-gateway

--- src/lib/core/port/secondary/did-gateway-output-port.ts
+++ src/lib/core/port/secondary/did-gateway-output-port.ts
@@ -1,4 +1,4 @@
-import { ListDIDDTO, DIDDTO, DIDMetaDTO } from "../../dto/did-dto";
+import { ListDIDDTO, DIDDTO, DIDMetaDTO, DIDKeyValuePairsDTO } from "../../dto/did-dto";
 import { DIDType } from "../../entity/rucio";
 
 
@@ -12,13 +12,21 @@ export default interface DIDGatewayOutputPort {
     getDID(rucioAuthToken: string, scope: string, name: string): Promise<DIDDTO>
     
     /**
-     * 
+     * Returns a selection of metadata in an object
      * @param rucioAuthToken A valid rucio auth token
      * @param scope The scope of the DID
      * @param name The name of the DID
      */
     getDIDMeta(rucioAuthToken: string, scope: string, name: string): Promise<DIDMetaDTO>
     
+    /**
+     * Returns the key value pairs for the DID as a list (sourced from rucio meta)
+     * @param rucioAuthToken A valid rucio auth token
+     * @param scope The scope of the DID
+     * @param name The name of the DID
+     */
+    getDIDKeyValuePairs(rucioAuthToken: string, scope: string, name: string): Promise<DIDKeyValuePairsDTO>
+
     /**
      * Searches Rucio Server for expressions that match the scope:name pattern provided.
      * @param rucioAuthToken A valid rucio auth token
