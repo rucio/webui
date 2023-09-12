df82211e088ccc4bd844c5fef2cb70de2de97fc3
maany <imptodefeat@gmail.com>
2023-09-12 14:30:17 +0200

    api: list-did-parrents; add usecase #317

---
+++ src/lib/core/use-case/list-did-parents-usecase.ts
@@ -0,0 +1,53 @@
+import { injectable } from "inversify";
+import { BaseSingleEndpointStreamingUseCase } from "@/lib/sdk/usecase"
+import { AuthenticatedRequestModel } from "@/lib/sdk/usecase-models";
+
+import { ListDIDParentsError, ListDIDParentsRequest, ListDIDParentsResponse } from "@/lib/core/usecase-models/list-did-parents-usecase-models";
+import { ListDIDParentsInputPort, type ListDIDParentsOutputPort } from "@/lib/core/port/primary/list-did-parents-ports";
+import { DIDViewModel } from "@/lib/infrastructure/data/view-model/did";
+
+import { ListDIDDTO } from "@/lib/core/dto/did-dto";
+import { DIDParentDTO} from "@/lib/core/dto/did-dto";
+import type DIDGatewayOutputPort from "@/lib/core/port/secondary/did-gateway-output-port";
+
+@injectable()
+export default class ListDIDParentsUseCase extends BaseSingleEndpointStreamingUseCase<AuthenticatedRequestModel<ListDIDParentsRequest>, ListDIDParentsResponse, ListDIDParentsError, ListDIDDTO, DIDParentDTO, DIDViewModel> implements ListDIDParentsInputPort {
+   
+    constructor(
+        protected readonly presenter: ListDIDParentsOutputPort,
+        private readonly gateway: DIDGatewayOutputPort,
+    ) {
+        super(presenter);
+    }
+    
+    validateRequestModel(requestModel: AuthenticatedRequestModel<ListDIDParentsRequest>): ListDIDParentsError | undefined {
+        return undefined;
+    }
+
+    async makeGatewayRequest(requestModel: AuthenticatedRequestModel<ListDIDParentsRequest>): Promise<ListDIDDTO> {
+        const { rucioAuthToken, scope, name } = requestModel;
+        const dto: ListDIDDTO = await this.gateway.listDIDParents(rucioAuthToken, scope, name);
+        return dto;
+    }
+    
+    handleGatewayError(error: ListDIDDTO): ListDIDParentsError {
+        return {
+            status: 'error',
+            error: `Gateway retuned with ${error.errorCode}: ${error.errorMessage}`,
+            message: error.errorMessage? error.errorMessage : 'Gateway Error',
+            name: `Gateway Error`,
+            code: error.errorCode,
+        } as ListDIDParentsError
+    }
+
+    processStreamedData(dto: DIDParentDTO): { data: ListDIDParentsResponse | ListDIDParentsError; status: "success" | "error"; } {
+        const responseModel: ListDIDParentsResponse = {
+            ...dto,
+            status: 'success',
+        }
+        return {
+            status: 'success',
+            data: responseModel,
+        }
+    }
+}
