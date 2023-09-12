607ff0008bf590c7978b3a0be6be9ee61cc5b661
maany <imptodefeat@gmail.com>
2023-09-12 14:44:36 +0200

    api: list-did-parrents; add controller #317

---
+++ src/lib/infrastructure/controller/list-did-parents-controller.ts
@@ -0,0 +1,31 @@
+import { injectable, inject } from "inversify";
+import { NextApiResponse } from "next";
+
+import { AuthenticatedRequestModel } from "@/lib/sdk/usecase-models";
+import { BaseController, TAuthenticatedControllerParameters } from "@/lib/sdk/controller";
+import { ListDIDParentsRequest } from "@/lib/core/usecase-models/list-did-parents-usecase-models";
+import { ListDIDParentsInputPort } from "@/lib/core/port/primary/list-did-parents-ports";
+import USECASE_FACTORY from "@/lib/infrastructure/ioc/ioc-symbols-usecase-factory";
+
+export type ListDIDParentsControllerParameters = TAuthenticatedControllerParameters & {
+    scope: string,
+    name: string,
+};
+
+@injectable()
+class ListDIDParentsController extends BaseController<ListDIDParentsControllerParameters, AuthenticatedRequestModel<ListDIDParentsRequest>> {
+    constructor(
+        @inject(USECASE_FACTORY.LIST_DID_PARENTS) listDIDParentsUseCaseFactory: (response: NextApiResponse) => ListDIDParentsInputPort,
+    ) {
+        super(listDIDParentsUseCaseFactory);
+    }
+    prepareRequestModel(parameters: ListDIDParentsControllerParameters): AuthenticatedRequestModel<ListDIDParentsRequest> {
+        return {
+            rucioAuthToken: parameters.rucioAuthToken,
+            scope: parameters.scope,
+            name: parameters.name,
+        }
+    }
+}
+
+export default ListDIDParentsController;
