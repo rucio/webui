17ea19a2cd4b9689b23a42e9bfbc7e67df77451f
maany <imptodefeat@gmail.com>
2023-09-13 19:26:05 +0200

    feature: list-file-replicase; primary_ports #319

---
+++ src/lib/core/port/primary/list-file-replicas-ports.ts
@@ -0,0 +1,14 @@
+import { BaseAuthenticatedInputPort, BaseStreamingOutputPort } from "@/lib/sdk/primary-ports";
+import { ListFileReplicasResponse, ListFileReplicasRequest, ListFileReplicasError } from "@/lib/core/usecase-models/list-file-replicas-usecase-models";
+import { FilereplicaStateViewModel } from "@/lib/infrastructure/data/view-model/did";
+
+/**
+ * @interface ListFileReplicasInputPort that abstracts the usecase.
+ */
+export interface ListFileReplicasInputPort extends BaseAuthenticatedInputPort<ListFileReplicasRequest> {}
+
+
+/**
+ * @interface ListFileReplicasOutputPort that abtrsacts the presenter
+ */
+export interface ListFileReplicasOutputPort extends BaseStreamingOutputPort<ListFileReplicasResponse, ListFileReplicasError, FilereplicaStateViewModel> {}
