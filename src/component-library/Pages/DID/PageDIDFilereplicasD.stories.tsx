f40ed0723023bfbe2c9399bad2b1a7336776b664
Anton Schwarz <anton.schwarz@cern.ch>
2023-09-13 10:15:15 +0200

    Components: Changed type of PageDIDFileReplicasD table
    Now simply uses `DIDViewModel` instead of `FilereplicaStateDViewModel`,
    since we are simply displaying the file DIDs within the dataset.
    
    Also added PageDID Updates after reworking FileReplicaStatesD

--- src/component-library/Pages/DID/PageDIDFilereplicasD.stories.tsx
+++ src/component-library/Pages/DID/PageDIDFilereplicasD.stories.tsx
@@ -1,6 +1,7 @@
 import { StoryFn, Meta } from '@storybook/react';
 import { PageDIDFilereplicasD as PDFD } from "./PageDIDFilereplicasD"
-import { fixtureFilereplicaStateViewModel, fixtureFilereplicaStateDViewModel, mockUseComDOM } from 'test/fixtures/table-fixtures';
+import { fixtureFilereplicaStateViewModel, fixtureFilereplicaStateDViewModel, mockUseComDOM, fixtureDIDViewModel } from 'test/fixtures/table-fixtures';
+import { useEffect, useState } from 'react';
 
 export default {
     title: "Components/Pages/DID",
@@ -9,9 +10,10 @@ export default {
 
 const Template: StoryFn<typeof PDFD> = (args) => <PDFD {...args} />;
 
+
 export const PageDIDFilereplicasD = Template.bind({});
 PageDIDFilereplicasD.args = {
+    datasetComDOM: mockUseComDOM(Array.from({length: 100}, (_, i) => fixtureDIDViewModel())),
     replicaComDOM: mockUseComDOM(Array.from({length: 100}, (_, i) => fixtureFilereplicaStateViewModel())),
-    datasetComDOM: mockUseComDOM(Array.from({length: 100}, (_, i) => fixtureFilereplicaStateDViewModel())),
-    onChangeDatasetSelection: (dataset: string) => console.log("onChangeDatasetSelection", dataset),
+    onChangeFileSelection: (scope: string, name: string) => console.log("onChangeFileSelection", scope, name),
 }
