7cb805040b9dfaab510cc3fa20266f7e027ac5e3
maany <imptodefeat@gmail.com>
2023-09-18 14:39:34 +0200

    api: list-did-contents;feature #323

--- src/lib/infrastructure/ioc/ioc-symbols-input-port.ts
+++ src/lib/infrastructure/ioc/ioc-symbols-input-port.ts
@@ -8,6 +8,7 @@ const INPUT_PORT = {
     GET_SUBSCRIPTION: Symbol.for("GetSubscriptionInputPort"),
     LOGIN_CONFIG: Symbol.for("LoginConfigInputPort"),
     LIST_DIDS: Symbol.for("ListDIDsInputPort"),
+    LIST_DID_CONTENTS: Symbol.for("ListDIDContentsInputPort"),
     LIST_DID_PARENTS: Symbol.for("ListDIDParentsInputPort"),
     LIST_DID_RULES: Symbol.for("ListDIDRulesInputPort"),
     LIST_DATASET_REPLICAS: Symbol.for("ListDatasetReplicasInputPort"),
