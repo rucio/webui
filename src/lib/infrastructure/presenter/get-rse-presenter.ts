98ad546e5f7909e95ded9d884713e5d1d5378f28
maany <imptodefeat@gmail.com>
2023-09-19 12:58:19 +0200

    api: feature-get_rse; presenter #337

---
+++ src/lib/infrastructure/presenter/get-rse-presenter.ts
@@ -0,0 +1,26 @@
+import { BasePresenter } from "@/lib/sdk/presenter";
+import { GetRSEError, GetRSEResponse } from "@/lib/core/usecase-models/get-rse-usecase-models";
+import { generateEmptyRSEViewModel, RSEViewModel } from "@/lib/infrastructure/data/view-model/rse";
+
+export default class GetRSEPresenter extends BasePresenter<GetRSEResponse, GetRSEError, RSEViewModel> {
+    convertResponseModelToViewModel(responseModel: GetRSEResponse): { viewModel: RSEViewModel; status: number; } {
+        const viewModel: RSEViewModel = {
+            ...responseModel,
+        }
+        return {
+            status: 200,
+            viewModel: viewModel
+        }
+    }
+    
+    convertErrorModelToViewModel(errorModel: GetRSEError): { viewModel: RSEViewModel; status: number; } {
+        const viewModel: RSEViewModel = generateEmptyRSEViewModel();
+        const message = errorModel.message.toString() || errorModel.name;
+        viewModel.message = message;
+        const errorCode = errorModel.code || 500;
+        return {
+            status: errorCode,
+            viewModel: viewModel
+        }
+    }
+}
