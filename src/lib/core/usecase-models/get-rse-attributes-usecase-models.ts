7c8bbba93b3bf7100a9d7d9802b6df7801705724
maany <imptodefeat@gmail.com>
2023-09-19 16:38:45 +0200

    api: get-rse-attributes; usecase_models #344

---
+++ src/lib/core/usecase-models/get-rse-attributes-usecase-models.ts
@@ -0,0 +1,21 @@
+import { BaseErrorResponseModel, BaseResponseModel } from "@/lib/sdk/usecase-models"
+import { RSEAttribute } from "../entity/rucio";
+/**
+ * @interface GetRSEAttributesRequest represents the RequestModel for get_rse_attributes usecase
+*/
+export interface GetRSEAttributesRequest {
+    rseName: string;
+}
+
+/** 
+ * @interface GetRSEAttributesResponse represents the ResponseModel for get_rse_attributes usecase
+*/
+export interface GetRSEAttributesResponse extends BaseResponseModel {
+    attributes: RSEAttribute[];
+}
+
+
+/**
+* @interface GetRSEAttributesError represents the ErrorModel for get_rse_attributes usecase
+*/
+export interface GetRSEAttributesError extends BaseErrorResponseModel {}
