39cc4b34d9b79fe4ba1dd17698ea88a02af30450
maany <imptodefeat@gmail.com>
2023-09-19 12:51:51 +0200

    api: feature-get_rse; primary_ports #337

---
+++ src/lib/core/port/primary/get-rse-ports.ts
@@ -0,0 +1,12 @@
+import { BaseAuthenticatedInputPort, BaseOutputPort } from "@/lib/sdk/primary-ports";
+import { GetRSEError, GetRSERequest, GetRSEResponse } from "@/lib/core/usecase-models/get-rse-usecase-models";
+
+/**
+ * @interface GetRSEInputPort representing the GetRSE usecase.
+ */
+export interface GetRSEInputPort extends BaseAuthenticatedInputPort<GetRSERequest> {}
+
+/**
+ * @interface GetRSEOutputPort representing the GetRSE presenter.
+ */
+export interface GetRSEOutputPort extends BaseOutputPort<GetRSEResponse, GetRSEError> {}
