4b6d38aa0adfede8e0e790bfce4e132f25e01fb3
Anton Schwarz <anton.schwarz@cern.ch>
2023-09-20 11:30:29 +0200

    App: Connected ListRSE

--- src/app/(rucio)/rse/list/page.tsx
+++ src/app/(rucio)/rse/list/page.tsx
@@ -1,10 +1,37 @@
 'use client';
 import { ListRSE as ListRSEStory } from "@/component-library/Pages/RSE/ListRSE";
+import { RSEViewModel } from "@/lib/infrastructure/data/view-model/rse";
+import useComDOM from "@/lib/infrastructure/hooks/useComDOM";
+import { HTTPRequest } from "@/lib/sdk/http";
 import { mockUseComDOM, fixtureRSEViewModel } from "test/fixtures/table-fixtures";
 export default function Page() {
+
+    const setRSEQuery = async (rseExpression: string) => {
+        await RSESearchComDOM.setRequest({
+            url: new URL(`${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/list-rses`),
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            } as HeadersInit),
+            params: {
+                "rseExpression": rseExpression
+            },
+        } as HTTPRequest)
+    }
+
+    const RSESearchComDOM = useComDOM<RSEViewModel>(
+        'list-rse-query',
+        [],
+        false,
+        Infinity,
+        200,
+        true
+    )
+
     return (
         <ListRSEStory
-            comdom={mockUseComDOM(Array.from({ length: 100 }, () => fixtureRSEViewModel()))} // replace with ViewModel
+            comdom={RSESearchComDOM}
+            setRSEQuery={setRSEQuery}
         />
     )
 }
