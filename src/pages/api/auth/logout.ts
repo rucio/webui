9da2713bd0e4939e06aba252fef933cc5c7788a6
maany <imptodefeat@gmail.com>
2023-09-22 01:23:20 +0200

    auth: fix session management issues #360

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,10 +1,46 @@
 import "reflect-metadata"
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { withSessionRoute } from '@/lib/infrastructure/auth/session-utils';
+import { removeSessionUser, withSessionRoute } from '@/lib/infrastructure/auth/session-utils';
 
-const logoutRoute = (req: NextApiRequest, res: NextApiResponse) => {
-    req.session.destroy();
-    res.status(200).json({ message: 'Logged out' });
+const logoutRoute = async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = req.session;
+    const host = req.url ? req.url.split('/')[0] : '';
+    const { callbackUrl } = req.query;
+    const redirectUrl = `${host}/auth/login?callbackUrl=${callbackUrl}`
+
+    if(!session) {
+        res.status(200).json({ message: 'No session present to logout!' })
+        return
+    }
+
+    const user = session.user;
+    if(!user) {
+        res.status(200).json({ message: 'No user found!' })
+        return
+    }
+    
+    if(req.method === 'GET') {
+        await removeSessionUser(req.session, user)
+        const message = `Logged out ${user.rucioAccount}. Currently active accounts: ${session.allUsers}`;
+       
+        if(callbackUrl) {
+            res.redirect(redirectUrl as string)
+            return
+        }
+        res.status(200).json({ message })
+        return;
+    }
+
+    if(req.method === 'POST') {
+        req.session.destroy();
+        const { callbackUrl } = req.query;
+        if(callbackUrl) {
+            res.redirect(redirectUrl as string)
+            return
+        }
+        res.status(200).json({ message: 'Logged out from all acounts!' })
+        return;
+    }
 }
 
 
