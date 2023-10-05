f5270cd9f6fb392fe86b0dabe976cefc30d98cea
maany <imptodefeat@gmail.com>
2023-10-05 13:52:30 +0200

    feature: list-subscription-rule-states; usecase-models.ts #368

---
+++ src/lib/core/usecase-models/list-subscription-rule-states-usecase-models.ts
@@ -0,0 +1,19 @@
+import { BaseErrorResponseModel, BaseResponseModel } from "@/lib/sdk/usecase-models"
+import { SubscriptionRuleStates } from "../entity/rucio"
+/**
+ * @interface ListSubscriptionRuleStatesRequest represents the RequestModel for list_subscription_rule_states usecase
+*/
+export interface ListSubscriptionRuleStatesRequest {
+    account: string
+}
+
+/** 
+ * @interface ListSubscriptionRuleStatesResponse represents the ResponseModel for list_subscription_rule_states usecase
+*/
+export interface ListSubscriptionRuleStatesResponse extends SubscriptionRuleStates, BaseResponseModel {}
+
+
+/**
+* @interface ListSubscriptionRuleStatesError represents the ErrorModel for list_subscription_rule_states usecase
+*/
+export interface ListSubscriptionRuleStatesError extends BaseErrorResponseModel {}
