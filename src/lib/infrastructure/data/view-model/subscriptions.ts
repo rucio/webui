0032fc78953edc266855d1c52ceb540951fcef22
maany <imptodefeat@gmail.com>
2023-10-05 13:56:39 +0200

    feature: list-subscription-rule-states; presenter #368

--- src/lib/infrastructure/data/view-model/subscriptions.ts
+++ src/lib/infrastructure/data/view-model/subscriptions.ts
@@ -1,4 +1,4 @@
-import { Subscription, SubscriptionReplicationRule, SubscriptionState, SubscriptionRuleStates } from "@/lib/core/entity/rucio";
+import { Subscription, SubscriptionState, SubscriptionRuleStates } from "@/lib/core/entity/rucio";
 import { BaseViewModel } from "@/lib/sdk/view-models";
 
 export interface SubscriptionViewModel extends BaseViewModel, Omit<Subscription, 'replication_rules'> {
@@ -26,6 +26,19 @@ export function getEmptySubscriptionViewModel(): SubscriptionViewModel {
         updated_at: '',
         filter: '',
         replication_rules: '',
+    }
+    return viewModel
+}
+
+export function generateEmptySubscriptionRuleStatesViewModel(): SubscriptionRuleStatesViewModel {
+    const viewModel: SubscriptionRuleStatesViewModel = {
+        status: 'error',
+        name: '' ,
+        state_ok: 0,
+        state_replicating: 0,
+        state_stuck: 0,
+        state_suspended: 0,
+       
     }
     return viewModel
 }
