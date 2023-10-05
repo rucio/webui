062684a0ecf0e87ca79f22d29dfc4c02e5a8b80f
maany <imptodefeat@gmail.com>
2023-10-05 17:42:27 +0200

    add mock-list-rules nextjs endpoint #373

---
+++ src/pages/api/feature/mock-list-rules.ts
@@ -0,0 +1,25 @@
+import { withAuthenticatedSessionRoute } from "@/lib/infrastructure/auth/session-utils";
+import { NextApiRequest, NextApiResponse } from "next";
+import { fixtureRuleViewModel, generateSequenceArray } from "test/fixtures/table-fixtures";
+import { Readable, PassThrough } from "stream";
+
+async function endpoint(req: NextApiRequest, res: NextApiResponse, rucioAuthToken: string) {
+    if(req.method !== 'GET') {
+        res.status(405).json({ error: 'Method Not Allowed' })
+        return
+    }
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'text/event-stream;charset=utf-8');
+    res.setHeader('Cache-Control', 'no-cache, no-transform');
+    res.setHeader('X-Accel-Buffering', 'no');
+    res.setHeader('Connection', 'keep-alive');
+    res.setHeader('Content-Encoding', 'none');
+
+    const data = generateSequenceArray(10, fixtureRuleViewModel)
+    
+    const stream = Readable.from(data.map( (entry) => JSON.stringify(entry) + "\n"))
+    stream.pipe(res)
+}
+
+export default withAuthenticatedSessionRoute(endpoint);
