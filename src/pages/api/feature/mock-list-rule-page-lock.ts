eb9ee4202543d4654f7099ec6a1e19583fe69b46
maany <imptodefeat@gmail.com>
2023-10-06 14:48:38 +0200

    add mock-list-rule-page-lock nextjs endpoint #373

---
+++ src/pages/api/feature/mock-list-rule-page-lock.ts
@@ -0,0 +1,25 @@
+import { withAuthenticatedSessionRoute } from "@/lib/infrastructure/auth/session-utils";
+import { NextApiRequest, NextApiResponse } from "next";
+import { fixtureRulePageLockEntryViewModel, generateSequenceArray } from "test/fixtures/table-fixtures";
+import { Readable } from "stream";
+
+async function endpoint(req: NextApiRequest, res: NextApiResponse, rucioAuthToken: string) {
+    if(req.method !== 'POST') {
+        res.status(405).json({ error: 'Method Not Allowed' })
+        return
+    }
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'text/event-stream;charset=utf-8');
+    res.setHeader('Cache-Control', 'no-cache, no-transform');
+    res.setHeader('X-Accel-Buffering', 'no');
+    res.setHeader('Connection', 'keep-alive');
+    res.setHeader('Content-Encoding', 'none');
+
+    const data = generateSequenceArray(10, fixtureRulePageLockEntryViewModel)
+    
+    const stream = Readable.from(data.map( (entry) => JSON.stringify(entry) + "\n"))
+    stream.pipe(res)
+}
+
+export default withAuthenticatedSessionRoute(endpoint);
