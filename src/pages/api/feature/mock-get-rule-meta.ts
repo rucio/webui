bce724233d2d8428fd6dc484c0a7f07ffd773280
maany <imptodefeat@gmail.com>
2023-10-06 14:53:05 +0200

    add mock-get-rule-meta nextjs endpoint #373

---
+++ src/pages/api/feature/mock-get-rule-meta.ts
@@ -0,0 +1,13 @@
+import { withAuthenticatedSessionRoute } from "@/lib/infrastructure/auth/session-utils";
+import { NextApiRequest, NextApiResponse } from "next";
+import { fixtureRuleMetaViewModel, generateSequenceArray } from "test/fixtures/table-fixtures";
+
+async function endpoint(req: NextApiRequest, res: NextApiResponse, rucioAuthToken: string) {
+    if(req.method !== 'GET') {
+        res.status(405).json({ error: 'Method Not Allowed' })
+        return
+    }
+    res.status(200).json(fixtureRuleMetaViewModel())
+}
+
+export default withAuthenticatedSessionRoute(endpoint);
