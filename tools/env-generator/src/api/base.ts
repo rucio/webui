1d56ad35feac8ca33dd741a2ea91debbe11bfe0e
maany <imptodefeat@gmail.com>
2023-10-12 18:52:37 +0200

    create a env-generator cli

---
+++ tools/env-generator/src/api/base.ts
@@ -0,0 +1,216 @@
+import { existsSync } from "fs";
+import { Liquid } from "liquidjs";
+import { readFileSync, writeFileSync } from "fs";
+import crypto from "crypto";
+
+
+export type TTemplateGeneratorOutput = {
+    status: boolean,
+    message: string
+    content: string
+    file: string
+  
+}
+
+export type EnvValidationError = {
+    message: string
+    type: 'error' | 'warning'
+}
+
+export class BaseCompiledTemplate {
+    protected engine: Liquid
+
+    constructor(
+        protected environmentVariables: Record<string, string>,
+        protected outputFilePath: string,
+        protected templateName: string,
+        protected write: boolean = false
+    ) {
+        const templateDir = `${__dirname}/../templates`
+        const templatePath = `${templateDir}/${templateName}.liquid`
+        this.environmentVariables = environmentVariables
+
+        if(!existsSync(templatePath)) throw new Error(`templatePath ${templatePath} does not exist`)
+        this.engine = new Liquid({
+            root: templateDir,
+            extname: '.liquid'
+        })
+
+    } 
+
+    generateRandomString = (): string => {
+        const length = 32
+        const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        const randomBytes = crypto.randomBytes(length)
+        const result = new Array(length)
+        let cursor = 0
+      
+        for (let i = 0; i < length; i++) {
+          cursor += randomBytes[i]
+          result[i] = chars[cursor % chars.length]
+        }
+      
+        return result.join('')
+    }
+    
+    mergeWithDefaults(): void {
+        this.environmentVariables = {
+            ...this.environmentVariables,
+            'SESSION_COOKIE_NAME': 'rucio-webui-session',
+            'SESSION_COOKIE_PASSWORD': this.generateRandomString()
+        }
+    }
+    validateOIDCProvider(provider: string, global_prefix: string): EnvValidationError[] {
+        const prefix = `OIDC_${provider.toUpperCase()}_`
+        const requiredVariables: string[] = ["client_id", "client_secret", "authorization_url", "token_url", "userinfo_url", "redirect_url"]
+        const errors: EnvValidationError[] = []
+        requiredVariables.forEach((key) => {
+            const varName = `${prefix}${key.toUpperCase()}`
+            if(!this.environmentVariables[varName]) {
+                errors.push({
+                    type: 'error',
+                    message: `required variable ${global_prefix}${varName} is not set. Please set a value for ${global_prefix}${varName}`
+                })
+            }
+        })
+        return errors
+    }
+    validateVO(vo: string, global_prefix: string): EnvValidationError[] {
+        const prefix = `VO_${vo.toUpperCase()}_`
+        const requiredVariables: string[] = ["name"]
+        const optionalVariables: string[] = ["logo", "oidc_enabled", "oidc_providers"]
+        const errors: EnvValidationError[] = []
+        requiredVariables.forEach((key) => {
+            const varName = `${prefix}${key.toUpperCase()}`
+            if(!this.environmentVariables[varName]) {
+                errors.push({
+                    type: 'error',
+                    message: `required variable ${global_prefix}${varName} is not set. Please set a value for ${global_prefix}${varName}`
+                })
+            }
+        })
+        // check if OIDC is enabled for VO
+        const oidcEnabledVarName = `${prefix}OIDC_ENABLED`
+        const oidcProvidersVarName = `${prefix}OIDC_PROVIDERS`
+        if(this.environmentVariables[oidcEnabledVarName] === 'true') {
+            if(!this.environmentVariables[oidcProvidersVarName]) {
+                errors.push({
+                    type: 'error',
+                    message: `required variable ${global_prefix}${oidcProvidersVarName} is not set. Please set a value for ${global_prefix}${oidcProvidersVarName} as a csv string of oidc provider names`
+                })
+            } else {
+            const voOIDCProviders = this.environmentVariables[oidcProvidersVarName].split(',')
+            voOIDCProviders.forEach((oidcProvider) => {
+                const oidcErrors = this.validateOIDCProvider(oidcProvider, prefix)
+                oidcErrors.forEach((error) => {
+                    errors.push(error)
+                })
+            })
+            }
+        }
+        optionalVariables.forEach((key) => {
+            const varName = `${prefix}${key.toUpperCase()}`
+            if(!this.environmentVariables[varName]) {
+                errors.push({
+                    type: 'warning',
+                    message: `optional variable ${global_prefix}${varName} is not set. Please set a value for ${global_prefix}${varName} if needed`
+                })
+            }
+        })
+        return errors
+    }
+    validateEnv(prefix: string): EnvValidationError[] {
+        console.log('validating environment variables')
+        console.log(this.environmentVariables)
+        const env = this.environmentVariables
+        const errors: EnvValidationError[] = []
+
+        const requiredVariables: string[] = ["rucio_host", "rucio_auth_host", "host", "project_url"]
+      
+        // check if all required variables are set
+        requiredVariables.forEach((key) => {
+          let keyName = `${key.toUpperCase()}`
+          console.log(`checking if ${keyName} is set`)
+          console.log(env[keyName])
+          if (!env[key.toUpperCase()]) {
+            errors.push({
+              type: 'error',
+              message: `required variable ${key.toUpperCase()} is not set. Please set a value for ${prefix.toUpperCase()}${key.toUpperCase()}`
+            })
+          }
+        })
+      
+        // check if multi vo is enabled, all vo variables are set
+        if (env['MULTI_VO_ENABLED'] === 'true') {
+          // check if vo_list is defined and a default VO is set
+          if (!env['VO_LIST'] || !env['VO_DEFAULT']) {
+            if(!env['VO_LIST']) {
+                errors.push({
+                  type: 'error',
+                  message: `Multi VO is enabled. Please set a value for ${prefix}VO_LIST as csv string of vo short names`
+              })
+            }
+            if(!env['VO_DEFAULT']) {
+                errors.push({
+                  type: 'error',
+                  message: `Please set a value for ${prefix}VO_DEFAULT as the default vo's short name`
+              })
+            }
+          } else{
+            const voList = env['VO_LIST'].split(',')
+            voList.forEach((vo) => {
+              const voErrors = this.validateVO(vo, prefix)
+              voErrors.forEach((error) => {
+                errors.push(error)
+              })
+            })
+          }
+        }
+
+        // check if oidc is enabled, all oidc variables are set
+        if (env['OIDC_ENABLED'] === 'true') {
+          // check if oidc_providers is defined
+          if (!env['OIDC_PROVIDERS']) {
+            errors.push({
+              type: 'error',
+              message: `OIDC is enabled. Please set a value for ${prefix}OIDC_PROVIDERS as csv string of oidc provider names`
+            })
+          } else{
+            const oidcProviders = env['OIDC_PROVIDERS'].split(',')
+            oidcProviders.forEach((oidcProvider) => {
+              const oidcErrors = this.validateOIDCProvider(oidcProvider, prefix)
+              oidcErrors.forEach((error) => {
+                errors.push(error)
+              })
+            })
+          }
+        }
+        return errors
+    }
+
+    execute(): TTemplateGeneratorOutput {
+        try {
+            console.log(`rendering template ${this.templateName}`)
+            const content = this.engine.renderFileSync(this.templateName, this.environmentVariables)
+            if(this.write) {
+                writeFileSync(this.outputFilePath, content)
+            }
+            console.log(`output file ${this.outputFilePath} created`)
+            return {
+                status: true,
+                message: `output file ${this.outputFilePath} created`,
+                content: content,
+                file: this.outputFilePath
+            }
+        } catch(e: unknown) {
+            console.log(`error rendering template ${this.templateName}`)
+            console.log(e as Error)
+            return {
+                status: false,
+                message: `error rendering template ${this.templateName}`,
+                content: e as string,
+                file: this.outputFilePath
+            }
+        }
+    }
+}
