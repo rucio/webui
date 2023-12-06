892b215ea35e6ad5e3a0d4e35538139927a2d773
maany <imptodefeat@gmail.com>
2023-12-06 11:43:22 +0100

    add domain services for did and rules

---
+++ src/lib/core/domain-service/did-domain-services.ts
@@ -0,0 +1,33 @@
+import { AccountType, DID, DIDType } from "../entity/rucio";
+
+/**
+ * Generates a new scope name for the account
+ * @param account The rucio account name
+ * @param accountType The rucio account type
+ * @returns A new scope name for the account
+ */
+export const generateNewScope = (account: string, accountType: AccountType): string => {
+    if(accountType === AccountType.USER) {
+        return `user.${account}`;
+    }else if(accountType === AccountType.GROUP) {
+        return `group.${account}`;
+    } else {
+        throw new Error(`Invalid account type for rule creation: ${accountType}`);
+    }
+}
+
+
+/**
+ * Generates a new DID name for the given DID
+ * @param newScope A new scope name for the account that should be used for the derived DID
+ * @param selectedDID The DID that the derived DID is being created from
+ * @returns A DID object with type DIDType.DERIVED 
+ */
+export const generateDerivedDIDName = (newScope: string, selectedDID: DID): DID => {
+    const newName = `${newScope}.${selectedDID.name}_der${Date.now() / 1000}`;
+    return {
+        name: newName,
+        scope: newScope,
+        did_type: DIDType.DERIVED,
+    } as DID
+}
