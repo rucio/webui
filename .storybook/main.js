662f926526103b44b8d995894a2b7e7e609c0ceb
maany <imptodefeat@gmail.com>
2023-12-13 17:02:59 +0100

    upgrade storybook to 7.6

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,18 +3,25 @@ module.exports = {
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
+
   framework: {
     name: "@storybook/nextjs",
     options: {}
   },
+
   resolve: {
     fallback: {
       util: require.resolve("util")
     }
   },
+
+  docs: {
+    autodocs: true
+  }
 };
--- src/component-library/Pages/Rule/LifetimeWithExpirationDateInput.stories.tsx
+++ src/component-library/Pages/Rule/LifetimeWithExpirationDateInput.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryFn } from "@storybook/react";
 import { LifetimeWithExpirationDateInput } from "./LifetimeWithExpiryDateInput";
 
 export default {
-    "title": "Components/Pages/Rule/Components",
+    title: "Components/Pages/Rule/Components",
     component: LifetimeWithExpirationDateInput,
 } as Meta<typeof LifetimeWithExpirationDateInput>;
 
