e96622223239ac265697b0a3960788481504bfc9
maany <imptodefeat@gmail.com>
2024-02-27 15:28:47 +0100

    viewmodel: rename rule.d.ts to rule.ts #303

--- src/lib/infrastructure/data/view-model/rule.ts
+++ src/lib/infrastructure/data/view-model/rule.ts
@@ -1,6 +1,8 @@
-import { AccountInfo, Rule } from "@/lib/core/entity/rucio";
+import { AccountInfo, DIDLong, DIDType, Rule } from "@/lib/core/entity/rucio";
 import { RulePageLockEntry, RuleMeta } from "@/lib/core/entity/rucio";
 import { BaseViewModel } from "@/lib/sdk/view-models";
+import { DIDName, RSEName } from "./create-rule";
+import { RSEAccountUsageLimitViewModel } from "./rse";
 
 export interface RuleViewModel extends Rule, BaseViewModel {}
 export interface RulePageLockEntryViewModel extends RulePageLockEntry, BaseViewModel {}
