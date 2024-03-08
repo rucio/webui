d3869d521da01a313baa85c104a23c089d845e86
Eraldo Junior <eusoueraldo@gmail.com>
2024-03-08 15:54:22 -0300

    Components: Add responsiveness on PageRSEProtocols

--- src/component-library/Pages/RSE/PageRSEProtocols.tsx
+++ src/component-library/Pages/RSE/PageRSEProtocols.tsx
@@ -31,7 +31,8 @@ export const PageRSEProtocols = (
         columnHelper.accessor("hostname", {
             id: "hostname",
             header: info => <H3>Hostname</H3>,
-            cell: info => <P className="break-all pr-1">{info.getValue()}</P>
+            cell: info => <P className="break-all pr-1">{info.getValue()}</P>,
+            meta: { style: "md:w-26 sm:w-24" }
         }),
         columnHelper.accessor("port", {
             id: "port",
@@ -46,38 +47,44 @@ export const PageRSEProtocols = (
         }),
         columnHelper.accessor("priorities_lan.read", {
             id: "priorities_lan.read",
-            header: info => <TableSortUpDown name="LAN/R" column={info.column} stack />,
-            cell: info => <P className="break-all pr-1 text-right">{info.getValue()}</P>,
+            header: info => (responsive.md && !responsive.lg) ? <TableSortUpDown name="L/R" column={info.column} stack /> :
+            <TableSortUpDown name="LAN/R" column={info.column} stack />,
+            cell: info => <P className="break-all pr-1 text-center">{info.getValue()}</P>,
             meta: shortstyleblue
         }),
         columnHelper.accessor("priorities_lan.write", {
             id: "priorities_lan.write",
-            header: info => <TableSortUpDown name="LAN/W" column={info.column} stack />,
-            cell: info => <P className="break-all pr-1 text-right">{info.getValue()}</P>,
+            header: info => (responsive.md && !responsive.lg) ? <TableSortUpDown name="L/W" column={info.column} stack />:
+            <TableSortUpDown name="LAN/W" column={info.column} stack />,
+            cell: info => <P className="break-all pr-1 text-center">{info.getValue()}</P>,
             meta: shortstyleblue
         }),
         columnHelper.accessor("priorities_lan.delete", {
             id: "priorities_lan.delete",
-            header: info => <TableSortUpDown name="LAN/D" column={info.column} stack />,
-            cell: info => <P className="break-all pr-1 text-right">{info.getValue()}</P>,
+            header: info => (responsive.md && !responsive.lg) ? <TableSortUpDown name="L/D" column={info.column} stack />:
+            <TableSortUpDown name="LAN/D" column={info.column} stack />,
+            cell: info => <P className="break-all pr-1 text-center">{info.getValue()}</P>,
             meta: shortstyleblue
         }),
         columnHelper.accessor("priorities_wan.read", {
             id: "priorities_wan.read",
-            header: info => <TableSortUpDown name="WAN/R" column={info.column} stack />,
-            cell: info => <P className="break-all pr-1 text-right">{info.getValue()}</P>,
+            header: info => (responsive.md && !responsive.lg) ? <TableSortUpDown name="W/R" column={info.column} stack />:
+            <TableSortUpDown name="WAN/R" column={info.column} stack />,
+            cell: info => <P className="break-all pr-1 text-center">{info.getValue()}</P>,
             meta: shortstylepink
         }),
         columnHelper.accessor("priorities_wan.write", {
             id: "priorities_wan.write",
-            header: info => <TableSortUpDown name="WAN/W" column={info.column} stack />,
-            cell: info => <P className="break-all pr-1 text-right">{info.getValue()}</P>,
+            header: info => (responsive.md && !responsive.lg) ? <TableSortUpDown name="W/W" column={info.column} stack />:
+            <TableSortUpDown name="WAN/W" column={info.column} stack />,
+            cell: info => <P className="break-all pr-1 text-center">{info.getValue()}</P>,
             meta: shortstylepink
         }),
         columnHelper.accessor("priorities_wan.delete", {
             id: "priorities_wan.delete",
-            header: info => <TableSortUpDown name="WAN/D" column={info.column} stack />,
-            cell: info => <P className="break-all pr-1 text-right">{info.getValue()}</P>,
+            header: info => (responsive.md && !responsive.lg) ? <TableSortUpDown name="W/D" column={info.column} stack />:
+            <TableSortUpDown name="WAN/D" column={info.column} stack />,
+            cell: info => <P className="break-all pr-1 text-center">{info.getValue()}</P>,
             meta: shortstylepink,
         }),
         // columnHelper.accessor("priorities_wan.tpc", {
@@ -88,14 +95,16 @@ export const PageRSEProtocols = (
         // }),
         columnHelper.accessor("priorities_wan.tpcwrite", {
             id: "priorities_wan.tpcwrite",
-            header: info => <TableSortUpDown name="TPC/W" column={info.column} stack />,
-            cell: info => <P className="break-all pr-1 text-right">{info.getValue()}</P>,
+            header: info => (responsive.md && !responsive.lg) ? <TableSortUpDown name="T/W" column={info.column} stack />:
+            <TableSortUpDown name="TPC/W" column={info.column} stack />,
+            cell: info => <P className="break-all pr-1 text-center">{info.getValue()}</P>,
             meta: shortstylepink,
         }),
         columnHelper.accessor("priorities_wan.tpcread", {
             id: "priorities_wan.tpcread",
-            header: info => <TableSortUpDown name="TPC/R" column={info.column} stack />,
-            cell: info => <P className="break-all pr-1 text-right">{info.getValue()}</P>,
+            header: info => (responsive.md && !responsive.lg) ?  <TableSortUpDown name="T/R" column={info.column} stack />:
+            <TableSortUpDown name="TPC/R" column={info.column} stack />,
+            cell: info => <P className="break-all pr-1 text-center">{info.getValue()}</P>,
             meta: shortstylepink,
         }),
     ]
@@ -106,46 +115,14 @@ export const PageRSEProtocols = (
             tablecolumns={tablecolumns}
             tablestyling={{
                 visibility: {
-                    "priorities_lan.read": responsive.lg,
-                    "priorities_lan.write": responsive.lg,
-                    "priorities_lan.delete": responsive.lg,
-                    "priorities_wan.read": responsive.lg,
-                    "priorities_wan.write": responsive.lg,
-                    "priorities_wan.delete": responsive.lg,
-                    "priorities_wan.tpcwrite": responsive.lg,
-                    "priorities_wan.tpcread": responsive.lg,
-                    "port": responsive.md,
-                    "prefix": responsive.md,
+                    "port": responsive.lg,
+                    "prefix": responsive.lg,
                 },
                 pageSize: 5,
             } as TableStyling}
             tableselecting={{
                 enableRowSelection: !responsive.lg,
                 handleChange: (rows: RSEProtocol[]) => {},
-                breakOut: {
-                    breakoutVisibility: !responsive.lg,
-                    keys: responsive.md ? {
-                        "priorities_lan.read": "LAN/R",
-                        "priorities_lan.write": "LAN/W",
-                        "priorities_lan.delete": "LAN/D",
-                        "priorities_wan.read": "WAN/R",
-                        "priorities_wan.write": "WAN/W",
-                        "priorities_wan.delete": "WAN/D",
-                        "priorities_wan.tpcwrite": "TPC/W",
-                        "priorities_wan.tpcread": "TPC/R",
-                    } : {
-                        "port": "Port",
-                        "prefix": "Prefix",
-                        "priorities_lan.read": "LAN/R",
-                        "priorities_lan.write": "LAN/W",
-                        "priorities_lan.delete": "LAN/D",
-                        "priorities_wan.read": "WAN/R",
-                        "priorities_wan.write": "WAN/W",
-                        "priorities_wan.delete": "WAN/D",
-                        "priorities_wan.tpcwrite": "TPC/W",
-                        "priorities_wan.tpcread": "TPC/R",
-                    }
-                }
             }
             }
         />
