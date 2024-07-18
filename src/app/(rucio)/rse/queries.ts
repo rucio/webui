8e2516bb5fa34298b5dc0327d0f0b8eb24e37498
maany <imptodefeat@gmail.com>
2024-07-18 08:36:00 +0200

    http: remove interface HTTPRequest from client side usage #259
    Certain modules like http, https, agent are not available client side

--- src/app/(rucio)/rse/queries.ts
+++ src/app/(rucio)/rse/queries.ts
@@ -1,8 +1,7 @@
 import { RSEAttributeViewModel, RSEProtocolViewModel, RSEViewModel } from "@/lib/infrastructure/data/view-model/rse";
-import { HTTPRequest, prepareRequestArgs } from "@/lib/sdk/http";
 
 export async function getRSE(rseName: string): Promise<RSEViewModel> {
-    const req: HTTPRequest = {
+    const req: any = {
         method: "GET",
         url: new URL(`${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse`),
         params: {
@@ -12,8 +11,7 @@ export async function getRSE(rseName: string): Promise<RSEViewModel> {
             'Content-Type': 'application/json',
         } as HeadersInit)
     }
-    const { url, requestArgs } = prepareRequestArgs(req)
-    const res = await fetch(url, {
+    const res = await fetch(req.url, {
         method: "GET",
         headers: new Headers({
             'Content-Type': 'application/json',
@@ -23,7 +21,7 @@ export async function getRSE(rseName: string): Promise<RSEViewModel> {
 }
 
 export async function getProtocols(rseName: string): Promise<RSEProtocolViewModel> {
-    const req: HTTPRequest = {
+    const req: any = {
         method: "GET",
         url: new URL(`${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse-protocols`),
         params: {
@@ -33,8 +31,7 @@ export async function getProtocols(rseName: string): Promise<RSEProtocolViewMode
             'Content-Type': 'application/json',
         } as HeadersInit)
     }
-    const { url, requestArgs } = prepareRequestArgs(req)
-    const res = await fetch(url, {
+    const res = await fetch(req.url, {
         method: "GET",
         headers: new Headers({
             'Content-Type': 'application/json',
@@ -44,7 +41,7 @@ export async function getProtocols(rseName: string): Promise<RSEProtocolViewMode
 }
 
 export async function getAttributes(rseName:string): Promise<RSEAttributeViewModel> {
-    const req: HTTPRequest = {
+    const req: any = {
         method: "GET",
         url: new URL(`${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse-attributes`),
         params: {
@@ -54,8 +51,7 @@ export async function getAttributes(rseName:string): Promise<RSEAttributeViewMod
             'Content-Type': 'application/json',
         } as HeadersInit)
     }
-    const { url, requestArgs } = prepareRequestArgs(req)
-    const res = await fetch(url, {
+    const res = await fetch(req.url, {
         method: "GET",
         headers: new Headers({
             'Content-Type': 'application/json',
