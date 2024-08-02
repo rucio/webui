269fba1f963dc3620ccbfe60b77e33b642e394ec
MytsV <mytsvictoria@gmail.com>
2024-08-02 14:26:33 +0300

    Fix improper handling of requests on RSE page

--- src/app/(rucio)/rse/queries.ts
+++ src/app/(rucio)/rse/queries.ts
@@ -1,62 +1,19 @@
 import { RSEAttributeViewModel, RSEProtocolViewModel, RSEViewModel } from "@/lib/infrastructure/data/view-model/rse";
 
 export async function getRSE(rseName: string): Promise<RSEViewModel> {
-    const req: any = {
-        method: "GET",
-        url: new URL(`${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse`),
-        params: {
-            "rseName": rseName
-        },
-        headers: new Headers({
-            'Content-Type': 'application/json',
-        } as HeadersInit)
-    }
-    const res = await fetch(req.url, {
-        method: "GET",
-        headers: new Headers({
-            'Content-Type': 'application/json',
-        } as HeadersInit)
-    })
+    const url = `${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse?` + new URLSearchParams({rseName})
+    const res = await fetch(url, {method: "GET"})
     return await res.json()
 }
 
 export async function getProtocols(rseName: string): Promise<RSEProtocolViewModel> {
-    const req: any = {
-        method: "GET",
-        url: new URL(`${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse-protocols`),
-        params: {
-            "rseName": rseName
-        },
-        headers: new Headers({
-            'Content-Type': 'application/json',
-        } as HeadersInit)
-    }
-    const res = await fetch(req.url, {
-        method: "GET",
-        headers: new Headers({
-            'Content-Type': 'application/json',
-        } as HeadersInit)
-    })
+    const url = `${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse-protocols?` + new URLSearchParams({rseName})
+    const res = await fetch(url, {method: "GET"})
     return await res.json()
 }
 
 export async function getAttributes(rseName:string): Promise<RSEAttributeViewModel> {
-    const req: any = {
-        method: "GET",
-        url: new URL(`${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse-attributes`),
-        params: {
-            "rseName": rseName
-        },
-        headers: new Headers({
-            'Content-Type': 'application/json',
-        } as HeadersInit)
-    }
-    const res = await fetch(req.url, {
-        method: "GET",
-        headers: new Headers({
-            'Content-Type': 'application/json',
-        } as HeadersInit)
-    })
+    const url = `${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse-attributes?` + new URLSearchParams({rseName})
+    const res = await fetch(url, {method: "GET"})
     return await res.json()
-    
 }
