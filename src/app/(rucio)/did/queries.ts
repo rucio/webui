4068eafc2c60af50506cde877f9b7c7f54ed55f7
MytsV <mytsvictoria@gmail.com>
2024-08-02 18:29:52 +0300

    Fix invalid handling of requests

--- src/app/(rucio)/did/queries.ts
+++ src/app/(rucio)/did/queries.ts
@@ -1,47 +1,13 @@
 import { DIDKeyValuePairsDataViewModel, DIDMetaViewModel } from "@/lib/infrastructure/data/view-model/did";
 
 export async function didMetaQueryBase(scope: string, name: string): Promise<DIDMetaViewModel> {
-    const req: any = {
-        method: "GET",
-        url: new URL(`${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-did-meta`),
-        params: {
-            "scope": scope,
-            "name": name
-        },
-        headers: new Headers({
-            'Content-Type': 'application/json',
-        } as HeadersInit)
-    }
-
-    const res = await fetch(req.url, {
-        method: "GET",
-        headers: new Headers({
-            'Content-Type': 'application/json',
-        } as HeadersInit)
-    })
-
+    const url = `${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-did-meta?` + new URLSearchParams({scope, name})
+    const res = await fetch(url)
     return await res.json()
 }
 
 export async function didKeyValuePairsDataQuery(scope: string, name: string): Promise<DIDKeyValuePairsDataViewModel> {
-    const req: any = {
-        method: "GET",
-        url: new URL(`${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-did-keyvaluepairs`),
-        params: {
-            "scope": scope,
-            "name": name
-        },
-        headers: new Headers({
-            'Content-Type': 'application/json',
-        } as HeadersInit)
-    }
-
-    const res = await fetch(req.url, {
-        method: "GET",
-        headers: new Headers({
-            'Content-Type': 'application/json',
-        } as HeadersInit)
-    })
-
+    const url = `${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-did-keyvaluepairs?` + new URLSearchParams({scope, name})
+    const res = await fetch(url)
     return await res.json()
 }
