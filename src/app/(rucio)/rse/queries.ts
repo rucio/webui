cfd0e483c57540655c10ba248acac57e4a784dc5
MytsV <mytsvictoria@gmail.com>
2024-08-02 14:42:34 +0300

    Remove unnecessary request method specification

--- src/app/(rucio)/rse/queries.ts
+++ src/app/(rucio)/rse/queries.ts
@@ -2,18 +2,18 @@ import { RSEAttributeViewModel, RSEProtocolViewModel, RSEViewModel } from "@/lib
 
 export async function getRSE(rseName: string): Promise<RSEViewModel> {
     const url = `${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse?` + new URLSearchParams({rseName})
-    const res = await fetch(url, {method: "GET"})
+    const res = await fetch(url)
     return await res.json()
 }
 
 export async function getProtocols(rseName: string): Promise<RSEProtocolViewModel> {
     const url = `${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse-protocols?` + new URLSearchParams({rseName})
-    const res = await fetch(url, {method: "GET"})
+    const res = await fetch(url)
     return await res.json()
 }
 
 export async function getAttributes(rseName:string): Promise<RSEAttributeViewModel> {
     const url = `${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/get-rse-attributes?` + new URLSearchParams({rseName})
-    const res = await fetch(url, {method: "GET"})
+    const res = await fetch(url)
     return await res.json()
 }
