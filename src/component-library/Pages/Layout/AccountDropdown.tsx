6d8fadc000c95dcc843d51927d58d21b5b825b1f
MytsV <mytsvictoria@gmail.com>
2024-08-08 13:51:38 +0300

    Change dropdown structure

--- src/component-library/Pages/Layout/AccountDropdown.tsx
+++ src/component-library/Pages/Layout/AccountDropdown.tsx
@@ -1,7 +1,67 @@
-import { twMerge } from "tailwind-merge"
-import { ForwardedRef, forwardRef, useState } from "react"
-import { HiCog, HiSwitchHorizontal, HiLogout } from "react-icons/hi"
+import {twMerge} from "tailwind-merge"
+import {ForwardedRef, forwardRef, useState} from "react"
+import {HiLogin, HiSwitchHorizontal, HiLogout, HiUserAdd} from "react-icons/hi"
 import Link from "next/link"
+
+const AccountList = (props: { accountList: string[] }) => {
+    return <div className="flex flex-col">
+        {
+            props.accountList.map((account, index) => {
+                return (
+                    <Link
+                        className={twMerge(
+                            "text-text-600 hover:bg-neutral-200 hover:cursor-pointer",
+                            "dark:text-text-300 dark:hover:bg-neutral-600",
+                            "flex items-center justify-between py-2 px-1 space-x-4",
+                            "text-right"
+                        )}
+                        key={index}
+                        href="/api/account/switch"
+                        prefetch={false}
+                    >
+                        <HiSwitchHorizontal className="text-2xl text-text-900 dark:text-text-100 shrink-0"/>
+                        <span>
+                            <span>Switch to </span>
+                            <b className="text-text-800 dark:text-text-100">{account}</b>
+                        </span>
+                    </Link>
+                )
+            })
+        }
+    </div>
+};
+
+const SignOutOfAllButton = () => {
+    return <Link
+        className={twMerge(
+            "text-text-800 hover:bg-base-warning-200 hover:cursor-pointer",
+            "dark:text-text-100 dark:hover:bg-base-warning-600",
+            "flex items-center justify-between py-2 px-1 space-x-4",
+            "text-right",
+        )}
+        href="/api/auth/logout"
+        prefetch={false}
+    >
+        <span><b>Sign out</b> of all accounts</span>
+        <HiLogout className="dark:text-text-100 text-2xl text-text-900 shrink-0"/>
+    </Link>
+}
+
+const SignIntoButton = () => {
+    return <Link
+        className={twMerge(
+            "text-text-800 hover:bg-base-warning-200 hover:cursor-pointer",
+            "dark:text-text-100 dark:hover:bg-base-warning-600",
+            "flex items-center justify-between py-2 px-1 space-x-4",
+            "text-right",
+        )}
+        href="/auth/login"
+        prefetch={false}
+    >
+        <span>Sign <b>into</b> another account</span>
+        <HiUserAdd className="dark:text-text-100 text-2xl text-text-900 shrink-0"/>
+    </Link>
+}
 
 export const AccountDropdown = forwardRef(function AccountDropdown
     (
@@ -12,79 +72,48 @@ export const AccountDropdown = forwardRef(function AccountDropdown
         },
         ref: ForwardedRef<HTMLDivElement>
     ) {
-    return (
+        return (
 
-        <div
-            className={twMerge("flex flex-col p-2 sm:w-fit",
-                "rounded-md border shadow-md",
-                props.isProfileOpen ? "visible" : "invisible",
-                "absolute top-10 right-0",
-                "divide-y",
-                "bg-neutral-100 dark:bg-neutral-800",
-                "z-[100]"
-            )}
-            onMouseEnter={e => e.preventDefault()}
-            ref={ref}
-        >
-            <Link
-                className={twMerge(
-                    "text-text-600 hover:bg-neutral-200 hover:cursor-pointer",
-                    "dark:text-text-300 dark:hover:bg-neutral-600",
-                    "flex items-center justify-between py-2 px-1 space-x-4",
-                    "text-right"
-                )}
-                href="/accountsettings"
-                prefetch={false}
-            >
-                <HiCog className="text-3xl text-text-600 dark:text-text-100 shrink-0" />
-                <span>
-                    <span>Settings for </span>
-                    <b className="text-text-800 dark:text-text-100">{props.accountActive}</b>
-                </span>
-            </Link>
             <div
-                className="flex flex-col"
-            >
-                {
-                    (props.accountsPossible.filter(
-                        (account) => account !== props.accountActive
-                    )).map((account, index) => {
-                        return (
-                            <Link
-                                className={twMerge(
-                                    "text-text-600 hover:bg-neutral-200 hover:cursor-pointer",
-                                    "dark:text-text-300 dark:hover:bg-neutral-600",
-                                    "flex items-center justify-between py-2 px-1 space-x-4",
-                                    "text-right"
-                                )}
-                                key={index}
-                                href="/api/account/switch"
-                                prefetch={false}
-                            >
-                                <HiSwitchHorizontal className="text-2xl text-text-900 dark:text-text-100 shrink-0" />
-                                <span>
-                                    <span>Switch to </span>
-                                    <b className="text-text-800 dark:text-text-100">{account}</b>
-                                </span>
-                            </Link>
-                        )
-                    })
-                }
-            </div>
-            <Link
-                className={twMerge(
-                    "text-text-800 hover:bg-base-warning-200 hover:cursor-pointer",
-                    "dark:text-text-100 dark:hover:bg-base-warning-600",
-                    "flex items-center justify-between py-2 px-1 space-x-4",
-                    "text-right",
+                className={twMerge("flex flex-col p-2 sm:w-fit",
+                    "rounded-md border shadow-md",
+                    props.isProfileOpen ? "visible" : "invisible",
+                    "absolute top-10 right-0",
+                    "divide-y",
+                    "bg-neutral-100 dark:bg-neutral-800",
+                    "z-[100]",
                     "w-64 sm:w-96"
                 )}
-                href="/api/auth/logout"
-                prefetch={false}
+                onMouseEnter={e => e.preventDefault()}
+                ref={ref}
             >
-                <b>Logout</b> <HiLogout className="text-3xl text-text-900 shrink-0" />
-            </Link>
-        </div>
-    )
-}
+                <div
+                    className={twMerge(
+                        "text-text-600 hover:cursor-pointer",
+                        "dark:text-text-300",
+                        "flex justify-between items-center py-4 px-1 space-x-4",
+                        "text-right"
+                    )}
+                >
+                <span className="text-xl">
+                    <span>Hello, </span>
+                    <b className="text-text-800 dark:text-text-100">{props.accountActive}</b>!
+                </span>
+                    <div
+                        className={twMerge(
+                            "bg-neutral-200 hover:bg-base-warning-600",
+                            "p-1",
+                            "rounded-md"
+                        )}
+                    >
+                        <HiLogout className="text-2xl text-text-900 shrink-0"/>
+                    </div>
+                </div>
+                {props.accountsPossible.length !== 1 &&
+                        <AccountList accountList={props.accountsPossible.filter(account => account !== props.accountActive)}/>
+                }
+                <SignIntoButton/>
+            </div>
+        )
+    }
 )
