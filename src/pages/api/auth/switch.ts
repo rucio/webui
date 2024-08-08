75327035d2c077f62a54c57cc82d79fe3d137814
MytsV <mytsvictoria@gmail.com>
2024-08-08 15:53:46 +0300

    Implement switching users in session

---
+++ src/pages/api/auth/switch.ts
@@ -0,0 +1,31 @@
+import {addOrUpdateSessionUser, withSessionRoute} from "@/lib/infrastructure/auth/session-utils"
+import { IronSession } from "iron-session"
+import { NextApiRequest, NextApiResponse } from "next"
+
+async function switchUsers(req: NextApiRequest, res: NextApiResponse) {
+    const session: IronSession = req.session
+    const { account, callbackUrl } = req.query
+
+    if (account === undefined) {
+        res.status(400).json({ message: 'Account is not specified' })
+        return
+    }
+
+    if (session.allUsers === undefined) {
+        res.status(400).json({ message: 'No authentications found' })
+        return
+    }
+
+    const userIdx = session.allUsers?.findIndex((sessionUser) => sessionUser.rucioAccount === account)
+
+    if (userIdx === -1) {
+        res.status(400).json({ message: 'No authentication found for the specified account' })
+        return
+    }
+
+    const user = session.allUsers?.at(userIdx!)
+    await addOrUpdateSessionUser(session, user!, true)
+    res.redirect(callbackUrl as string ?? '/dashboard')
+}
+
+export default withSessionRoute(switchUsers)
