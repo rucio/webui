f1fb240b17fdf1b31bee4873c30da01eba613355
MytsV <mytsvictoria@gmail.com>
2024-08-17 16:58:46 +0300

    Set up testing for react hooks

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    projects: [ 
+    projects: [
       '<rootDir>/test/api/jest.api.config.js',
       '<rootDir>/test/component/jest.component.config.js',
+      '<rootDir>/test/hook/jest.hook.config.js',
       '<rootDir>/test/gateway/jest.gateway.config.js',
       '<rootDir>/test/sdk/jest.sdk.config.js',
     ],
