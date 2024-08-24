8fb15493e371eac52ee1f6a6ac509b1fd32b72d1
MytsV <mytsvictoria@gmail.com>
2024-08-24 23:03:45 +0300

    Configure shadcn/ui

--- src/component-library/utils.ts
+++ src/component-library/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 // Function to emulate pausing between interactions
 export function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
