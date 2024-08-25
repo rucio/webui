8d0a3eb346a233247dad5d0d96d0f71f4f50e982
MytsV <mytsvictoria@gmail.com>
2024-08-25 16:35:46 +0300

    Fix ListDIDs storybook and a bug with stretching and pagination
    Set a fixed height for the table until the full redesign of the page is finished

--- src/component-library/Pages/DID/ListDID.stories.tsx
+++ src/component-library/Pages/DID/ListDID.stories.tsx
@@ -1,17 +1,27 @@
-import { StoryFn, Meta } from '@storybook/react';
-import { ListDID as LD } from './ListDID';
-import { fixtureDIDMetaViewModel, fixtureDIDViewModel, mockUseComDOM } from 'test/fixtures/table-fixtures';
+import {StoryFn, Meta} from '@storybook/react';
+import {ListDID as LD} from './ListDID';
+import {
+    fixtureDIDMetaViewModel,
+    fixtureDIDViewModel,
+    mockUseChunkedStream,
+} from 'test/fixtures/table-fixtures';
+import {DIDMetaViewModel} from "@/lib/infrastructure/data/view-model/did";
 
 export default {
     title: 'Components/Pages/DID',
     component: LD,
 } as Meta<typeof LD>;
 
-const Template: StoryFn<typeof LD> = (args) => <LD {...args} />;
+const Template: StoryFn<typeof LD> = (args) => {
+    return <div className="flex flex-col h-screen bg-neutral-900">
+        <LD {...args} />
+    </div>;
+}
 
 export const ListDID = Template.bind({});
 ListDID.args = {
-    comdom: mockUseComDOM(Array.from({length: 100}, () => fixtureDIDViewModel())),
-    didMetaQuery: (scope: string, name: string) => { },
-    didMetaQueryResponse: fixtureDIDMetaViewModel()
+    streamingHook: mockUseChunkedStream(Array.from({length: 100}, () => fixtureDIDViewModel())),
+    metaQuery: async (): Promise<DIDMetaViewModel> => {
+        return Promise.resolve(fixtureDIDMetaViewModel());
+    },
 }
