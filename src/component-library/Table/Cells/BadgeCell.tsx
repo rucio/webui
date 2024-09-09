4c7fd69c6613d48656a1e7014a78fd49ed5a90b7
MytsV <mytsvictoria@gmail.com>
2024-09-09 14:58:08 +0300

    Fix badge cell not centering text in firefox

--- src/component-library/Table/Cells/BadgeCell.tsx
+++ src/component-library/Table/Cells/BadgeCell.tsx
@@ -4,9 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export const BadgeCell = (props: { value: string; colorClass: string }) => {
     const badgeClasses = twMerge(
         'inline grow',
+        'flex justify-center items-center',
         'rounded',
         'px-3 m-2',
-        'content-center text-center',
         'text-neutral-900 dark:text-neutral-100',
         props.colorClass,
         'bg-opacity-50',
