7a6e8641314a6246f53639866c5ca94e276ca303
MytsV <mytsvictoria@gmail.com>
2024-09-15 12:38:34 +0300

    Add a story to test streaming in list rules

--- src/component-library/pages/Rule/list/ListRule.stories.tsx
+++ src/component-library/pages/Rule/list/ListRule.stories.tsx
@@ -2,6 +2,8 @@ import {StoryFn, Meta} from '@storybook/react';
 import {fixtureRuleViewModel} from '@/test/fixtures/table-fixtures';
 import {ListRule} from "@/component-library/pages/Rule/list/ListRule";
 import {ToastedTemplate} from "@/component-library/templates/ToastedTemplate/ToastedTemplate";
+import {getDecoratorWithWorker} from "@/test/mocks/handlers/story-decorators";
+import {getMockStreamEndpoint} from "@/test/mocks/handlers/streaming-handlers";
 
 export default {
     title: 'Components/Pages/Rule/List',
@@ -12,7 +14,17 @@ const Template: StoryFn<typeof ListRule> = args => <ToastedTemplate>
     <ListRule {...args} />
 </ToastedTemplate>;
 
-export const InitialData = Template.bind({});
-InitialData.args = {
+export const InitialDataNoEndpoint = Template.bind({});
+InitialDataNoEndpoint.args = {
     initialData: Array.from({length: 50}, () => fixtureRuleViewModel()),
 }
+
+export const RegularStreaming = Template.bind({});
+RegularStreaming.decorators = [
+    getDecoratorWithWorker([
+        getMockStreamEndpoint('/api/feature/list-rules', {
+            data: Array.from({length: 500}, fixtureRuleViewModel),
+            delay: 1,
+        }),
+    ]),
+];
