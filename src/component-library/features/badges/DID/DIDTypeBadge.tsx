0faee0db48d40453a63c26cf5417947b33079e23
MytsV <mytsvictoria@gmail.com>
2024-09-20 16:00:57 +0300

    Finish selected DIDs table view in the first view of rule creation

--- src/component-library/features/badges/DID/DIDTypeBadge.tsx
+++ src/component-library/features/badges/DID/DIDTypeBadge.tsx
@@ -4,13 +4,13 @@ import { Badge } from '@/component-library/atoms/misc/Badge';
 import { twMerge } from 'tailwind-merge';
 
 const typeColorClasses: Record<DIDType, string> = {
-    All: 'bg-neutral-500',
+    All: 'bg-neutral-0 dark:bg-neutral-900',
     Collection: 'bg-base-info-500',
     Container: 'bg-extra-emerald-500',
     Dataset: 'bg-extra-yellow-400',
-    Derived: 'bg-neutral-500',
+    Derived: 'bg-neutral-0 dark:bg-neutral-900',
     File: 'bg-extra-rose-500',
-    Unknown: 'bg-neutral-500',
+    Unknown: 'bg-neutral-0 dark:bg-neutral-900',
 };
 
 export const DIDTypeBadge = (props: { value: DIDType; className?: string }) => {
