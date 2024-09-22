8c1391da5b219dfb9319462859f3b8bdcfaf5ad6
MytsV <mytsvictoria@gmail.com>
2024-09-22 20:22:44 +0300

    Make previous elements in timeline clickable

--- src/component-library/atoms/misc/CircleWithText.tsx
+++ src/component-library/atoms/misc/CircleWithText.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { cn } from '@/component-library/utils';
 
-const CircleWithText = ({ text, className }: { text: string; className?: string }) => {
+const CircleWithText = ({ text, className, onClick }: { text: string; onClick?: () => void; className?: string }) => {
     const classes = cn(
         'flex items-center justify-center',
         'w-12 h-12 rounded-full',
         'text-neutral-100',
         'border border-neutral-900 dark:border-neutral-100 border-opacity-10 dark:border-opacity-10',
         className,
+        onClick ? 'cursor-pointer' : '',
     );
 
     return (
-        <div className={classes}>
+        <div className={classes} onClick={onClick}>
             <span className="text-lg">{text}</span>
         </div>
     );
