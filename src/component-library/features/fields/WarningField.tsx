6fea486ace47f68dde59417fa3c08dca2c1ea729
MytsV <mytsvictoria@gmail.com>
2024-09-22 19:20:15 +0300

    Improve dark mode warning field

--- src/component-library/features/fields/WarningField.tsx
+++ src/component-library/features/fields/WarningField.tsx
@@ -5,7 +5,7 @@ import {HiExclamationCircle} from "react-icons/hi";
 
 export const WarningField = ({ children, className, ...props }: { children: ReactNode; className?: string }) => {
     return (
-        <Field className={cn('bg-base-warning-300', 'items-center py-2 space-x-2', className)} {...props}>
+        <Field className={cn('bg-base-warning-300 dark:bg-base-error-500 dark:bg-opacity-60', 'items-center py-2 space-x-2', className)} {...props}>
             <HiExclamationCircle className="h-6 w-6 flex-shrink-0" />
             {children}
         </Field>
