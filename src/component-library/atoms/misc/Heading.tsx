4e3dec75024ece5963c16c1e12f3a3d3f9259c0c
MytsV <mytsvictoria@gmail.com>
2024-09-23 19:44:14 +0300

    Make heading size configurable

--- src/component-library/atoms/misc/Heading.tsx
+++ src/component-library/atoms/misc/Heading.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
 import { cn } from '@/component-library/utils';
+import { cva } from 'class-variance-authority';
 
-export const Heading = ({ text, className, ...props }: { text: string; className?: string }) => {
+const headingStyles = cva('text-neutral-900 dark:text-neutral-100 font-bold', {
+    variants: {
+        size: {
+            sm: 'text-xl',
+            md: 'text-2xl',
+            lg: 'text-4xl',
+        },
+    },
+    defaultVariants: {
+        size: 'lg',
+    },
+});
+
+interface HeadingProps {
+    text: string;
+    size?: 'sm' | 'md' | 'lg';
+    className?: string;
+}
+
+export const Heading = ({ text, size, className, ...props }: HeadingProps) => {
     return (
-        <h1 className={cn('text-4xl text-neutral-900 dark:text-neutral-100 font-bold', className)} {...props}>
+        <h1 className={cn(headingStyles({ size }), className)} {...props}>
             {text}
         </h1>
     );
