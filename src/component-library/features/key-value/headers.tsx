86a0eb27e7f23532979811b35a1fc71b2b6b6be2
MytsV <mytsvictoria@gmail.com>
2024-09-24 12:28:27 +0300

    Decompose the list DID meta

---
+++ src/component-library/features/key-value/headers.tsx
@@ -0,0 +1,36 @@
+import {cn} from "@/component-library/utils";
+import Link from "next/link";
+import {HiExternalLink} from "react-icons/hi";
+import {ReactNode} from "react";
+
+const headerCommonClasses = cn(
+    'h-[52px] px-4 py-2 space-x-2',
+    'bg-neutral-200 dark:bg-neutral-700',
+    'border-b border-neutral-900 dark:border-neutral-100 border-opacity-10 dark:border-opacity-10',
+    'text-neutral-700 dark:text-neutral-300 font-medium',
+    'flex grow items-center w-full',
+    'whitespace-nowrap',
+);
+
+export const KeyValueLinkHeader = ({href, children}: { href: string; children: ReactNode }) => {
+    return (
+        <Link
+            href={href}
+            className={cn(
+                headerCommonClasses,
+                'hover:text-brand-600 dark:hover:text-brand-400',
+            )}
+        >
+            <HiExternalLink className="h-5 w-5"/>
+            {children}
+        </Link>
+    );
+};
+
+export const KeyValueHeader = ({href, children}: { href: string; children: ReactNode }) => {
+    return (
+        <div className={headerCommonClasses}>
+            {children}
+        </div>
+    );
+};
--- src/component-library/pages/DID/list/meta/ListDIDMeta.tsx
+++ src/component-library/pages/DID/list/meta/ListDIDMeta.tsx
@@ -1,8 +1,5 @@
 import { DIDMetaViewModel } from '@/lib/infrastructure/data/view-model/did';
 import { cn } from '@/component-library/utils';
-import { HiExternalLink } from 'react-icons/hi';
-import Link from 'next/link';
-import { ReactNode } from 'react';
 import { Field } from '@/component-library/atoms/misc/Field';
 import { formatDate, formatFileSize } from '@/component-library/features/utils/text-formatters';
 import { Divider } from '@/component-library/atoms/misc/Divider';
@@ -12,35 +9,15 @@ import { DIDTypeBadge } from '@/component-library/features/badges/DID/DIDTypeBad
 import { CopyableField } from '@/component-library/features/fields/CopyableField';
 import { DIDAvailabilityBadge } from '@/component-library/features/badges/DID/DIDAvailabilityBadge';
 import { LoadingSpinner } from '@/component-library/atoms/loading/LoadingSpinner';
-
-const MetaRow = (props: { name: string; children: ReactNode }) => {
-    return (
-        <div className="flex w-full items-center h-12">
-            <span className={cn('text-neutral-700 dark:text-neutral-300', 'min-w-[10rem] pr-3', 'font-medium text-right text-sm')}>{props.name}</span>
-            <span className="flex items-center">{props.children}</span>
-        </div>
-    );
-};
+import {KeyValueLinkHeader} from "@/component-library/features/key-value/headers";
+import {KeyValueRow} from "@/component-library/features/key-value/KeyValueRow";
+import {KeyValueWrapper} from "@/component-library/features/key-value/KeyValueWrapper";
 
 const MetaHeader = ({ scope, name }: { scope: string; name: string }) => {
     return (
-        <Link
-            href={`/did/page/${scope}/${name}`}
-            className={cn(
-                'h-[52px] px-4 py-2 space-x-2',
-                'bg-neutral-200 dark:bg-neutral-700',
-                'border-b border-neutral-900 dark:border-neutral-100 border-opacity-10 dark:border-opacity-10',
-                'text-neutral-700 dark:text-neutral-300 font-medium',
-                'flex grow items-center w-full',
-                'hover:text-brand-600 dark:hover:text-brand-400',
-                'whitespace-nowrap',
-            )}
-        >
-            <HiExternalLink className="h-5 w-5" />
-            <span>
-                {scope}:{name}
-            </span>
-        </Link>
+        <KeyValueLinkHeader href={`/did/page/${scope}/${name}`}>
+            <span>{scope}:{name}</span>
+        </KeyValueLinkHeader>
     );
 };
 
@@ -49,24 +26,24 @@ const MetaContents = ({ meta }: { meta: DIDMetaViewModel }) => {
         return (
             <>
                 {meta.bytes && (
-                    <MetaRow name="Size">
+                    <KeyValueRow name="Size">
                         <Field>{formatFileSize(meta.bytes)}</Field>
-                    </MetaRow>
+                    </KeyValueRow>
                 )}
                 {meta.guid && (
-                    <MetaRow name="GUID">
+                    <KeyValueRow name="GUID">
                         <CopyableField text={meta.guid} />
-                    </MetaRow>
+                    </KeyValueRow>
                 )}
                 {meta.adler32 && (
-                    <MetaRow name="Adler32">
+                    <KeyValueRow name="Adler32">
                         <CopyableField text={meta.adler32} />
-                    </MetaRow>
+                    </KeyValueRow>
                 )}
                 {meta.md5 && (
-                    <MetaRow name="MD5">
+                    <KeyValueRow name="MD5">
                         <CopyableField text={meta.md5} />
-                    </MetaRow>
+                    </KeyValueRow>
                 )}
                 <Divider />
             </>
@@ -77,44 +54,44 @@ const MetaContents = ({ meta }: { meta: DIDMetaViewModel }) => {
         <div className="min-w-full w-fit md:max-h-[0px]">
             <MetaHeader scope={meta.scope} name={meta.name} />
             <div className="flex flex-col px-4 py-2 w-full">
-                <MetaRow name="Type">
+                <KeyValueRow name="Type">
                     <DIDTypeBadge value={meta.did_type} />
-                </MetaRow>
-                <MetaRow name="Account">
+                </KeyValueRow>
+                <KeyValueRow name="Account">
                     <Field>{meta.account}</Field>
-                </MetaRow>
+                </KeyValueRow>
                 {meta.is_open !== null && (
-                    <MetaRow name="Is Open">
+                    <KeyValueRow name="Is Open">
                         <Checkbox checked={meta.is_open} />
-                    </MetaRow>
+                    </KeyValueRow>
                 )}
-                <MetaRow name="Monotonic">
+                <KeyValueRow name="Monotonic">
                     <Checkbox checked={meta.monotonic} />
-                </MetaRow>
+                </KeyValueRow>
                 <Divider />
-                <MetaRow name="Created At">
+                <KeyValueRow name="Created At">
                     <Field>{formatDate(meta.created_at)}</Field>
-                </MetaRow>
-                <MetaRow name="Updated At">
+                </KeyValueRow>
+                <KeyValueRow name="Updated At">
                     <Field>{formatDate(meta.updated_at)}</Field>
-                </MetaRow>
+                </KeyValueRow>
                 <Divider />
                 {meta.did_type === DIDType.FILE && getFileInformation()}
-                <MetaRow name="Obsolete">
+                <KeyValueRow name="Obsolete">
                     <Checkbox checked={meta.obsolete} />
-                </MetaRow>
-                <MetaRow name="Hidden">
+                </KeyValueRow>
+                <KeyValueRow name="Hidden">
                     <Checkbox checked={meta.hidden} />
-                </MetaRow>
-                <MetaRow name="Suppressed">
+                </KeyValueRow>
+                <KeyValueRow name="Suppressed">
                     <Checkbox checked={meta.suppressed} />
-                </MetaRow>
-                <MetaRow name="Purge Replicas">
+                </KeyValueRow>
+                <KeyValueRow name="Purge Replicas">
                     <Checkbox checked={meta.purge_replicas} />
-                </MetaRow>
-                <MetaRow name="Availability">
+                </KeyValueRow>
+                <KeyValueRow name="Availability">
                     <DIDAvailabilityBadge value={meta.availability} />
-                </MetaRow>
+                </KeyValueRow>
             </div>
         </div>
     );
@@ -138,15 +115,13 @@ export const ListDIDMeta = ({ meta, isLoading, hasError }: ListDIDMetaProps) => 
     const showMeta = meta && !isLoading && !hasError;
 
     return (
-        <div
+        <KeyValueWrapper
             className={cn(
                 showMeta ? 'flex-none' : 'flex grow items-center min-h-[10rem]',
                 'md:flex-1 overflow-auto',
-                'bg-neutral-0 dark:bg-neutral-800',
-                'rounded-md border border-neutral-900 dark:border-neutral-100 border-opacity-10 dark:border-opacity-10',
             )}
         >
             {showMeta ? <MetaContents meta={meta} /> : <MetaStub isLoading={isLoading} hasError={hasError} />}
-        </div>
+        </KeyValueWrapper>
     );
 };
