dafd58bb7bd730f6a72fdf1a177ebe9e24ad178c
MytsV <mytsvictoria@gmail.com>
2024-10-31 10:55:44 +0200

    Make parameters encoding configurable with an environmental variable

--- src/lib/sdk/streaming-gateway.ts
+++ src/lib/sdk/streaming-gateway.ts
@@ -1,12 +1,17 @@
 import 'reflect-metadata';
 import StreamGatewayOutputPort from '@/lib/core/port/secondary/stream-gateway-output-port';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import fetch, { Response } from 'node-fetch';
 import { PassThrough, Transform } from 'node:stream';
 import { HTTPRequest, prepareRequestArgs } from '@/lib/sdk/http';
 import { BytesToStringifiedJSONTransform, NewlineDelimittedDataParser } from '@/lib/sdk/stream-transformers';
+import type EnvConfigGatewayOutputPort from '@/lib/core/port/secondary/env-config-gateway-output-port';
+import GATEWAYS from '@/lib/infrastructure/ioc/ioc-symbols-gateway';
+
 @injectable()
 export default class StreamingGateway implements StreamGatewayOutputPort {
+    constructor(@inject(GATEWAYS.ENV_CONFIG) private envConfigGateway: EnvConfigGatewayOutputPort) {}
+
     private convertChunkBytesToString = new Transform({
         transform(chunk, encoding, callback) {
             callback(null, chunk.toString());
@@ -14,7 +19,8 @@ export default class StreamingGateway implements StreamGatewayOutputPort {
     });
 
     async getTextStream(request: HTTPRequest): Promise<PassThrough | Response> {
-        const { url, requestArgs } = prepareRequestArgs(request);
+        const encodeParams = await this.envConfigGateway.paramsEncodingEnabled();
+        const { url, requestArgs } = prepareRequestArgs(request, encodeParams);
 
         const response = await fetch(url, requestArgs);
         if (!response.ok || response.body === null) {
@@ -27,7 +33,8 @@ export default class StreamingGateway implements StreamGatewayOutputPort {
     }
 
     async getJSONChunks(request: HTTPRequest, ndjson: boolean = false): Promise<{ type: 'response' | 'stream'; content: PassThrough | Response }> {
-        const { url, requestArgs } = prepareRequestArgs(request);
+        const encodeParams = await this.envConfigGateway.paramsEncodingEnabled();
+        const { url, requestArgs } = prepareRequestArgs(request, encodeParams);
         const response = await fetch(url, requestArgs);
 
         if (!response.ok || response.body === null) {
