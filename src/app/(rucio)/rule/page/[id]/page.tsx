4557de0ab0a81830a092f90486dce19629f8fd81
MytsV <mytsvictoria@gmail.com>
2024-11-14 10:08:18 +0200

    Add title to the rule details page

--- src/app/(rucio)/rule/page/[id]/page.tsx
+++ src/app/(rucio)/rule/page/[id]/page.tsx
@@ -1,7 +1,12 @@
 'use client';
 
 import { DetailsRule } from '@/component-library/pages/Rule/details/DetailsRule';
+import {useEffect} from "react";
 
 export default function PageRule({ params }: { params: { id: string } }) {
+    useEffect(() => {
+        document.title = 'Rule - Rucio';
+    }, []);
+    
     return <DetailsRule id={params.id} />;
 }
