c29662cfb5fc95982f02c9b7e96c5e29d9365758
MytsV <mytsvictoria@gmail.com>
2024-11-26 13:08:16 +0200

    Implement theme switching button

--- src/component-library/features/layout/Header.tsx
+++ src/component-library/features/layout/Header.tsx
@@ -1,13 +1,44 @@
+'use client';
+
 import { cn } from '@/component-library/utils';
 import { useTheme } from 'next-themes';
+import { HiMoon, HiSun } from 'react-icons/all';
+import { useEffect, useRef, useState } from 'react';
 
-export const Header = () => {
-    const { setTheme } = useTheme();
+const ThemeSwitchButton = () => {
+    const { resolvedTheme, setTheme } = useTheme();
+    const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+    const buttonRef = useRef<HTMLDivElement>(null);
+
+    const updateTheme = () => {
+        setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+    };
+
+    useEffect(() => {
+        if (resolvedTheme && buttonRef.current) {
+            buttonRef.current.className = buttonRef.current.className.replace(' hidden', ' flex');
+            setIsDarkMode(resolvedTheme === 'dark');
+        }
+    }, [resolvedTheme]);
 
     return (
-        <header className={cn('h-14 z-[100]', 'border border-neutral-900 dark:border-neutral-100 border-opacity-10 dark:border-opacity-10')}>
-            <button onClick={() => setTheme('dark')}>Dark</button>
-            <button onClick={() => setTheme('light')}>Light</button>
+        <div className="rounded-md hover:bg-neutral-100 dark:hover:bg-neutral-800 px-2 items-center hidden" onClick={updateTheme} ref={buttonRef}>
+            {isDarkMode ? <HiMoon className="h-6 w-6" /> : <HiSun className="h-6 w-6" />}
+        </div>
+    );
+};
+
+export const Header = () => {
+    return (
+        <header
+            className={cn(
+                'h-14 z-[100]',
+                'border border-neutral-900 dark:border-neutral-100 border-opacity-10 dark:border-opacity-10',
+                'p-2 flex flex-row',
+            )}
+        >
+            <ThemeSwitchButton />
         </header>
     );
 };
