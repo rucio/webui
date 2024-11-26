171b5b48ab8c250d92270d5aa8546ed52e8b3e79
MytsV <mytsvictoria@gmail.com>
2024-11-26 17:29:54 +0200

    Implement a responsive navigation menu

--- src/component-library/features/layout/Header.tsx
+++ src/component-library/features/layout/Header.tsx
@@ -2,8 +2,132 @@
 
 import { cn } from '@/component-library/utils';
 import { useTheme } from 'next-themes';
-import { HiMoon, HiSun } from 'react-icons/all';
+import {HiChevronDown, HiMenu, HiMoon, HiSun, HiX} from 'react-icons/hi';
 import { useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+type TMenuItem = {
+    title: string;
+    path?: string;
+};
+
+type TFullMenuItem = TMenuItem & {
+    children?: TMenuItem[];
+};
+
+const MenuItem = ({ item, pathname, onClick }: { item: TMenuItem; pathname: string | null; onClick?: () => void }) => {
+    const classes = `hover:text-brand-500 ${item.path === pathname && 'text-brand-500 font-semibold'}`;
+    return (
+        <Link href={item.path ?? '/'} className={classes} onClick={onClick}>
+            {item.title}
+        </Link>
+    );
+};
+
+const DesktopNavigationBar = ({ menuItems }: { menuItems: TFullMenuItem[] }) => {
+    const pathname = usePathname();
+
+    const getItemChildren = (children: TMenuItem[]) => {
+        return (
+            <div
+                className={cn(
+                    'absolute left-0 w-max',
+                    'opacity-0 group-hover:opacity-100 group-hover:pointer-events-auto',
+                    'bg-neutral-0 dark:bg-neutral-900',
+                    'rounded-md border border-neutral-900 dark:border-neutral-100 border-opacity-10 dark:border-opacity-10',
+                )}
+            >
+                <div className="flex flex-col space-y-2 p-4">
+                    {children.map(child => (
+                        <MenuItem key={child.path} item={child} pathname={pathname} />
+                    ))}
+                </div>
+            </div>
+        );
+    };
+
+    return (
+        <div className="hidden md:flex items-center space-x-6">
+            {menuItems.map(item => {
+                const key = item.title.toLowerCase();
+                const classes = `hover:text-brand-500 ${item.path === pathname && 'text-brand-500 font-semibold'}`;
+                if (item.path) {
+                    return <MenuItem key={item.path} item={item} pathname={pathname} />;
+                } else {
+                    return (
+                        <div key={key} className="relative group">
+                            <div className={classes}>
+                                <span>{item.title}</span>
+                                <HiChevronDown className="inline pl-1 h-5 w-5"/>
+                            </div>
+                            {item.children && getItemChildren(item.children)}
+                        </div>
+                    );
+                }
+            })}
+        </div>
+    );
+};
+
+const MobileNavigationBar = ({ menuItems }: { menuItems: TFullMenuItem[] }) => {
+    const pathname = usePathname();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    return (
+        <div className="flex md:hidden">
+            <button
+                className="rounded-md hover:bg-neutral-100 dark:hover:bg-neutral-800 px-2 items-center"
+                onClick={() => setIsMenuOpen(prevState => !prevState)}
+            >
+                <HiMenu className="h-5 w-5" />
+            </button>
+            {isMenuOpen && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 z-50">
+                    <div className="fixed inset-0 bg-white dark:bg-neutral-900 p-6 overflow-y-auto flex flex-col space-y-6">
+                        <button onClick={() => setIsMenuOpen(false)} className="self-end p-2 focus:outline-none">
+                            <HiX className="h-5 w-5" />
+                        </button>
+
+                        <nav className="flex flex-col items-center justify-center space-y-4">
+                            {menuItems.map(item => {
+                                if (item.path) {
+                                    return <MenuItem key={item.path} item={item} pathname={pathname} onClick={() => setIsMenuOpen(false)} />;
+                                } else {
+                                    return item.children?.map(child => (
+                                        <MenuItem key={child.path} item={child} pathname={pathname} onClick={() => setIsMenuOpen(false)} />
+                                    ));
+                                }
+                            })}
+                        </nav>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+const NavigationBar = () => {
+    const menuItems: TFullMenuItem[] = [
+        { title: 'Dashboard', path: '/dashboard' },
+        { title: 'DIDs', path: '/did/list' },
+        { title: 'RSEs', path: '/rse/list' },
+        {
+            title: 'Rules',
+            children: [
+                { title: 'List Rules', path: '/rule/list' },
+                { title: 'Create a rule', path: '/rule/create' },
+            ],
+        },
+    ];
+
+    return (
+        <>
+            <MobileNavigationBar menuItems={menuItems} />
+            <DesktopNavigationBar menuItems={menuItems} />
+        </>
+    );
+};
 
 const ThemeSwitchButton = () => {
     const { resolvedTheme, setTheme } = useTheme();
@@ -39,6 +163,7 @@ export const Header = () => {
             )}
         >
             <ThemeSwitchButton />
+            <NavigationBar />
         </header>
     );
 };
