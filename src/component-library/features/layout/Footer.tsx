93e877a40649a6d36db9fdb20cb4b5d3b5537649
MytsV <mytsvictoria@gmail.com>
2024-11-26 13:15:47 +0200

    Add a stub footer

---
+++ src/component-library/features/layout/Footer.tsx
@@ -0,0 +1,7 @@
+import { cn } from '@/component-library/utils';
+
+export const Footer = () => {
+    return (
+        <footer className={cn('h-14 z-[100]', 'border border-neutral-900 dark:border-neutral-100 border-opacity-10 dark:border-opacity-10')}></footer>
+    );
+};
--- src/component-library/pages/Layout.tsx
+++ src/component-library/pages/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import { Header } from '@/component-library/features/layout/Header';
+import { Footer } from '@/component-library/features/layout/Footer';
 
 type LayoutProps = {
     children: ReactNode;
@@ -10,6 +11,7 @@ export const Layout = ({ children }: LayoutProps) => {
         <div className="min-h-screen flex flex-col bg-neutral-0 dark:bg-neutral-900">
             <Header />
             <main className="flex flex-col grow px-4 py-6 sm:px-8 md:px-12 lg:px-24 xl:px-32 2xl:px-48 3xl:px-64">{children}</main>
+            <Footer />
         </div>
     );
 };
