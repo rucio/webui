d7b80baf3c9fe2193e5a760d82a20ddd253aa316
MytsV <mytsvictoria@gmail.com>
2024-11-26 12:44:43 +0200

    Fix background coloring of the layout

--- src/component-library/pages/Layout.tsx
+++ src/component-library/pages/Layout.tsx
@@ -7,7 +7,7 @@ type LayoutProps = {
 
 export const Layout = ({ children }: LayoutProps) => {
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col bg-neutral-0 dark:bg-neutral-900">
             <Header />
             <main className="flex flex-col grow px-4 py-6 sm:px-8 md:px-12 lg:px-24 xl:px-32 2xl:px-48 3xl:px-64">{children}</main>
         </div>
