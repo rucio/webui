eac2ae7967653f40169f05f2c42ed7223620266a
MytsV <mytsvictoria@gmail.com>
2025-01-10 16:16:14 +0200

    Return the toaster to the layout

--- src/component-library/pages/Layout.tsx
+++ src/component-library/pages/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import { Header } from '@/component-library/features/layout/Header';
 import { Footer } from '@/component-library/features/layout/Footer';
+import {Toaster} from "@/component-library/atoms/toast/Toaster";
 
 type LayoutProps = {
     children: ReactNode;
@@ -11,6 +12,7 @@ export const Layout = ({ children }: LayoutProps) => {
         <div className="min-h-screen flex flex-col bg-neutral-0 dark:bg-neutral-900">
             <Header />
             <main className="flex flex-col grow px-4 py-6 sm:px-8 md:px-12 lg:px-24 xl:px-32 2xl:px-48 3xl:px-64">{children}</main>
+            <Toaster />
             <Footer />
         </div>
     );
