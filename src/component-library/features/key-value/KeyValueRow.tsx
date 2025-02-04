3c5f9b58b1b7ea49e45debaa6440b860b843f3c4
maany <imptodefeat@gmail.com>
2025-02-04 18:14:28 +0100

    chore(ui): <KeyValueRow/>, autoscoll if data overflows #514

--- src/component-library/features/key-value/KeyValueRow.tsx
+++ src/component-library/features/key-value/KeyValueRow.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/component-library/utils';
 
 export const KeyValueRow = (props: { name: string; children: ReactNode }) => {
     return (
-        <div className="flex w-full items-center h-12">
+        <div className="flex w-full items-center h-12 overflow-auto">
             <span className={cn('text-neutral-700 dark:text-neutral-300', 'min-w-[10rem] pr-3', 'font-medium text-right text-sm')}>{props.name}</span>
             <span className="flex items-center space-x-2">{props.children}</span>
         </div>
