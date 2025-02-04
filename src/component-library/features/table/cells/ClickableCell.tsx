867ae6db68f49894c7159915fb4e824bb44568de
maany <imptodefeat@gmail.com>
2025-02-04 18:13:31 +0100

    chore(ui): <ClickableCell/>, allow optional className prop

--- src/component-library/features/table/cells/ClickableCell.tsx
+++ src/component-library/features/table/cells/ClickableCell.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 import { HiExternalLink } from 'react-icons/hi';
 import { twMerge } from 'tailwind-merge';
 
-export const ClickableCell = (props: { href: string; children: React.ReactNode }) => {
+export const ClickableCell = (props: { href: string; children: React.ReactNode, className?: string }) => {
     return (
         <Link href={props.href}>
-            <div>
+            <div className={twMerge(props.className)}>
                 {props.children}
                 <HiExternalLink className={twMerge('inline', 'pl-2', 'text-2xl', 'text-neutral-900 dark:text-neutral-100')} />
             </div>
