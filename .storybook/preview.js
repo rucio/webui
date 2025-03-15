12dba3b6110e8cac5f18f7b931532d618e8f6098
MytsV <mytsvictoria@gmail.com>
2025-03-15 15:31:32 +0200

    Decorate the storybooks with a theme provider component to fix dependent on theming components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,26 @@
+import {ThemeProvider, useTheme} from "next-themes";
+
+export const ThemedStory = ({ children }) => {
+  const { theme, setTheme } = useTheme();
+  return <div>
+    <button
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      className="mb-2"
+    >Toggle Theme</button>
+    {children}
+  </div>;
+}
+
+export const decorators = [
+  (Story) => {
+    return (
+      <ThemeProvider attribute="class" defaultTheme="light">
+        <ThemedStory><Story/></ThemedStory>
+      </ThemeProvider>
+    );
+  },
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
