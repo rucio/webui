271bbddb8347cfb686c5ff353389409230614428
MytsV <mytsvictoria@gmail.com>
2025-03-15 18:04:37 +0200

    Implement subscription streaming from the feature endpoint

--- src/app/(rucio)/subscription/list/page.tsx
+++ src/app/(rucio)/subscription/list/page.tsx
@@ -1,36 +1,6 @@
 'use client';
-import { ListSubscription as ListSubscriptionStory } from '@/component-library/pages/legacy/Subscriptions/ListSubscription';
-import { Component, useEffect, useState } from 'react';
-import useComDOM from '@/lib/infrastructure/hooks/useComDOM';
-import { SubscriptionRuleStatesViewModel } from '@/lib/infrastructure/data/view-model/subscriptions';
-import { SiteHeaderViewModel } from '@/lib/infrastructure/data/view-model/site-header';
-import { getSiteHeader } from '../../queries';
+import { ListSubscription } from '@/component-library/pages/Subscriptions/list/ListSubscription';
 
-export default function ListSubscription({ params }: { params: { account: string } }) {
-    const [accountName, setAccountName] = useState<string>('');
-    useEffect(() => {
-        getSiteHeader().then((data: SiteHeaderViewModel) => {
-            setAccountName(data.activeAccount?.rucioAccount ?? 'Unknown');
-        });
-    }, []);
-
-    const ComDOM = useComDOM<SubscriptionRuleStatesViewModel>('subscription-rule-states-query', [], false, Infinity, 50, true);
-    useEffect(() => {
-        const runQuery = async () => {
-            await ComDOM.start({
-                url: new URL(`${process.env.NEXT_PUBLIC_WEBUI_HOST}/api/feature/list-subscription-rule-states`),
-                method: 'GET',
-                headers: new Headers({
-                    'Content-Type': 'application/json',
-                } as HeadersInit),
-                body: null,
-            });
-        };
-        runQuery();
-    }, []);
-    return (
-        <div>
-            <ListSubscriptionStory accountname={accountName} comdom={ComDOM} />
-        </div>
-    );
+export default function Page() {
+    return <ListSubscription />;
 }
--- src/component-library/pages/Subscriptions/list/ListSubscription.tsx
+++ src/component-library/pages/Subscriptions/list/ListSubscription.tsx
@@ -2,13 +2,22 @@ import { ListSubscriptionTable } from '@/component-library/pages/Subscriptions/l
 import useTableStreaming from '@/lib/infrastructure/hooks/useTableStreaming';
 import { SubscriptionRuleStatesViewModel } from '@/lib/infrastructure/data/view-model/subscriptions';
 import { Heading } from '@/component-library/atoms/misc/Heading';
+import { useEffect, useState } from 'react';
 
 type ListSubscriptionProps = {
     initialData?: SubscriptionRuleStatesViewModel[];
 };
 
 export const ListSubscription = (props: ListSubscriptionProps) => {
-    const { onGridReady, streamingHook, startStreaming, stopStreaming } = useTableStreaming<SubscriptionRuleStatesViewModel>(props.initialData);
+    const { gridApi, onGridReady, streamingHook, startStreaming } = useTableStreaming<SubscriptionRuleStatesViewModel>(props.initialData);
+    const [startedStreaming, setStartedStreaming] = useState(false);
+
+    useEffect(() => {
+        if (gridApi !== null && !startedStreaming) {
+            startStreaming('/api/feature/list-subscription-rule-states');
+            setStartedStreaming(true);
+        }
+    }, [gridApi, startStreaming]);
 
     return (
         <div className="flex flex-col space-y-3 w-full grow">
