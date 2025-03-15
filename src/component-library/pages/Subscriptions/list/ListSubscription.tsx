4da08542b373092691cb2c8544f12eef5c017c58
MytsV <mytsvictoria@gmail.com>
2025-03-15 18:27:53 +0200

    Make subscription name clickable and load account name for navigation

--- src/component-library/pages/Subscriptions/list/ListSubscription.tsx
+++ src/component-library/pages/Subscriptions/list/ListSubscription.tsx
@@ -3,6 +3,10 @@ import useTableStreaming from '@/lib/infrastructure/hooks/useTableStreaming';
 import { SubscriptionRuleStatesViewModel } from '@/lib/infrastructure/data/view-model/subscriptions';
 import { Heading } from '@/component-library/atoms/misc/Heading';
 import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { SiteHeaderViewModel } from '@/lib/infrastructure/data/view-model/site-header';
+import { InfoField } from '@/component-library/features/fields/InfoField';
+import { WarningField } from '@/component-library/features/fields/WarningField';
 
 type ListSubscriptionProps = {
     initialData?: SubscriptionRuleStatesViewModel[];
@@ -19,10 +23,47 @@ export const ListSubscription = (props: ListSubscriptionProps) => {
         }
     }, [gridApi, startStreaming]);
 
+    // TODO: replace with server-side request
+    const querySiteHeader = async () => {
+        const res = await fetch('/api/feature/get-site-header');
+        return res.json();
+    };
+
+    const {
+        data: siteHeader,
+        error: siteHeaderError,
+        isFetching: isSiteHeaderFetching,
+    } = useQuery<SiteHeaderViewModel>({
+        queryKey: ['subscription-account'],
+        queryFn: querySiteHeader,
+        retry: false,
+        refetchOnWindowFocus: false,
+    });
+
+    if (isSiteHeaderFetching) {
+        return (
+            <InfoField>
+                <span>Loading account information...</span>
+            </InfoField>
+        );
+    }
+
+    if (siteHeaderError || !siteHeader || !siteHeader.activeAccount) {
+        console.log(siteHeaderError);
+        return (
+            <WarningField>
+                <span>Failed to load account information</span>
+            </WarningField>
+        );
+    }
+
+    const account = siteHeader.activeAccount.rucioAccount;
+
     return (
         <div className="flex flex-col space-y-3 w-full grow">
             <Heading text="Subscriptions" />
-            <ListSubscriptionTable streamingHook={streamingHook} onGridReady={onGridReady} />
+            <Heading size="sm" text={`for account ${account}`} />
+            <ListSubscriptionTable streamingHook={streamingHook} onGridReady={onGridReady} account={account} />
         </div>
     );
 };
