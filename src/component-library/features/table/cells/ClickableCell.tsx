a76b7e6a570c0ad181882e3a9432732bf9cbaf32
MytsV <mytsvictoria@gmail.com>
2025-06-30 13:24:34 +0300

    Move link icon to the right and open a new tab

--- src/component-library/features/table/cells/ClickableCell.tsx
+++ src/component-library/features/table/cells/ClickableCell.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
 import { HiExternalLink } from 'react-icons/hi';
-import { twMerge } from 'tailwind-merge';
+import { cn } from '@/component-library/utils';
 
 export const ClickableCell = (props: { href: string; children: React.ReactNode; className?: string }) => {
+    const onClick = () => {
+        window.open(props.href, '_blank', 'noopener,noreferrer');
+    };
+
     return (
-        <Link href={props.href}>
-            <div className={twMerge(props.className)}>
-                {props.children}
-                <HiExternalLink className={twMerge('inline', 'pl-2', 'text-2xl', 'text-neutral-900 dark:text-neutral-100')} />
-            </div>
-        </Link>
+        <div className={cn('flex items-center cursor-pointer', props.className)} onClick={onClick}>
+            <HiExternalLink className="flex-shrink-0 text-lg mr-1 text-neutral-900 dark:text-neutral-100" />
+            {props.children}
+        </div>
     );
 };
